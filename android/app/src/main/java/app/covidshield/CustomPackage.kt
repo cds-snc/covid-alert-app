CovidShield/mobile

Initial open-source release
Authors alphabetically:

Co-authored-by: Anne-Marie Leger <31770030+annemarieleger@users.noreply.github.com>
Co-authored-by: Burke Libbey <burke@libbey.me>
Co-authored-by: Guillaumegranger1 <guillaume.granger@shopify.com>
Co-authored-by: Henry Tao <henry.tao@shopify.com>
Co-authored-by: Jordan Good <jordan.good@shopify.com>
Co-authored-by: Kyle Peatt <kpeatt@gmail.com>
Co-authored-by: Sergey Gavrilyuk <sergey.gavrilyuk@shopify.com>
Co-authored-by: emilybulger <56970554+emilybulger@users.noreply.github.com>

android/app/src/main/java/com/covidshield/CustomPackage.kt
@@ -1,0 +1,22 @@
+package com.covidshield
+
+import android.view.View
+import com.covidshield.module.CovidShieldModule
+import com.covidshield.module.ExposureNotificationModule
+import com.covidshield.module.PushNotificationModule
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class CustomPackage : ReactPackage {
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> = mutableListOf(
+        ExposureNotificationModule(reactContext),
+        PushNotificationModule(reactContext),
+        CovidShieldModule(reactContext)
+    )
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+}

android/app/src/main/java/com/covidshield/MainActivity.java
@@ -1,0 +1,23 @@
+package com.covidshield;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "CovidShield";
+  }
+}

android/app/src/main/java/com/covidshield/MainApplication.java
@@ -1,0 +1,82 @@
+package com.covidshield;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          packages.add(new CustomPackage());
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+
+  /**
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+   */
+  private static void initializeFlipper(
+      Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.covidshield.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}

android/app/src/main/java/com/covidshield/module/ExposureNotificationModule.kt
@@ -1,0 +1,64 @@
+package com.covidshield.module
+
+import com.covidshield.extensions.toWritableArray
+import com.covidshield.extensions.toWritableMap
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.ReadableMap
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.exposurenotification.ExposureConfiguration.ExposureConfigurationBuilder
+
+class ExposureNotificationModule(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
+
+    private val exposureNotificationClient = Nearby.getExposureNotificationClient(context.applicationContext)
+
+    override fun getName(): String = "ExposureNotification"
+
+    @ReactMethod
+    fun start(promise: Promise) {
+        promise.resolve(null)
+    }
+
+    @ReactMethod
+    fun stop(promise: Promise) {
+        exposureNotificationClient.stop()
+            .addOnCompleteListener {
+                promise.resolve(null)
+            }
+    }
+
+    @ReactMethod
+    fun resetAllData(promise: Promise) {
+        promise.resolve(null)
+    }
+
+    @ReactMethod
+    fun getStatus(promise: Promise) {
+        exposureNotificationClient.isEnabled
+            .addOnSuccessListener {
+                val status = if (it) Status.ACTIVE else Status.DISABLED
+                promise.resolve(status.value)
+            }
+            .addOnFailureListener {
+                promise.resolve(Status.DISABLED.value)
+            }
+    }
+
+    @ReactMethod
+    fun detectExposure(configuration: ReadableMap, diagnosisKeysURLs: ReadableArray, promise: Promise) {
+        promise.resolve(emptyMap<String, String>().toWritableMap())
+    }
+
+    @ReactMethod
+    fun getExposureInformation(keys: ReadableArray, promise: Promise) {
+        promise.resolve(emptyList<Any>().toWritableArray())
+    }
+}
+
+private enum class Status(val value: String) {
+    ACTIVE("active"),
+    DISABLED("disabled")
+}

cds-snc/covid-alert-app

android/app/src/main/java/app/covidshield/CustomPackage.kt
@@ -1,0 +1,22 @@
+package app.covidshield
+
+import android.view.View
+import app.covidshield.module.CovidShieldModule
+import app.covidshield.module.ExposureNotificationModule
+import app.covidshield.module.PushNotificationModule
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class CustomPackage : ReactPackage {
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> = mutableListOf(
+        ExposureNotificationModule(reactContext),
+        PushNotificationModule(reactContext),
+        CovidShieldModule(reactContext)
+    )
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+}

android/app/src/main/java/app/covidshield/MainActivity.java
@@ -1,0 +1,23 @@
+package app.covidshield;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "CovidShield";
+  }
+}

android/app/src/main/java/app/covidshield/MainApplication.java
@@ -1,0 +1,78 @@
+package app.covidshield;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          packages.add(new CustomPackage());
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
+
+  /**
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+   */
+  private static void initializeFlipper(
+      Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("app.covidshield.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+}

android/app/src/main/java/app/covidshield/module/ExposureNotificationModule.kt
@@ -1,0 +1,63 @@
+package app.covidshield.module
+
+import app.covidshield.extensions.toWritableArray
+import app.covidshield.extensions.toWritableMap
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.ReadableMap
+import com.google.android.gms.nearby.Nearby
+
+class ExposureNotificationModule(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
+
+    private val exposureNotificationClient = Nearby.getExposureNotificationClient(context.applicationContext)
+
+    override fun getName(): String = "ExposureNotification"
+
+    @ReactMethod
+    fun start(promise: Promise) {
+        promise.resolve(null)
+    }
+
+    @ReactMethod
+    fun stop(promise: Promise) {
+        exposureNotificationClient.stop()
+            .addOnCompleteListener {
+                promise.resolve(null)
+            }
+    }
+
+    @ReactMethod
+    fun resetAllData(promise: Promise) {
+        promise.resolve(null)
+    }
+
+    @ReactMethod
+    fun getStatus(promise: Promise) {
+        exposureNotificationClient.isEnabled
+            .addOnSuccessListener {
+                val status = if (it) Status.ACTIVE else Status.DISABLED
+                promise.resolve(status.value)
+            }
+            .addOnFailureListener {
+                promise.resolve(Status.DISABLED.value)
+            }
+    }
+
+    @ReactMethod
+    fun detectExposure(configuration: ReadableMap, diagnosisKeysURLs: ReadableArray, promise: Promise) {
+        promise.resolve(emptyMap<String, String>().toWritableMap())
+    }
+
+    @ReactMethod
+    fun getExposureInformation(keys: ReadableArray, promise: Promise) {
+        promise.resolve(emptyList<Any>().toWritableArray())
+    }
+}
+
+private enum class Status(val value: String) {
+    ACTIVE("active"),
+    DISABLED("disabled")
+}