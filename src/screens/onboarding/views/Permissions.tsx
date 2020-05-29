02fefefd38a461d7514b404f0d445644e8897be4
Henry Tao <henry.tao@shopify.com>
2020-05-28 21:42:39 -0400

    Fix button and background in onboarding and tutorial screen
    Fix lint

--- src/screens/onboarding/views/Permissions.tsx
+++ src/screens/onboarding/views/Permissions.tsx
@@ -1,34 +1,31 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
 import {Box, Text} from 'components';
 import {useI18n} from '@shopify/react-i18n';
 
 export const Permissions = () => {
   const [i18n] = useI18n();
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <Box flex={1} paddingHorizontal="xl">
-        <Box paddingHorizontal="l" marginTop="m">
-          <Text variant="bodyTitle" color="overlayBodyText" marginHorizontal="l" marginBottom="l" textAlign="center">
-            {i18n.translate('OnboardingPermissions.Title')}
-          </Text>
-        </Box>
-        <Box marginBottom="l">
-          <Text variant="bodyText" color="overlayBodyText" textAlign="center">
-            {i18n.translate('OnboardingPermissions.Body')}
-          </Text>
-        </Box>
-        <Box marginBottom="l">
-          <Text variant="bodyText" color="overlayBodyText" textAlign="center">
-            {i18n.translate('OnboardingPermissions.Body2')}
-          </Text>
-        </Box>
-        <Box marginBottom="l">
-          <Text variant="bodyText" color="overlayBodyText" textAlign="center">
-            {i18n.translate('OnboardingPermissions.Body3')}
-          </Text>
-        </Box>
+    <Box flex={1} paddingHorizontal="xl" justifyContent="center">
+      <Box paddingHorizontal="l" marginTop="m">
+        <Text variant="bodyTitle" color="overlayBodyText" marginHorizontal="l" marginBottom="l" textAlign="center">
+          {i18n.translate('OnboardingPermissions.Title')}
+        </Text>
       </Box>
-    </ScrollView>
+      <Box marginBottom="l">
+        <Text variant="bodyText" color="overlayBodyText" textAlign="center">
+          {i18n.translate('OnboardingPermissions.Body')}
+        </Text>
+      </Box>
+      <Box marginBottom="l">
+        <Text variant="bodyText" color="overlayBodyText" textAlign="center">
+          {i18n.translate('OnboardingPermissions.Body2')}
+        </Text>
+      </Box>
+      <Box marginBottom="l">
+        <Text variant="bodyText" color="overlayBodyText" textAlign="center">
+          {i18n.translate('OnboardingPermissions.Body3')}
+        </Text>
+      </Box>
+    </Box>
   );
 };
--- src/screens/onboarding/views/Start.tsx
+++ src/screens/onboarding/views/Start.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
 import {Box, Text, Button, Icon} from 'components';
 import {useNavigation} from '@react-navigation/native';
 import {useI18n} from '@shopify/react-i18n';
@@ -8,33 +7,31 @@ export const Start = () => {
   const [i18n] = useI18n();
   const navigation = useNavigation();
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <Box flex={1} paddingHorizontal="xl">
-        <Box paddingHorizontal="l" marginTop="m">
-          <Text variant="bodyTitle" color="overlayBodyText" marginHorizontal="l" marginBottom="l" textAlign="center">
-            {i18n.translate('OnboardingStart.Title')}
-          </Text>
-        </Box>
-        <Box flexDirection="row" alignItems="center" marginBottom="l">
-          <Icon size={30} name="icon-notifications" />
-          <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
-            {i18n.translate('OnboardingStart.Body1')}
-          </Text>
-        </Box>
-        <Box flexDirection="row" alignItems="center" marginBottom="l">
-          <Icon size={30} name="icon-notify" />
-          <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
-            {i18n.translate('OnboardingStart.Body2')}
-          </Text>
-        </Box>
-        <Box flexDirection="row" justifyContent="space-around" alignItems="center" marginBottom="l">
-          <Button
-            text={i18n.translate('OnboardingStart.TutorialAction')}
-            variant="bigFlatWhite"
-            onPress={() => navigation.navigate('Tutorial')}
-          />
-        </Box>
+    <Box flex={1} paddingHorizontal="xl" justifyContent="center">
+      <Box paddingHorizontal="l" marginTop="m">
+        <Text variant="bodyTitle" color="overlayBodyText" marginHorizontal="l" marginBottom="l" textAlign="center">
+          {i18n.translate('OnboardingStart.Title')}
+        </Text>
       </Box>
-    </ScrollView>
+      <Box flexDirection="row" alignItems="center" marginBottom="l">
+        <Icon size={30} name="icon-notifications" />
+        <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
+          {i18n.translate('OnboardingStart.Body1')}
+        </Text>
+      </Box>
+      <Box flexDirection="row" alignItems="center" marginBottom="l">
+        <Icon size={30} name="icon-notify" />
+        <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
+          {i18n.translate('OnboardingStart.Body2')}
+        </Text>
+      </Box>
+      <Box flexDirection="row" justifyContent="space-around" alignItems="center" marginBottom="l">
+        <Button
+          text={i18n.translate('OnboardingStart.TutorialAction')}
+          variant="bigFlatWhite"
+          onPress={() => navigation.navigate('Tutorial')}
+        />
+      </Box>
+    </Box>
   );
 };
