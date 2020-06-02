9dfee487288a38f384332e6bedde85759ff04b21
Armon Khosravi <armon.khosravi@shopify.com>
2020-06-02 00:34:26 -0400

    update icons to be png

--- src/screens/regionPicker/RegionPicker.tsx
+++ src/screens/regionPicker/RegionPicker.tsx
@@ -1,23 +1,23 @@
 import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {Box, Text, Icon} from 'components';
-import {StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
+import {Box, Text, Icon, Button} from 'components';
+import {StyleSheet, TouchableOpacity, ScrollView, Image} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useStorage} from 'services/StorageService';
 import {useI18n} from '@shopify/react-i18n';
-import AbFlag from 'assets/flags/ab-flag.svg';
-import BcFlag from 'assets/flags/bc-flag.svg';
-import MbFlag from 'assets/flags/mb-flag.svg';
-import NbFlag from 'assets/flags/nb-flag.svg';
-import NlFlag from 'assets/flags/nl-flag.svg';
-import NtFlag from 'assets/flags/nt-flag.svg';
-import NsFlag from 'assets/flags/ns-flag.svg';
-import NuFlag from 'assets/flags/nu-flag.svg';
-import OnFlag from 'assets/flags/on-flag.svg';
-import PeFlag from 'assets/flags/pe-flag.svg';
-import QcFlag from 'assets/flags/qc-flag.svg';
-import SkFlag from 'assets/flags/sk-flag.svg';
-import YtFlag from 'assets/flags/yt-flag.svg';
+import AbFlag from 'assets/flags/ab-flag.png';
+import BcFlag from 'assets/flags/bc-flag.png';
+import MbFlag from 'assets/flags/mb-flag.png';
+import NbFlag from 'assets/flags/nb-flag.png';
+import NlFlag from 'assets/flags/nl-flag.png';
+import NtFlag from 'assets/flags/nt-flag.png';
+import NsFlag from 'assets/flags/ns-flag.png';
+import NuFlag from 'assets/flags/nu-flag.png';
+import OnFlag from 'assets/flags/on-flag.png';
+import PeFlag from 'assets/flags/pe-flag.png';
+import QcFlag from 'assets/flags/qc-flag.png';
+import SkFlag from 'assets/flags/sk-flag.png';
+import YtFlag from 'assets/flags/yt-flag.png';
 import {Region} from 'shared/Region';
 
 interface RegionItemProps {
@@ -44,13 +44,13 @@ const items: Omit<RegionItemProps, 'onPress' | 'selected'>[] = [
   {code: 'YT', flagIcon: YtFlag, name: 'Yukon'},
 ];
 
-const RegionItem_ = ({code, onPress, name, flagIcon: FlagIcon, selected}: RegionItemProps) => (
+const RegionItem_ = ({code, onPress, name, flagIcon, selected}: RegionItemProps) => (
   <>
     <TouchableOpacity onPress={() => onPress(code)}>
       <Box paddingVertical="s" flexDirection="row" alignContent="center" justifyContent="space-between">
         <Box flexDirection="row" alignItems="center" paddingVertical="s">
-          <FlagIcon width={50} height={20} />
-          <Text variant="bodyText" color="overlayBodyText" marginHorizontal="s">
+          <Image source={flagIcon} style={styles.flag} />
+          <Text variant="bodyText" color="overlayBodyText" marginHorizontal="m">
             {name}
           </Text>
         </Box>
@@ -66,7 +66,7 @@ export const RegionPickerScreen = () => {
   const [i18n] = useI18n();
   const {setRegion: persistRegion} = useStorage();
   const [selectedRegion, setSelectedRegion] = useState<Region>('None');
-  const navigation = useNavigation();
+  const {navigate} = useNavigation();
 
   return (
     <Box flex={1} backgroundColor="overlayBackground">
@@ -94,6 +94,21 @@ export const RegionPickerScreen = () => {
             </Box>
           </Box>
         </ScrollView>
+        <Box
+          backgroundColor="overlayBackground"
+          padding="m"
+          borderTopColor="infoBlockNeutralBackground"
+          borderTopWidth={1}
+        >
+          <Button
+            text={selectedRegion === 'None' ? 'Skip' : 'Get started'}
+            variant={selectedRegion === 'None' ? 'bigHollow' : 'bigFlat'}
+            onPress={async () => {
+              await persistRegion(selectedRegion);
+              navigate('OnboardingTutorial');
+            }}
+          />
+        </Box>
       </SafeAreaView>
     </Box>
   );
@@ -102,5 +117,10 @@ export const RegionPickerScreen = () => {
 const styles = StyleSheet.create({
   flex: {
     flex: 1,
+  },
+  flag: {
+    width: 40,
+    height: 22,
+    resizeMode: 'stretch',
   },
 });
