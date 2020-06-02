8b6f52ce171651ddec0bb6e39de2ab5b05557b23
Armon Khosravi <armon.khosravi@shopify.com>
2020-06-02 00:20:09 -0400

    render flags

--- src/screens/regionPicker/RegionPicker.tsx
+++ src/screens/regionPicker/RegionPicker.tsx
@@ -5,40 +5,55 @@ import {StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useStorage} from 'services/StorageService';
 import {useI18n} from '@shopify/react-i18n';
-import OnboardingBg from 'assets/onboarding-bg.svg';
+import AbFlag from 'assets/flags/ab-flag.svg';
+import BcFlag from 'assets/flags/bc-flag.svg';
+import MbFlag from 'assets/flags/mb-flag.svg';
+import NbFlag from 'assets/flags/nb-flag.svg';
+import NlFlag from 'assets/flags/nl-flag.svg';
+import NtFlag from 'assets/flags/nt-flag.svg';
+import NsFlag from 'assets/flags/ns-flag.svg';
+import NuFlag from 'assets/flags/nu-flag.svg';
+import OnFlag from 'assets/flags/on-flag.svg';
+import PeFlag from 'assets/flags/pe-flag.svg';
+import QcFlag from 'assets/flags/qc-flag.svg';
+import SkFlag from 'assets/flags/sk-flag.svg';
+import YtFlag from 'assets/flags/yt-flag.svg';
 import {Region} from 'shared/Region';
 
 interface RegionItemProps {
   code: Region;
-  icon: any;
+  flagIcon: any;
   name: string;
   selected: boolean;
   onPress: (code: Region) => void;
 }
 
 const items: Omit<RegionItemProps, 'onPress' | 'selected'>[] = [
-  {code: 'AB', icon: '', name: 'Alberta'},
-  {code: 'BC', icon: '', name: 'British Columbia'},
-  {code: 'MB', icon: '', name: 'Manitoba'},
-  {code: 'NB', icon: '', name: 'New Brunswick'},
-  {code: 'NL', icon: '', name: 'Newfoundland and Labrador'},
-  {code: 'NT', icon: '', name: 'Northwest Territories'},
-  {code: 'NS', icon: '', name: 'Nova Scotia'},
-  {code: 'NU', icon: '', name: 'Nunavut'},
-  {code: 'ON', icon: '', name: 'Ontario'},
-  {code: 'PE', icon: '', name: 'Prince Edward Island'},
-  {code: 'QC', icon: '', name: 'Quebec'},
-  {code: 'SK', icon: '', name: 'Saskatchewan'},
-  {code: 'YT', icon: '', name: 'Yukon'},
+  {code: 'AB', flagIcon: AbFlag, name: 'Alberta'},
+  {code: 'BC', flagIcon: BcFlag, name: 'British Columbia'},
+  {code: 'MB', flagIcon: MbFlag, name: 'Manitoba'},
+  {code: 'NB', flagIcon: NbFlag, name: 'New Brunswick'},
+  {code: 'NL', flagIcon: NlFlag, name: 'Newfoundland and Labrador'},
+  {code: 'NT', flagIcon: NtFlag, name: 'Northwest Territories'},
+  {code: 'NS', flagIcon: NsFlag, name: 'Nova Scotia'},
+  {code: 'NU', flagIcon: NuFlag, name: 'Nunavut'},
+  {code: 'ON', flagIcon: OnFlag, name: 'Ontario'},
+  {code: 'PE', flagIcon: PeFlag, name: 'Prince Edward Island'},
+  {code: 'QC', flagIcon: QcFlag, name: 'Quebec'},
+  {code: 'SK', flagIcon: SkFlag, name: 'Saskatchewan'},
+  {code: 'YT', flagIcon: YtFlag, name: 'Yukon'},
 ];
 
-const RegionItem_ = ({code, onPress, name, icon, selected}: RegionItemProps) => (
+const RegionItem_ = ({code, onPress, name, flagIcon: FlagIcon, selected}: RegionItemProps) => (
   <>
     <TouchableOpacity onPress={() => onPress(code)}>
       <Box paddingVertical="s" flexDirection="row" alignContent="center" justifyContent="space-between">
-        <Text variant="bodyText" marginVertical="s" color="overlayBodyText">
-          {name}
-        </Text>
+        <Box flexDirection="row" alignItems="center" paddingVertical="s">
+          <FlagIcon width={50} height={20} />
+          <Text variant="bodyText" color="overlayBodyText" marginHorizontal="s">
+            {name}
+          </Text>
+        </Box>
         <Box alignSelf="center">{selected && <Icon size={32} name="icon-check" />}</Box>
       </Box>
     </TouchableOpacity>
