4c5480c7ab8eda103be6d4b7d206573642ccffe8
Henry Tao <henry.tao@shopify.com>
2020-06-03 19:54:16 -0400

    Add ScrollView back for onboarding

--- src/screens/onboarding/views/Permissions.tsx
+++ src/screens/onboarding/views/Permissions.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
 import {Box, Text} from 'components';
 import {useI18n} from '@shopify/react-i18n';
 
 export const Permissions = () => {
   const [i18n] = useI18n();
   return (
-    <Box flex={1} paddingHorizontal="xl" justifyContent="center">
-      <Box paddingHorizontal="l" marginTop="m">
-        <Text variant="bodyTitle" color="overlayBodyText" marginHorizontal="l" marginBottom="l" textAlign="center">
-          {i18n.translate('OnboardingPermissions.Title')}
-        </Text>
+    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.content}>
+      <Box paddingHorizontal="xl">
+        <Box paddingHorizontal="l" marginTop="m">
+          <Text variant="bodyTitle" color="overlayBodyText" marginHorizontal="l" marginBottom="l" textAlign="center">
+            {i18n.translate('OnboardingPermissions.Title')}
+          </Text>
+        </Box>
+        <Box marginBottom="l">
+          <Text variant="bodyText" color="overlayBodyText" textAlign="center">
+            {i18n.translate('OnboardingPermissions.Body')}
+          </Text>
+        </Box>
+        <Box marginBottom="l">
+          <Text variant="bodyText" color="overlayBodyText" textAlign="center">
+            {i18n.translate('OnboardingPermissions.Body2')}
+          </Text>
+        </Box>
+        <Box marginBottom="l">
+          <Text variant="bodyText" color="overlayBodyText" textAlign="center">
+            {i18n.translate('OnboardingPermissions.Body3')}
+          </Text>
+        </Box>
       </Box>
-      <Box marginBottom="l">
-        <Text variant="bodyText" color="overlayBodyText" textAlign="center">
-          {i18n.translate('OnboardingPermissions.Body')}
-        </Text>
-      </Box>
-      <Box marginBottom="l">
-        <Text variant="bodyText" color="overlayBodyText" textAlign="center">
-          {i18n.translate('OnboardingPermissions.Body2')}
-        </Text>
-      </Box>
-      <Box marginBottom="l">
-        <Text variant="bodyText" color="overlayBodyText" textAlign="center">
-          {i18n.translate('OnboardingPermissions.Body3')}
-        </Text>
-      </Box>
-    </Box>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+});
--- src/screens/onboarding/views/Start.tsx
+++ src/screens/onboarding/views/Start.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
 import {Box, Text, Button, Icon} from 'components';
 import {useNavigation} from '@react-navigation/native';
 import {useI18n} from '@shopify/react-i18n';
@@ -7,31 +8,40 @@ export const Start = () => {
   const [i18n] = useI18n();
   const navigation = useNavigation();
   return (
-    <Box flex={1} paddingHorizontal="xl" justifyContent="center">
-      <Box paddingHorizontal="l" marginTop="m">
-        <Text variant="bodyTitle" color="overlayBodyText" marginHorizontal="l" marginBottom="l" textAlign="center">
-          {i18n.translate('OnboardingStart.Title')}
-        </Text>
+    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.content}>
+      <Box paddingHorizontal="xl">
+        <Box paddingHorizontal="l" marginTop="m">
+          <Text variant="bodyTitle" color="overlayBodyText" marginHorizontal="l" marginBottom="l" textAlign="center">
+            {i18n.translate('OnboardingStart.Title')}
+          </Text>
+        </Box>
+        <Box flexDirection="row" alignItems="center" marginBottom="l">
+          <Icon size={30} name="icon-notifications" />
+          <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
+            {i18n.translate('OnboardingStart.Body1')}
+          </Text>
+        </Box>
+        <Box flexDirection="row" alignItems="center" marginBottom="l">
+          <Icon size={30} name="icon-notify" />
+          <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
+            {i18n.translate('OnboardingStart.Body2')}
+          </Text>
+        </Box>
+        <Box flexDirection="row" justifyContent="space-around" alignItems="center" marginBottom="l">
+          <Button
+            text={i18n.translate('OnboardingStart.TutorialAction')}
+            variant="bigFlatWhite"
+            onPress={() => navigation.navigate('Tutorial')}
+          />
+        </Box>
       </Box>
-      <Box flexDirection="row" alignItems="center" marginBottom="l">
-        <Icon size={30} name="icon-notifications" />
-        <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
-          {i18n.translate('OnboardingStart.Body1')}
-        </Text>
-      </Box>
-      <Box flexDirection="row" alignItems="center" marginBottom="l">
-        <Icon size={30} name="icon-notify" />
-        <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
-          {i18n.translate('OnboardingStart.Body2')}
-        </Text>
-      </Box>
-      <Box flexDirection="row" justifyContent="space-around" alignItems="center" marginBottom="l">
-        <Button
-          text={i18n.translate('OnboardingStart.TutorialAction')}
-          variant="bigFlatWhite"
-          onPress={() => navigation.navigate('Tutorial')}
-        />
-      </Box>
-    </Box>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+});
