7cb15ec55578c6e9fecdaf79b4bad7237cf66232
Tim Arney <tim@line37.com>
2020-06-18 19:46:45 -0400

    format text

--- src/screens/home/views/NoExposureView.tsx
+++ src/screens/home/views/NoExposureView.tsx
@@ -4,8 +4,11 @@ import {Text, Box, Button, LastCheckedDisplay} from 'components';
 import {useI18n} from '@shopify/react-i18n';
 import {useStorage} from 'services/StorageService';
 import {getRegionCase} from 'shared/RegionLogic';
+import {Theme} from 'shared/theme';
 
 import {BaseHomeView} from '../components/BaseHomeView';
+
+type Color = keyof Theme['colors'];
 
 export const NoExposureView = () => {
   const [i18n] = useI18n();
@@ -15,8 +18,16 @@ export const NoExposureView = () => {
 
   // console.log('the region', regionCase);
 
-  const titleTextColor = 'bodyTitleWhite';
-  const bodyTextColor = 'bodyTextWhite';
+  let titleTextColor = 'bodyTitleWhite';
+  let bodyTextColor = 'bodyTextWhite';
+
+  if (regionCase === 'regionCovered') {
+    titleTextColor = 'successText';
+    bodyTextColor = 'bodyText';
+  }
+
+  const titleColor: Color = titleTextColor as Color;
+  const textColor: Color = bodyTextColor as Color;
 
   const onAction = useCallback(() => {
     Linking.openURL(i18n.translate('Home.GuidanceUrl')).catch(err => console.error('An error occurred', err));
@@ -37,14 +48,14 @@ export const NoExposureView = () => {
   return (
     // note you can add an icon i.e. <BaseHomeView iconName="icon-offline>
     <BaseHomeView>
-      <Text variant="bodyTitle" color={titleTextColor} marginBottom="l" accessibilityRole="header">
+      <Text variant="bodyTitle" color={titleColor} marginBottom="l" accessibilityRole="header">
         {i18n.translate(regionTranslationsTitle[regionCase])}
       </Text>
-      <Text variant="bodyText" color={bodyTextColor} marginBottom="l">
+      <Text variant="bodyText" color={textColor} marginBottom="l">
         {i18n.translate(regionTranslationsBody[regionCase])}
       </Text>
 
-      <LastCheckedDisplay />
+      <LastCheckedDisplay textDark />
 
       <Box alignSelf="stretch" marginTop="l" marginBottom="l">
         <Button text={i18n.translate('Home.How')} variant="opaqueGrey" externalLink onPress={onAction} />
