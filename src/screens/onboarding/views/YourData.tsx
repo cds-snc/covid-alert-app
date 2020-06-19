ee24cd3ab1066dc19a735249aadd626d08cf31e3
Tim Arney <tim@line37.com>
2020-06-19 09:15:37 -0400

    lint

--- src/screens/onboarding/views/YourData.tsx
+++ src/screens/onboarding/views/YourData.tsx
@@ -20,19 +20,31 @@ export const YourData = () => {
           </Text>
         </Box>
         <Box flexDirection="row" marginBottom="l">
-          <Text marginRight="s">❌</Text>
+          <Text marginRight="s">
+            <span role="img" aria-label={i18n.translate('X')}>
+              ❌
+            </span>
+          </Text>
           <Text variant="bodyText" color="overlayBodyText" marginRight="m">
             {i18n.translate('YourData.Body1')}
           </Text>
         </Box>
         <Box flexDirection="row" marginBottom="l">
-          <Text marginRight="s">❌</Text>
+          <Text marginRight="s">
+            <span role="img" aria-label={i18n.translate('X')}>
+              ❌
+            </span>
+          </Text>
           <Text variant="bodyText" color="overlayBodyText" marginRight="m">
             {i18n.translate('YourData.Body2')}
           </Text>
         </Box>
         <Box flexDirection="row" alignItems="center" marginBottom="l">
-          <Text marginRight="s">❌</Text>
+          <Text marginRight="s">
+            <span role="img" aria-label={i18n.translate('X')}>
+              ❌
+            </span>
+          </Text>
           <Text variant="bodyText" color="overlayBodyText" marginRight="m">
             {i18n.translate('YourData.Body3')}
           </Text>
