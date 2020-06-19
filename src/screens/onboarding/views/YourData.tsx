209eabf5e106219160aa4f858b0f2eaa7730f88c
smcmurtry <smcmurtry@gmail.com>
2020-06-19 09:29:21 -0600

    removed emoji

--- src/screens/onboarding/views/YourData.tsx
+++ src/screens/onboarding/views/YourData.tsx
@@ -20,31 +20,16 @@ export const YourData = () => {
           </Text>
         </Box>
         <Box flexDirection="row" marginBottom="l">
-          <Text marginRight="s">
-            <span role="img" aria-label={i18n.translate('X')}>
-              ❌
-            </span>
-          </Text>
           <Text variant="bodyText" color="overlayBodyText" marginRight="m">
             {i18n.translate('YourData.Body1')}
           </Text>
         </Box>
         <Box flexDirection="row" marginBottom="l">
-          <Text marginRight="s">
-            <span role="img" aria-label={i18n.translate('X')}>
-              ❌
-            </span>
-          </Text>
           <Text variant="bodyText" color="overlayBodyText" marginRight="m">
             {i18n.translate('YourData.Body2')}
           </Text>
         </Box>
         <Box flexDirection="row" alignItems="center" marginBottom="l">
-          <Text marginRight="s">
-            <span role="img" aria-label={i18n.translate('X')}>
-              ❌
-            </span>
-          </Text>
           <Text variant="bodyText" color="overlayBodyText" marginRight="m">
             {i18n.translate('YourData.Body3')}
           </Text>
