143f9f9767f3e42ec781c5cbfe38b7d11c0890d1
Tim Arney <tim@line37.com>
2020-06-21 04:58:54 -0400

    remove LottieView

--- src/screens/home/components/BaseHomeView.tsx
+++ src/screens/home/components/BaseHomeView.tsx
@@ -1,69 +1,21 @@
-import React, {useRef, useEffect} from 'react';
+import React from 'react';
 import {StyleSheet, ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import LottieView from 'lottie-react-native';
-import {useReduceMotionPreference} from 'shared/useReduceMotionPreference';
 import {Box, Header, Icon, IconName} from 'components';
-import {useOrientation} from 'shared/useOrientation';
 
 interface BaseHomeViewProps {
   children?: React.ReactNode;
-  animationSource?: string;
-  animationPauseFrame?: number;
   iconName?: IconName;
 }
 
-export const BaseHomeView = ({children, animationSource, animationPauseFrame, iconName}: BaseHomeViewProps) => {
-  const {
-    orientation,
-    scaledSize: {width: viewportWidth, height: viewportHeight},
-  } = useOrientation();
-  const prefersReducedMotion = useReduceMotionPreference();
-  const animationRef: React.Ref<LottieView> = useRef(null);
-
-  useEffect(() => {
-    // need to stop if user prefers reduced animations
-    if (prefersReducedMotion) {
-      if (animationPauseFrame) {
-        animationRef.current?.play(animationPauseFrame, animationPauseFrame);
-      } else {
-        animationRef.current?.reset();
-        animationRef.current?.pause();
-      }
-    }
-  }, [prefersReducedMotion, animationPauseFrame]);
-
+export const BaseHomeView = ({children, iconName}: BaseHomeViewProps) => {
   return (
     <SafeAreaView>
       <Header />
-      <ScrollView
-        style={styles.flex}
-        contentContainerStyle={[
-          styles.scrollContainer,
-          animationSource && orientation === 'portrait' ? styles.scrollContainerWithAnimation : null,
-        ]}
-        bounces={false}
-      >
+      <ScrollView style={styles.flex} contentContainerStyle={[styles.scrollContainer]} bounces={false}>
         <Box marginTop="xxl">
           <Icon name={iconName} size={110} />
         </Box>
-
-        {animationSource && orientation === 'portrait' && (
-          <Box marginBottom="m">
-            <LottieView
-              ref={animationRef}
-              style={{
-                ...styles.animationBase,
-                width: viewportWidth * 2,
-                height: viewportHeight / 2,
-              }}
-              source={animationSource}
-              // don't play if user prefers reduced animations
-              autoPlay={!prefersReducedMotion}
-              loop={!prefersReducedMotion}
-            />
-          </Box>
-        )}
         <Box flex={1} alignItems="flex-start" justifyContent="flex-end" marginHorizontal="xl">
           {children}
         </Box>
