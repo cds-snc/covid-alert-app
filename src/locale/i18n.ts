0c11fc60ccc83bda593bd6e2fccebd1c96b42e0a
Henry Tao <henry.tao@shopify.com>
2020-06-21 16:55:59 -0400

    Fix EN bridge on Android related to Bluetooth

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -14,7 +14,7 @@ export const getBackgroundI18n = async (forceLocale?: string) => {
       const i18nManager = new I18nManager({
         locale,
         onError(error) {
-          console.log(error.message);
+          console.log('>>> i18N', error);
         },
       });
       const translations = (locale: string) => LOCALES[locale];
