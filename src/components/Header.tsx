3d0ef076a1a6119953fa276db662e203bdb64106
Tim Arney <tim@line37.com>
2020-06-23 09:39:31 -0400

    add header .svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,7 @@
 import React, {useCallback} from 'react';
-import {TouchableWithoutFeedback} from 'react-native';
+import {TouchableWithoutFeedback, StyleSheet} from 'react-native';
 import {useNavigation, DrawerActions} from '@react-navigation/native';
-import {useI18n} from '@shopify/react-i18n';
 import {Theme} from 'shared/theme';
-
 import {Box} from './Box';
 import {Icon} from './Icon';
 import {Text} from './Text';
@@ -12,30 +10,30 @@ export interface HeaderProps {
   isOverlay?: boolean;
 }
 
-type Color = keyof Theme['colors'];
-
 export const Header = () => {
-  const [i18n] = useI18n();
   const navigation = useNavigation();
-
-  const textColor = 'bodyText';
-
-  const headerTextColor: Color = textColor as Color;
 
   const onLogoPress = useCallback(() => {
     navigation.dispatch(DrawerActions.openDrawer());
   }, [navigation]);
   return (
     <TouchableWithoutFeedback onPress={onLogoPress}>
-      <Box flexDirection="row" alignItems="center" justifyContent="center" marginBottom="l">
-        <Box marginHorizontal="s">
-          <Icon size={20} name="maple-leaf" />
-        </Box>
-
-        <Text variant="homeHeader" color={headerTextColor}>
-          {i18n.translate('Home.AppName')}
-        </Text>
+      <Box
+        maxHeight={30}
+        // borderWidth={2}
+        // borderColor="bodyText"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Icon size={129} name="stop-covid" />
       </Box>
     </TouchableWithoutFeedback>
   );
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    marginTop: -10,
+  },
+});
