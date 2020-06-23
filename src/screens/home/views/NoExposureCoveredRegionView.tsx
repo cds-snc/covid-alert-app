661588c8bfa92661ec8c4797dd936873ba1345d5
Tim Arney <tim@line37.com>
2020-06-23 18:47:27 -0400

    updated text + buttons

--- src/screens/home/views/NoExposureCoveredRegionView.tsx
+++ src/screens/home/views/NoExposureCoveredRegionView.tsx
@@ -2,35 +2,45 @@ import React, {useCallback} from 'react';
 import {Linking} from 'react-native';
 import {Text, Box, ButtonMultiline, LastCheckedDisplay} from 'components';
 import {useI18n} from '@shopify/react-i18n';
+import {useStorage} from 'services/StorageService';
 
 import {BaseHomeView} from '../components/BaseHomeView';
 
 export const NoExposureCoveredRegionView = () => {
   const [i18n] = useI18n();
 
-  const onAction = useCallback(() => {
-    Linking.openURL(i18n.translate('Home.GuidanceUrl')).catch(err => console.error('An error occurred', err));
-  }, [i18n]);
+  const {region} = useStorage();
+
+  const onActionGuidance = useCallback(() => {
+    let url = i18n.translate(`RegionalGuidanceURL.CA`);
+    if (region !== undefined && region !== 'None') {
+      const regionalURL = i18n.translate(`RegionalGuidanceURL.${region}`);
+      if (regionalURL !== '') {
+        url = regionalURL;
+      }
+    }
+    Linking.openURL(url).catch(err => console.error('An error occurred', err));
+  }, [i18n, region]);
 
   return (
     // note you can add an icon i.e. <BaseHomeView iconName="icon-offline>
     <BaseHomeView iconName="thumbs-up">
       <Text variant="bodyTitle" color="bodyText" marginBottom="l" accessibilityRole="header">
-        {i18n.translate('Home.NoExposureDetected.RegionCoveredTitle')}
+        {i18n.translate('Home.NoExposureDetected.RegionCovered.Title')}
       </Text>
       <Text variant="bodyText" color="bodyText" marginBottom="l">
-        {i18n.translate('Home.NoExposureDetected.RegionCoveredBody')}
+        {i18n.translate('Home.NoExposureDetected.RegionCovered.Body')}
       </Text>
 
       <LastCheckedDisplay textDark />
 
       <Box alignSelf="stretch" marginTop="s" marginBottom="l">
         <ButtonMultiline
-          text={i18n.translate('Home.How')}
-          text1={i18n.translate('Home.CTA')}
+          text={i18n.translate('Home.NoExposureDetected.RegionCovered.BtnText1')}
+          text1={i18n.translate('Home.NoExposureDetected.RegionCovered.BtnText2')}
           variant="bigFlat"
           internalLink
-          onPress={onAction}
+          onPress={onActionGuidance}
         />
       </Box>
     </BaseHomeView>
--- src/screens/home/views/NoExposureUncoveredRegionView.tsx
+++ src/screens/home/views/NoExposureUncoveredRegionView.tsx
@@ -1,31 +1,48 @@
 import React, {useCallback} from 'react';
 import {Linking} from 'react-native';
-import {Text, Box, Button, LastCheckedDisplay} from 'components';
+import {Text, Box, ButtonMultiline, LastCheckedDisplay} from 'components';
 import {useI18n} from '@shopify/react-i18n';
-
+import {useStorage} from 'services/StorageService';
 import {BaseHomeView} from '../components/BaseHomeView';
 
 export const NoExposureUncoveredRegionView = () => {
   const [i18n] = useI18n();
 
-  const onAction = useCallback(() => {
-    Linking.openURL(i18n.translate('Home.GuidanceUrl')).catch(err => console.error('An error occurred', err));
-  }, [i18n]);
+  const {region} = useStorage();
+
+  const onActionGuidance = useCallback(() => {
+    let url = i18n.translate(`RegionalGuidanceURL.CA`);
+    if (region !== undefined && region !== 'None') {
+      const regionalURL = i18n.translate(`RegionalGuidanceURL.${region}`);
+      if (regionalURL !== '') {
+        url = regionalURL;
+      }
+    }
+    Linking.openURL(url).catch(err => console.error('An error occurred', err));
+  }, [i18n, region]);
 
   return (
     // note you can add an icon i.e. <BaseHomeView iconName="icon-offline>
     <BaseHomeView>
       <Text variant="bodyTitle" color="bodyText" marginBottom="l" accessibilityRole="header">
-        {i18n.translate('Home.NoExposureDetected.RegionNotCoveredTitle')}
+        {i18n.translate('Home.NoExposureDetected.RegionNotCovered.Title')}
       </Text>
       <Text variant="bodyText" color="bodyText" marginBottom="l">
-        {i18n.translate('Home.NoExposureDetected.RegionNotCoveredBody')}
+        {i18n.translate('Home.NoExposureDetected.RegionNotCovered.Body')}
       </Text>
 
       <LastCheckedDisplay textDark />
 
       <Box alignSelf="stretch" marginBottom="l">
-        <Button text={i18n.translate('Home.How')} variant="bigFlat" externalLink onPress={onAction} />
+        <Box alignSelf="stretch" marginTop="s" marginBottom="xl">
+          <ButtonMultiline
+            text={i18n.translate('Home.NoExposureDetected.RegionNotCovered.DiagnosedBtnText1')}
+            text1={i18n.translate('Home.NoExposureDetected.RegionNotCovered.DiagnosedBtnText2')}
+            variant="bigFlat"
+            internalLink
+            onPress={onActionGuidance}
+          />
+        </Box>
       </Box>
     </BaseHomeView>
   );
