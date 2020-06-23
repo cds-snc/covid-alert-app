e9f3cd806664beaed52ed7ff94f217f3e9908420
smcmurtry <smcmurtry@gmail.com>
2020-06-23 09:45:36 -0600

    fixed stroke width on icon, alignment

--- src/screens/onboarding/views/Start.tsx
+++ src/screens/onboarding/views/Start.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
-import {Box, Text, Button, Icon} from 'components';
-import {useNavigation} from '@react-navigation/native';
+import {Box, Text, Icon} from 'components';
 import {useI18n} from '@shopify/react-i18n';
 
 export const Start = () => {
   const [i18n] = useI18n();
-  const navigation = useNavigation();
   return (
     <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.content}>
       <Box paddingHorizontal="xl">
@@ -15,14 +13,18 @@ export const Start = () => {
             {i18n.translate('OnboardingStart.Title')}
           </Text>
         </Box>
-        <Box flexDirection="row" alignItems="center" marginBottom="l">
-          <Icon size={20} name="icon-notifications" />
+        <Box flexDirection="row" marginBottom="l">
+          <Box marginTop="xxxs">
+            <Icon size={20} name="icon-notifications" />
+          </Box>
           <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
             {i18n.translate('OnboardingStart.Body1')}
           </Text>
         </Box>
-        <Box flexDirection="row" alignItems="center" marginBottom="l">
-          <Icon size={20} name="icon-x" />
+        <Box flexDirection="row" marginBottom="l">
+          <Box marginTop="xxs">
+            <Icon size={20} name="icon-x" />
+          </Box>
           <Text variant="bodyText" color="overlayBodyText" marginLeft="m" marginRight="m">
             {i18n.translate('OnboardingStart.Body2')}
           </Text>
