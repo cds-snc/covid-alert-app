ebca3ccf71b81b983400f8ab295e61f3c764eadf
smcmurtry <smcmurtry@gmail.com>
2020-06-24 00:01:49 -0600

    added a step1 screen

--- src/screens/datasharing/DataSharing.tsx
+++ src/screens/datasharing/DataSharing.tsx
@@ -6,6 +6,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {useI18n} from '@shopify/react-i18n';
 import {useExposureStatus} from 'services/ExposureNotificationService';
 
+import {Step1} from './views/Step1';
 import {FormView} from './views/FormView';
 import {ConsentView} from './views/ConsentView';
 
@@ -16,8 +17,11 @@ export const DataSharingScreen = () => {
   const [exposureStatus] = useExposureStatus();
   const [codeValue, setCodeValue] = useState('');
   const handleChange = useCallback(text => setCodeValue(text), []);
+  const acceptStep1 = useCallback(() => setIsConfirmedStep1(true), []);
+
   // if keySubmissionStatus is None we need the 1-time code, otherwise we should go right to consent
   const [isVerified, setIsVerified] = useState(exposureStatus.type === 'diagnosed');
+  const [isConfirmedStep1, setIsConfirmedStep1] = useState(false);
   const onError = useCallback(() => {
     Alert.alert(i18n.translate('DataUpload.ErrorTitle'), i18n.translate('DataUpload.ErrorBody'), [
       {text: i18n.translate('DataUpload.ErrorAction')},
@@ -32,6 +36,16 @@ export const DataSharingScreen = () => {
     navigation.goBack();
   }, [navigation]);
 
+  const getContent = () => {
+    if (!isConfirmedStep1) {
+      return <Step1 onSuccess={acceptStep1} />;
+    } else if (isVerified) {
+      return <ConsentView onSuccess={handleUploaded} onError={onError} />;
+    } else {
+      return <FormView value={codeValue} onChange={handleChange} onSuccess={handleVerify} onError={onError} />;
+    }
+  };
+
   return (
     <Box backgroundColor="overlayBackground" flex={1}>
       <SafeAreaView style={styles.flex}>
@@ -42,12 +56,7 @@ export const DataSharingScreen = () => {
           navLabel={i18n.translate('DataUpload.Cancel')}
           onIconClicked={close}
         />
-        <ScrollView style={styles.flex}>
-          {!isVerified && (
-            <FormView value={codeValue} onChange={handleChange} onSuccess={handleVerify} onError={onError} />
-          )}
-          {isVerified && <ConsentView onSuccess={handleUploaded} onError={onError} />}
-        </ScrollView>
+        <ScrollView style={styles.flex}>{getContent()}</ScrollView>
       </SafeAreaView>
     </Box>
   );
