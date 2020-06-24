3beb3b70b4b4c19df8b219eff1241a18bd917a3a
Anirudh Agnihotri <Anirudh.Agnihotri@Ontario.ca>
2020-06-24 15:23:01 -0400

    Made BT & Internet error icons the same. Error message updated for no internet

--- src/screens/home/views/BluetoothDisabledView.tsx
+++ src/screens/home/views/BluetoothDisabledView.tsx
@@ -1,9 +1,9 @@
-import React, {useCallback} from 'react';
-import {useI18n} from '@shopify/react-i18n';
-import {Linking} from 'react-native';
-import {Text, Box, ButtonMultiline} from 'components';
+import React, { useCallback } from 'react';
+import { useI18n } from '@shopify/react-i18n';
+import { Linking } from 'react-native';
+import { Text, Box, ButtonMultiline, Icon } from 'components';
 
-import {BaseHomeView} from '../components/BaseHomeView';
+import { BaseHomeView } from '../components/BaseHomeView';
 
 export const BluetoothDisabledView = () => {
   const [i18n] = useI18n();
@@ -12,7 +12,10 @@ export const BluetoothDisabledView = () => {
   }, [i18n]);
 
   return (
-    <BaseHomeView iconName="icon-bluetooth-disabled">
+    <BaseHomeView>
+      <Box marginBottom="l" marginLeft="-xxl">
+        <Icon name="icon-bluetooth-disabled" size={152} />
+      </Box>
       <Text variant="bodyTitle" color="bodyText" marginBottom="l" accessibilityRole="header">
         {i18n.translate('Home.BluetoothDisabled')}
       </Text>
--- src/screens/home/views/NetworkDisabledView.tsx
+++ src/screens/home/views/NetworkDisabledView.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {useI18n} from '@shopify/react-i18n';
-import {Box, Text, LastCheckedDisplay, Icon} from 'components';
+import { useI18n } from '@shopify/react-i18n';
+import { Box, Text, LastCheckedDisplay, Icon } from 'components';
 
-import {BaseHomeView} from '../components/BaseHomeView';
+import { BaseHomeView } from '../components/BaseHomeView';
 
 export const NetworkDisabledView = () => {
   const [i18n] = useI18n();
   return (
     <BaseHomeView>
-      <Box marginBottom="l">
-        <Icon name="icon-offline" size={44} />
+      <Box marginBottom="l" marginLeft="-xxl">
+        <Icon name="icon-bluetooth-disabled" size={152} />
       </Box>
       <Text variant="bodyTitle" color="bodyText" marginBottom="l" accessibilityRole="header">
         {i18n.translate('Home.NoConnectivity')}
@@ -22,3 +22,4 @@ export const NetworkDisabledView = () => {
     </BaseHomeView>
   );
 };
+
