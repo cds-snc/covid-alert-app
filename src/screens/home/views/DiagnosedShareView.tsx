bf54e959d4f9c56d1fa34468cb7028be8e49ca4b
smcmurtry <smcmurtry@gmail.com>
2020-06-26 16:43:57 -0600

    updated upload share page to match designs

--- src/screens/home/views/DiagnosedShareView.tsx
+++ src/screens/home/views/DiagnosedShareView.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {useI18n} from '@shopify/react-i18n';
-import {Text, ButtonSingleLine, Box} from 'components';
+import {Text, Button, Box} from 'components';
 
 import {BaseHomeView} from '../components/BaseHomeView';
 
@@ -13,18 +13,16 @@ export const DiagnosedShareView = () => {
   return (
     <BaseHomeView iconName="hand-wave">
       <Text variant="bodyTitle" color="bodyText" marginBottom="m" accessibilityRole="header">
-        {i18n.translate('Home.DailyShare')}
+        {i18n.translate('Home.DiagnosedShareView.Title')}
       </Text>
       <Text variant="bodyText" color="bodyText" marginBottom="m">
-        {i18n.translate('Home.DailyShareDetailed')}
+        {i18n.translate('Home.DiagnosedShareView.Body1')}
       </Text>
-      <Box alignSelf="stretch" marginTop="xxl" marginBottom="xl">
-        <ButtonSingleLine
-          text={i18n.translate('Home.ShareRandomIDsCTA')}
-          variant="bigFlatPurple"
-          externalLink
-          onPress={toDataShare}
-        />
+      <Text variant="bodyText" color="bodyText" marginBottom="m">
+        {i18n.translate('Home.DiagnosedShareView.Body2')}
+      </Text>
+      <Box alignSelf="stretch" marginTop="xl" marginBottom="xl">
+        <Button text={i18n.translate('Home.DiagnosedShareView.ButtonCTA')} variant="thinFlat" onPress={toDataShare} />
       </Box>
     </BaseHomeView>
   );
