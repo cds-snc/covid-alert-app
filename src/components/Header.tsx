29461759b5a82e16f58c318ae5bf588fa6971ce1
smcmurtry <smcmurtry@gmail.com>
2020-06-25 20:32:46 -0600

    changed the name, changed the launch screen, changed the header svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, {useCallback} from 'react';
 import {TouchableWithoutFeedback} from 'react-native';
 import {useNavigation, DrawerActions} from '@react-navigation/native';
+import {useI18n} from '@shopify/react-i18n';
 
 import {Box} from './Box';
 import {Icon} from './Icon';
@@ -11,14 +12,16 @@ export interface HeaderProps {
 
 export const Header = () => {
   const navigation = useNavigation();
+  const [i18n] = useI18n();
 
   const onLogoPress = useCallback(() => {
     navigation.dispatch(DrawerActions.openDrawer());
   }, [navigation]);
+
   return (
     <TouchableWithoutFeedback onPress={onLogoPress}>
       <Box maxHeight={30} flexDirection="row" alignItems="center" justifyContent="center">
-        <Icon size={129} name="stop-covid" />
+        {i18n.locale === 'fr' ? <Icon size={138} name="covid-alert-fr" /> : <Icon size={129} name="covid-alert-en" />}
       </Box>
     </TouchableWithoutFeedback>
   );
