c7565a6e940dc56ccc736a36313a00a8c740ee56
Tim Arney <tim@line37.com>
2020-06-28 07:12:27 -0400

    fix link color

--- src/screens/onboarding/Onboarding.tsx
+++ src/screens/onboarding/Onboarding.tsx
@@ -67,7 +67,7 @@ export const OnboardingScreen = () => {
     <Button
       backButton
       text={i18n.translate('Onboarding.ActionBack')}
-      color="linkText"
+      textColor="linkText"
       variant="subduedText"
       onPress={prevItem}
     />
