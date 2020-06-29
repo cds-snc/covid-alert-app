a89c33e161656987159b441707d9f1ea9b3a1584
Anirudh Agnihotri <Anirudh.Agnihotri@Ontario.ca>
2020-06-29 16:25:56 -0400

    H tags added

--- src/assets/privacypolicy-fr.js
+++ src/assets/privacypolicy-fr.js
@@ -4,35 +4,37 @@ Alerte COVID suit tous les principes du Commissariat à la protection de la vie 
 
 Le service Alerte COVID ne collecte ni ne stocke aucun de vos renseignements personnels.
 
-*Données collectées et stockées par l’application*
+## Données collectées et stockées par l’application*
 - Les codes aléatoires émis par votre téléphone, pendant 14 jours.
 - Les codes aléatoires émis par les autres téléphones à proximité, pendant 14 jours.
 
-*Protection des données*
+Les codes aléatoires ne sont stockés et utilisés qu’aux fins d’intervention contre la COVID-19.
+
+### Protection des données
 - L’application ne peut pas établir de lien entre vous et les codes aléatoires.
 - Votre téléphone chiffre les codes aléatoires. Votre autorisation est requise avant que votre téléphone transmette les codes aléatoires à Alerte COVID.
 - Aucune donnée ne sera transmise sans votre autorisation explicite.
 
-*Alerte COVID n’a aucun moyen de connaître ou de transmettre :*
+### Alerte COVID n’a aucun moyen de connaître ou de transmettre :
 - votre position,
 - votre nom ou votre adresse,
 - les contacts de votre téléphone,
 - vos informations de santé.
 
-# Données partagées par l’application
-*Rien n’est fait sans votre autorisation*
+## Données partagées par l’application
+### Rien n’est fait sans votre autorisation
 - Quand vous donnez votre autorisation, seuls les codes aléatoires de votre téléphone sont partagés.
 - Seuls l’application et son serveur auront accès aux codes aléatoires.
 
-*Si vous avez un diagnostic de COVID-19*
+### Si vous avez un diagnostic de COVID-19
 - Vous pouvez décider de partager vos codes aléatoires des 14 derniers jours avec un serveur central.
 - Si vous partagez vos codes, personne ne recevra d’information sur vous ou sur le moment de votre proximité.
 - On vous demandera aussi l’autorisation de téléverser vos codes aléatoires pendant les 13 jours suivants.
 
-*Si vous avez été près d’une personne qui signale un diagnostic*
+### Si vous avez été près d’une personne qui signale un diagnostic
 - Aucun de vos renseignements personnels n’est partagé avec d’autres personnes.
 
-*Suppression de vos données*
+## Suppression de vos données
 - Tous les codes aléatoires, qu’ils proviennent de votre téléphone ou d’autres téléphones, sont supprimés après 14 jours.
 - Vous pouvez supprimer l’application à tout moment, supprimant du même coup les codes aléatoires de votre téléphone.
 - Si vous avez téléversé vos codes aléatoires avant de supprimer l’application, les codes aléatoires seront supprimés du serveur après 14 jours.
--- src/assets/privacypolicy.js
+++ src/assets/privacypolicy.js
@@ -4,38 +4,40 @@ COVID Alert follows all the principles of the Privacy Commissioner of Canada for
 
 The COVID Alert service does not collect or store any personal information about you.
 
-## What the app collects and stored
+## What the app collects and stores
 - Random codes from your phone, for 14 days.
 - Random codes from other phones near you, for 14 days.
 
-## How the data is protected
+The random codes are only stored and used for the purpose of responding to COVID-19
+
+### How the data is protected
 
 - The app cannot connect your identity with the random codes.
 - Your phone encrypts the random codes. You must give your permission before your phone will share the random codes with COVID Alert.
 - Nothing is shared without your explicit permission.
 
-## COVID Alert has no way of knowing or sharing
+### COVID Alert has no way of knowing or sharing
 - Your location.
 - Your name or adress.
 - Your phone's contacts.
 - Your health information.
 
-# What the app shares
-*Nothing is done without your permission.*
+## What the app shares
+### Nothing is done without your permission.
 
 - Even when you give permission, the only information that's shared is the random codes from your phone.
 - Only the app and its servers will have access to the random codes.
 
-*If you're diagnosed with COVID-19*
+### If you're diagnosed with COVID-19
 - You can choose to share your random codes from the past 14 days with a central server.
 
 - If you share your random codes, nobody will get any information about you or the time you were near them.
 - You'll be asked for permission to upload your random codes for 13 days after.
 
-*If a person you were near reports a diagnosis through the app*
+### If a person you were near reports a diagnosis through the app
 - No information about you is shared with anyone.
 
-*Deleting your data*
+## Deleting your data
 - All random codes, whether from your phone or others, are deleted after 15 days.
 - You can delete the app at any time, and the random codes on your phone will be automatically deleted.
 - If you had uploaded your random codes before deleting the app, the random codes will be deleted from the server after 14 days.
