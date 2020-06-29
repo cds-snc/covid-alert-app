1f3ff9e1f2a7cf03cda16e3183f70c51e0d10f3a
Nick Mallory <nick.mallory@ontario.ca>
2020-06-29 13:27:18 -0400

    lint

--- src/screens/tutorial/views/ItemView.tsx
+++ src/screens/tutorial/views/ItemView.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {StyleSheet, Image, ImageSourcePropType} from 'react-native';
 import {Text} from 'components';
 import {useI18n} from '@shopify/react-i18n';
+
 import {TutorialKey} from '../TutorialContent';
 
 interface ItemViewProps {
