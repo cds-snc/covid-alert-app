094cf69abbba333524e076b3620efcbf858c2d6c
Anirudh Agnihotri <Anirudh.Agnihotri@Ontario.ca>
2020-06-29 16:36:44 -0400

    Seperated the policy text

--- src/screens/home/views/DiagnosedShareView.tsx
+++ src/screens/home/views/DiagnosedShareView.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {useI18n} from '@shopify/react-i18n';
-import {Text, Button, Box} from 'components';
+import {Text, Button, Box, Icon} from 'components';
 
 import {BaseHomeView} from '../components/BaseHomeView';
 
@@ -19,10 +19,18 @@ export const DiagnosedShareView = () => {
         {i18n.translate('Home.DiagnosedShareView.Body1')}
       </Text>
       <Text variant="bodyText" color="bodyText" marginBottom="m">
-        {i18n.translate('Home.DiagnosedShareView.Body2')}
+        <Text variant="bodySubTitle" color="bodyText" marginBottom="m">
+          {i18n.translate('Home.DiagnosedShareView.Body2') + ' '}
+        </Text>
+        {i18n.translate('Home.DiagnosedShareView.Body3')}
       </Text>
       <Box alignSelf="stretch" marginTop="xl" marginBottom="xl">
-        <Button text={i18n.translate('Home.DiagnosedShareView.ButtonCTA')} variant="thinFlat" onPress={toDataShare} />
+        <Button
+          text={i18n.translate('Home.DiagnosedShareView.ButtonCTA')}
+          variant="bigFlat"
+          onPress={toDataShare}
+          iconName="icon-chevron-white"
+        />
       </Box>
     </BaseHomeView>
   );
