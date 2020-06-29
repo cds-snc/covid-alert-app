2586b208bd9f290f9a8befe64e4da43b902dcc77
Anirudh Agnihotri <Anirudh.Agnihotri@Ontario.ca>
2020-06-29 13:19:42 -0400

    English fixed for uploading diagnosis codes

--- src/screens/home/views/DiagnosedShareView.tsx
+++ src/screens/home/views/DiagnosedShareView.tsx
@@ -19,7 +19,10 @@ export const DiagnosedShareView = () => {
         {i18n.translate('Home.DiagnosedShareView.Body1')}
       </Text>
       <Text variant="bodyText" color="bodyText" marginBottom="m">
-        {i18n.translate('Home.DiagnosedShareView.Body2')}
+        <Text variant="bodyText" color="bodyText" marginBottom="m">
+          {i18n.translate('Home.DiagnosedShareView.Body2') + ' '}
+        </Text>
+        {i18n.translate('Home.DiagnosedShareView.Body3')}
       </Text>
       <Box alignSelf="stretch" marginTop="xl" marginBottom="xl">
         <Button text={i18n.translate('Home.DiagnosedShareView.ButtonCTA')} variant="thinFlat" onPress={toDataShare} />
