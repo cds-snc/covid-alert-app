24f33d2b53f8f86ca84d49e8237825c8f0c0cc69
Nick Mallory <nick.mallory@ontario.ca>
2020-06-30 10:52:35 -0400

    Smaller How It Works Images

--- src/components/OnboardingHeader.tsx
+++ src/components/OnboardingHeader.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     height: 189,
-    marginBottom: 10,
+    marginBottom: 30,
     resizeMode: 'contain',
     alignSelf: 'center',
   },
