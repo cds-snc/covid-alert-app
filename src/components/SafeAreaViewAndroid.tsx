09d32e1e94d59ea0ddf8c285e2c0a761714ee647
Nick Mallory <nick.mallory@ontario.ca>
2020-06-30 08:58:29 -0400

    Removed SafeAreaViewAndroid component

--- src/components/SafeAreaViewAndroid.tsx
+++
@@ -1,8 +0,0 @@
-import {StyleSheet, Platform, StatusBar} from 'react-native';
-
-export default StyleSheet.create({
-  AndroidSafeArea: {
-    flex: 1,
-    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-});
