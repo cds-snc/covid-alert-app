ad8e9819e55240b18a3502731966bbc3684dd621
Nick Mallory <nick.mallory@ontario.ca>
2020-07-03 12:27:14 -0400

    Removed duplicate SafeAre in RegionPicker

--- src/screens/regionPicker/RegionPicker.tsx
+++ src/screens/regionPicker/RegionPicker.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Box, Text} from 'components';
 import {ScrollView} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import {useI18n} from '@shopify/react-i18n';
 import {useStorage} from 'services/StorageService';
 
@@ -13,39 +12,37 @@ export const RegionPickerScreen = () => {
 
   return (
     <Box justifyContent="flex-start" backgroundColor="overlayBackground">
-      <SafeAreaView style={regionStyles.flex}>
-        <ScrollView style={regionStyles.flex}>
-          <Box flex={1} paddingHorizontal="m">
-            <Text marginBottom="s" variant="bodyTitle" color="overlayBodyText" accessibilityRole="header">
-              {i18n.translate('RegionPicker.Title')}
-            </Text>
-            <Text marginVertical="m" variant="bodyText" color="overlayBodyText">
-              {i18n.translate('RegionPicker.Body')}
-            </Text>
-            <Box
-              marginTop="s"
-              paddingHorizontal="m"
-              borderRadius={10}
-              backgroundColor="infoBlockNeutralBackground"
-              accessibilityRole="radiogroup"
-            >
-              {regionData.map(item => {
-                return (
-                  <RegionItem
-                    key={item.code}
-                    selected={region === item.code}
-                    onPress={async selectedRegion => {
-                      setRegion(selectedRegion);
-                    }}
-                    name={i18n.translate(`RegionPicker.${item.code}`)}
-                    {...item}
-                  />
-                );
-              })}
-            </Box>
+      <ScrollView style={regionStyles.flex}>
+        <Box flex={1} paddingHorizontal="m">
+          <Text marginBottom="s" variant="bodyTitle" color="overlayBodyText" accessibilityRole="header">
+            {i18n.translate('RegionPicker.Title')}
+          </Text>
+          <Text marginVertical="m" variant="bodyText" color="overlayBodyText">
+            {i18n.translate('RegionPicker.Body')}
+          </Text>
+          <Box
+            marginTop="s"
+            paddingHorizontal="m"
+            borderRadius={10}
+            backgroundColor="infoBlockNeutralBackground"
+            accessibilityRole="radiogroup"
+          >
+            {regionData.map(item => {
+              return (
+                <RegionItem
+                  key={item.code}
+                  selected={region === item.code}
+                  onPress={async selectedRegion => {
+                    setRegion(selectedRegion);
+                  }}
+                  name={i18n.translate(`RegionPicker.${item.code}`)}
+                  {...item}
+                />
+              );
+            })}
           </Box>
-        </ScrollView>
-      </SafeAreaView>
+        </Box>
+      </ScrollView>
     </Box>
   );
 };
