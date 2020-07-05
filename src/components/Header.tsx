a4583d325611ce6474867bb1eb9938baedfe7a09
smcmurtry <smcmurtry@gmail.com>
2020-07-04 22:07:39 -0600

    header now depends on test mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React, {useCallback} from 'react';
 import {TouchableWithoutFeedback} from 'react-native';
 import {useNavigation, DrawerActions} from '@react-navigation/native';
 import {useI18n} from '@shopify/react-i18n';
+import {TEST_MODE} from 'env';
 
 import {Box} from './Box';
 import {Icon} from './Icon';
@@ -10,19 +11,26 @@ export interface HeaderProps {
   isOverlay?: boolean;
 }
 
+const BasicHeader = () => {
+  const [i18n] = useI18n();
+  return (
+    <Box maxHeight={30} flexDirection="row" alignItems="center" justifyContent="center">
+      {i18n.locale === 'fr' ? <Icon size={138} name="covid-alert-fr" /> : <Icon size={129} name="covid-alert-en" />}
+    </Box>
+  );
+};
+
 export const Header = () => {
   const navigation = useNavigation();
-  const [i18n] = useI18n();
-
   const onLogoPress = useCallback(() => {
     navigation.dispatch(DrawerActions.openDrawer());
   }, [navigation]);
-
-  return (
-    <TouchableWithoutFeedback onPress={onLogoPress}>
-      <Box maxHeight={30} flexDirection="row" alignItems="center" justifyContent="center">
-        {i18n.locale === 'fr' ? <Icon size={138} name="covid-alert-fr" /> : <Icon size={129} name="covid-alert-en" />}
-      </Box>
-    </TouchableWithoutFeedback>
-  );
+  if (TEST_MODE) {
+    return (
+      <TouchableWithoutFeedback onPress={onLogoPress}>
+        <BasicHeader />
+      </TouchableWithoutFeedback>
+    );
+  }
+  return <BasicHeader />;
 };
