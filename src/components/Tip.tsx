78f0ff39fc80a14386e981c823730715a9eae4f0
smcmurtry <smcmurtry@gmail.com>
2020-07-07 15:45:33 -0600

    wip

--- src/components/Tip.tsx
+++ src/components/Tip.tsx
@@ -3,6 +3,8 @@ import {Box, Button, Icon, Text} from 'components';
 import {Linking} from 'react-native';
 import {useI18n} from '@shopify/react-i18n';
 
+import {ButtonSingleLine} from './ButtonSingleLine';
+
 interface Props {
   children: React.ReactElement;
 }
@@ -10,27 +12,23 @@ interface Props {
 export const Tip = ({children}: Props) => {
   const [i18n] = useI18n();
   return (
-    <Box
-      backgroundColor="green2"
-      borderRadius={10}
-      paddingVertical="m"
-      paddingLeft="s"
-      paddingRight="m"
-      flexDirection="row"
-      marginTop="m"
-      marginBottom="xl"
-    >
-      <Box flex={0} paddingTop="xxs" marginRight="xxs">
-        <Icon name="icon-light-bulb" size={40} />
+    <Box backgroundColor="green2" borderRadius={10} paddingVertical="m" marginTop="m" marginBottom="xl">
+      <Box flexDirection="row" paddingLeft="s" paddingRight="m">
+        <Box flex={0} paddingTop="xxs" marginRight="xxs">
+          <Icon name="icon-light-bulb" size={40} />
+        </Box>
+        <Box flex={1}>
+          <Text fontWeight="bold" accessible>
+            {i18n.translate('Home.DiagnosedView.TipTitle')}
+          </Text>
+          <Text accessible>{i18n.translate('Home.DiagnosedView.TipBody')}</Text>
+        </Box>
       </Box>
-      <Box flex={1}>
-        <Text fontWeight="bold" accessible>
-          {i18n.translate('Home.DiagnosedView.TipTitle')}
-        </Text>
-        <Text accessible>{i18n.translate('Home.DiagnosedView.TipBody')}</Text>
-        <Button
-          variant="subduedText"
+      <Box paddingHorizontal="m">
+        <ButtonSingleLine
           text={i18n.translate('Home.DiagnosedView.TipLinkText')}
+          variant="thinFlat"
+          externalLink
           onPress={() => Linking.openURL(i18n.translate('Home.DiagnosedView.TipURL'))}
         />
       </Box>
