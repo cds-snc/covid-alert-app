5cba5f80ca102cbe8dea6ad60b1927c535a2e1b2
mtropper <52504483+mtropper@users.noreply.github.com>
2020-07-07 13:48:14 -0400

    Update Start.tsx

--- src/screens/onboarding/views/Start.tsx
+++ src/screens/onboarding/views/Start.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { Box, Text, OnboardingHeader } from 'components';
-import { useI18n } from '@shopify/react-i18n';
-import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, {useCallback} from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
+import {Box, Text, OnboardingHeader} from 'components';
+import {useI18n} from '@shopify/react-i18n';
+import {useNavigation} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-import { NextButton, StepText } from '../components';
+import {NextButton, StepText} from '../components';
 
 export const Start = () => {
   const [i18n] = useI18n();
