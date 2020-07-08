77361a406ea48b49667e973ab4945bd62dd65c62
smcmurtry <smcmurtry@gmail.com>
2020-07-07 20:59:41 -0600

    moved start

--- src/screens/onboarding/views/Start.tsx
+++ src/screens/onboarding/views/Start.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import {Box, Text, OnboardingHeader} from 'components';
+import {useI18n} from '@shopify/react-i18n';
 
-import {ItemView, ItemViewProps} from './ItemView';
+import {ItemViewProps} from './ItemView';
 
 export const Start = (props: Pick<ItemViewProps, 'isActive'>) => {
-  return <ItemView {...props} item="step-1" image={require('assets/how-it-works-intro.png')} />;
+  const [i18n] = useI18n();
+
+  return (
+    <>
+      <OnboardingHeader
+        text={i18n.translate('Onboarding.Start.Title')}
+        imageSrc={require('assets/onboarding-start.png')}
+        accessible
+        accessibilityLabel={i18n.translate('Onboarding.Start.ImageAltText')}
+      />
+      <Box marginBottom="m">
+        <Text variant="bodyText" color="overlayBodyText">
+          {i18n.translate('Onboarding.Start.Body1')}
+        </Text>
+      </Box>
+      <Box marginBottom="m">
+        <Text variant="bodyText" color="overlayBodyText">
+          {i18n.translate('Onboarding.Start.Body2')}
+        </Text>
+      </Box>
+    </>
+  );
 };
