8a7aa03d7b45636be3f657c8bf787b97f8d6b500
Henry Tao <henry.tao@shopify.com>
2020-07-08 16:05:15 -0400

    Attempt to fix timing issue

--- src/shared/useAccessibilityAutoFocus.ts
+++ src/shared/useAccessibilityAutoFocus.ts
@@ -1,18 +1,22 @@
 import {useLayoutEffect, useState} from 'react';
-import {AccessibilityInfo, findNodeHandle} from 'react-native';
+import {AccessibilityInfo, findNodeHandle, InteractionManager} from 'react-native';
 
 /**
  * Remember to add `accessible={true}` to target component
+ * Note: Look like there is timing issue related to this https://github.com/react-native-community/discussions-and-proposals/issues/118
  */
 export const useAccessibilityAutoFocus = (isActive = true) => {
   const [autoFocusRef, setAutoFocusRef] = useState<any>();
 
   useLayoutEffect(() => {
-    const node = findNodeHandle(autoFocusRef);
-    if (!node || !isActive) {
-      return;
-    }
-    AccessibilityInfo.setAccessibilityFocus(node);
+    const {cancel} = InteractionManager.runAfterInteractions(() => {
+      const node = findNodeHandle(autoFocusRef);
+      if (!node || !isActive) {
+        return;
+      }
+      AccessibilityInfo.setAccessibilityFocus(node);
+    });
+    return cancel;
   }, [autoFocusRef, isActive]);
 
   return setAutoFocusRef;
