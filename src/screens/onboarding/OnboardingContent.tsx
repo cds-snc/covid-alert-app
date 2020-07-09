ea8c3f402995aad9f1f91231677df5a1580e4b7b
Tim Arney <tim@line37.com>
2020-07-08 22:39:00 -0400

    tsc

--- src/screens/onboarding/OnboardingContent.tsx
+++ src/screens/onboarding/OnboardingContent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {Box} from 'components';
 import RNFadedScrollView from 'rn-faded-scrollview';
 
