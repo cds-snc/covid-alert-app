1dae120d75413aafb17b4a2cf155a4eb4c8cbf99
smcmurtry <smcmurtry@gmail.com>
2020-07-08 20:56:39 -0600

    made the all set title text different for uncovered regions

--- src/screens/home/components/AllSetView.tsx
+++ src/screens/home/components/AllSetView.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import {Text} from 'components';
-import {useI18n} from '@shopify/react-i18n';
 
 import {BaseHomeView} from './BaseHomeView';
 
-export const AllSetView = ({bodyText}: {bodyText: string}) => {
-  const [i18n] = useI18n();
+export const AllSetView = ({titleText, bodyText}: {titleText: string; bodyText: string}) => {
   return (
     <BaseHomeView iconName="thumbs-up">
       <Text variant="bodyTitle" color="bodyText" marginBottom="m" accessibilityRole="header">
-        {i18n.translate('Home.NoExposureDetected.AllSetTitle')}
+        {titleText}
       </Text>
       <Text variant="bodyText" color="bodyText" marginBottom="m">
         {bodyText}
