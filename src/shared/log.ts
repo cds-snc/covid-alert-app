c982d13d4d3f5b6514abfc1441ec94ed2528e948
Henry Tao <henry.tao@shopify.com>
2020-07-12 10:46:03 -0400

    Remove unused code

--- src/shared/log.ts
+++
@@ -1,4 +0,0 @@
-export const localLog = async (message: string, params: {[key in string]: any} = {}) => {
-  const url = `http://192.168.0.24:3001/${encodeURI(message)}/${encodeURI(JSON.stringify(params))}`;
-  await fetch(url).catch(() => {});
-};
