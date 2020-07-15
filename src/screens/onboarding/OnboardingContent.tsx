8341507db9a6acaa2ac5dfc88f51874a7f0d716c
Marjan Tropper <marjan.tropper@canada.ca>
2020-07-15 18:01:04 -0400

    Remove step 3 of 6 from the onboarding flow
    Removed step 3 of 6 from the onboarding flow and add bullet #5 on Privacy screen

--- src/screens/onboarding/OnboardingContent.tsx
+++ src/screens/onboarding/OnboardingContent.tsx
@@ -7,21 +7,19 @@ import {Start} from './views/Start';
 import {Anonymous} from './views/Anonymous';
 import {HowItWorks} from './views/HowItWorks';
 import {Permissions} from './views/Permissions';
-import {WhatItsNot} from './views/WhatItsNot';
 import {Region} from './views/Region';
 import {ItemViewProps} from './views/ItemView';
 
-export type OnboardingKey = 'step-1' | 'step-2' | 'step-3' | 'step-4' | 'step-5' | 'step-6';
+export type OnboardingKey = 'step-1' | 'step-2' | 'step-3' | 'step-4' | 'step-5';
 
-export const onboardingData: OnboardingKey[] = ['step-1', 'step-2', 'step-3', 'step-4', 'step-5', 'step-6'];
+export const onboardingData: OnboardingKey[] = ['step-1', 'step-2', 'step-3', 'step-4', 'step-5'];
 
 const viewComponents: {[key in OnboardingKey]: React.ComponentType<Pick<ItemViewProps, 'isActive'>>} = {
   'step-1': Start,
   'step-2': Anonymous,
-  'step-3': WhatItsNot,
-  'step-4': HowItWorks,
-  'step-5': Permissions,
-  'step-6': Region,
+  'step-3': HowItWorks,
+  'step-4': Permissions,
+  'step-5': Region,
 };
 
 export interface OnboardingContentProps {
