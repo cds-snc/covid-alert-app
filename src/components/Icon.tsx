f71d7aab38b64022e569098b1d70eb423846b300
Tim Arney <tim@line37.com>
2020-07-15 19:47:36 -0400

    initial language select

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -45,6 +45,7 @@ import HandReminder from 'assets/hand-reminder.svg';
 import HandThankYouWithLove from 'assets/hand-thank-you-with-love.svg';
 import HandNoProvinceYet from 'assets/hand-no-province-yet.svg';
 import StopCOVID from 'assets/StopCOVID.svg';
+import CanadaLogo from 'assets/canada.svg';
 
 const ICONS = {
   'icon-x': IconX,
@@ -93,6 +94,7 @@ const ICONS = {
   'hand-reminder': HandReminder,
   'hand-thank-you-with-love': HandThankYouWithLove,
   'hand-no-province-yet': HandNoProvinceYet,
+  'canada-logo': CanadaLogo,
 };
 
 export type IconName = keyof typeof ICONS;
---
+++ src/screens/landing/LandingScreen.tsx
@@ -0,0 +1,51 @@
+import React, {useCallback} from 'react';
+import {Image, ScrollView, StyleSheet} from 'react-native';
+import {useStorage} from 'services/StorageService';
+import {Box, Button, Icon} from 'components';
+import {useI18n} from 'locale';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+const landingPNG = require('assets/landing.png');
+
+export const LandingScreen = () => {
+  const i18n = useI18n();
+  const navigation = useNavigation();
+  const {setLocale} = useStorage();
+  const toggle = useCallback(
+    (newLocale: 'en' | 'fr') => () => {
+      setLocale(newLocale);
+      navigation.navigate('Home');
+    },
+    [setLocale],
+  );
+  return (
+    <ScrollView style={styles.flex}>
+      <Box flex={1} backgroundColor="overlayBackground">
+        <Box marginBottom="s" style={{backgroundColor: '#EEEEEE'}}>
+          <Box paddingHorizontal="m" paddingBottom="xl">
+            <Image style={{marginTop: 60, paddingBottom: 20}} accessible source={landingPNG} />
+          </Box>
+        </Box>
+
+        <Box>
+          <Box paddingHorizontal="m" marginTop="l" marginBottom="l">
+            <Button onPress={toggle('en')} text={i18n.translate('LanguageSelect.En')} variant="thinFlat" />
+          </Box>
+          <Box paddingHorizontal="m" marginBottom="s">
+            <Button onPress={toggle('fr')} text={i18n.translate('LanguageSelect.Fr')} variant="thinFlat" />
+          </Box>
+
+          <Box alignSelf="center">
+            <Icon size={150} name="canada-logo" />
+          </Box>
+        </Box>
+      </Box>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+});
