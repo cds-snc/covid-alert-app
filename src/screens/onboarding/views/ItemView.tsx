8cb0a836644c63ee5d5b4c6587bca8f9d7dda9ad
Nick Mallory <nick.mallory@ontario.ca>
2020-07-17 16:24:26 -0400

    Use manual focus ref in ItemView for Onboarding

--- src/screens/onboarding/views/ItemView.tsx
+++ src/screens/onboarding/views/ItemView.tsx
@@ -1,8 +1,8 @@
-import React, {ReactNode, useRef} from 'react';
+import React, {ReactNode} from 'react';
 import {StyleSheet, Image, ImageSourcePropType} from 'react-native';
 import {Box, Text} from 'components';
 import {useI18n} from 'locale';
-import {focusOnElement} from 'shared/useAccessibilityAutoFocus';
+import {focusOnElement, useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
 import {useFocusEffect} from '@react-navigation/native';
 
 import {onboardingData, OnboardingKey} from '../OnboardingContent';
@@ -18,20 +18,19 @@ export interface ItemViewProps {
 
 export const ItemView = ({item, image, isActive, altText, header, children}: ItemViewProps) => {
   const i18n = useI18n();
+  const [focusRef, autoFocusRef] = useAccessibilityAutoFocus(isActive);
   const total = onboardingData.length;
   const step = i18n.translate('Onboarding.Step');
   const of = i18n.translate('Onboarding.Of');
   const x = onboardingData.indexOf(item) + 1;
   const stepText = `${step} ${x} ${of} ${total}`;
-  const textRef = useRef(null);
+
   useFocusEffect(() => {
-    if (isActive) {
-      focusOnElement(textRef.current);
-    }
+    focusOnElement(focusRef);
   });
   return (
     <>
-      <Text focusRef={textRef} marginBottom="s" marginTop="s" color="gray2">
+      <Text focusRef={autoFocusRef} marginBottom="s" marginTop="s" color="gray2">
         {stepText}
       </Text>
       {image ? (
