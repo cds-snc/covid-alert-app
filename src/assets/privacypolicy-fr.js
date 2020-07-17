132ba00f2758cc0687de0441dafba9e4ff37e403
EliseKa <45772213+EliseKa@users.noreply.github.com>
2020-07-17 10:50:45 -0400

    Update FR privacy
    -Replaced all "8-digit number" with "one-time key"
    -Applied updates from Lucas
    -Added contact info

--- src/assets/privacypolicy-fr.js
+++ src/assets/privacypolicy-fr.js
@@ -4,6 +4,8 @@ L’application Alerte COVID bénéficie d’une solide protection de la vie pri
 
 Il est extrêmement improbable que vous soyez identifié pendant que vous utilisez cette application. D’importantes mesures, décrites ci-dessous, ont été prises pour garantir la protection de votre vie privée et de vos données. 
 
+L’utilisation de l’application est volontaire.
+
 ## Données collectées et stockées sur votre téléphone
 - Les codes aléatoires émis par votre téléphone, pendant 14 jours.
 - Les codes aléatoires émis par les autres téléphones à proximité, pendant 14 jours.
@@ -27,9 +29,9 @@ Les codes aléatoires sont stockés et utilisés uniquement dans le but de vous 
 - Seuls l’application et son serveur auront accès aux codes aléatoires.
 
 ### Si vous avez un diagnostic de COVID-19
-- Vous pouvez décider de partager vos codes aléatoires des 14 derniers jours avec un serveur central.
+- Vous pouvez décider de partager vos codes aléatoires des 14 derniers jours avec un serveur central administré par le Canada.
 - Si vous partagez vos codes, personne ne recevra d’information sur vous ou sur le moment de votre proximité.
-- On vous demandera aussi l’autorisation de téléverser vos codes aléatoires pendant les 13 jours qui suivent.
+- On vous demandera aussi l’autorisation de partager vos codes aléatoires avec le serveur central pendant les 13 jours qui suivent.
 
 ### Si vous avez été près d’une personne qui signale un diagnostic au moyen de l’application
 - Aucun de vos renseignements personnels n’est partagé avec d’autres personnes.
@@ -41,16 +43,23 @@ Les codes aléatoires sont stockés et utilisés uniquement dans le but de vous 
 - Si vous avez téléversé vos codes aléatoires avant de supprimer l’application, les codes aléatoires seront supprimés du serveur après 15 jours.
 
 ## Autres renseignements à votre sujet
-Vos renseignements médicaux restent entre les mains de votre fournisseur de soins de santé provincial ou territorial. Alerte COVID ne sait pas qui vous êtes et ne peut pas avoir accès ces renseignements.
-- Le numéro à 8 chiffres fourni par votre professionnel de la santé indique à Alerte COVID que vous pouvez téléverser vos codes aléatoires.
-- L’application Alerte COVID a confiance en ce numéro. Elle l’utilise pour ne pas avoir à collecter de renseignements personnels qui permettraient de vous identifier.
+Alerte COVID est une application créée par le Gouvernement du Canada. Elle est conçue de sorte que vos renseignements médicaux restent entre les mains de votre fournisseur de soins de santé provincial ou territorial. Votre identité et vos informations de santé ne seront pas partagées avec le Gouvernement du Canada. Alerte COVID ne sait pas qui vous êtes et ne peut pas avoir accès ces renseignements.
+- La clé à usage unique fournie par votre professionnel de la santé indique à Alerte COVID que vous pouvez téléverser vos codes aléatoires.
+- L’application Alerte COVID a confiance en cette clé. Elle l’utilise pour ne pas avoir à collecter de renseignements personnels qui permettraient de vous identifier.
 - Les travailleurs de la santé n’ont aucun moyen de fournir vos renseignements personnels à Alerte COVID.
-- Afin de protéger l’intégrité des numéros à 8 chiffres fournis par votre fournisseur de soins de santé, votre province peut générer un long numéro d’identification interne garantissant que le numéro à 8 chiffres a été généré par une source valide. Le gouvernement du Canada n’est pas en mesure de faire le lien entre ces numéros d’identification internes et vous ou d’autres utilisateurs de l’application, et ces numéros ne sont utilisés qu’en cas de besoin pour prévenir les menaces de cybersécurité.
+- Afin de protéger l’intégrité des clés à usage unique fournies par votre fournisseur de soins de santé, votre province peut générer un long numéro d’identification interne garantissant que la clé à usage unique a été générée par une source valide. Le gouvernement du Canada n’est pas en mesure de faire le lien entre ces numéros d’identification internes et vous ou d’autres utilisateurs de l’application, et ces numéros ne sont utilisés qu’en cas de besoin pour prévenir les menaces de cybersécurité.
+- Si vous recevez une notification d’exposition, on vous suggérera des mesures à suivre. Le gouvernement du Canada ne recevra aucune information sur votre santé si vous suivez ces mesures.
+- Alerte COVID ne se connecte pas à d’autres applications de votre téléphone et ne collecte aucune information à partir de celles-ci.
 
 ## Votre adresse IP
-- Par mesure de sécurité, si vous saisissez un numéro à 8 chiffres incorrect, l’application stockera votre adresse IP pendant 5 minutes. L’application fait cela pour se protéger contre les personnes malveillantes.
+- Par mesure de sécurité, le serveur central stocke votre adresse IP quand l’application communique avec lui. Le serveur fait cela pour se protéger contre les personnes malveillantes.
 - Sans ces protections de sécurité en place, les polluposteurs pourraient inonder le système Alerte COVID et votre téléphone avec de fausses notifications d’exposition.
 - Aucun lien n’est jamais établi entre votre adresse IP et vos renseignements. L’adresse IP est uniquement utilisée pour garantir l’intégrité du système.
+
+## Questions?
+Contactez-nous par :
+- Téléphone : 1-833-784-4397
+- Courriel : CDS-SNC@tbs-sct.gc.ca
 `;
 
 export default privacy;
