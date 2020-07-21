821d89e7aafcdac7f2d8213dd9de3bb333b5fa81
Nick Mallory <nick.mallory@ontario.ca>
2020-07-21 14:48:29 -0400

    Better effect that only runs when navigation focus changes

--- src/screens/onboarding/views/Permissions.tsx
+++ src/screens/onboarding/views/Permissions.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useRef, useState, useEffect} from 'react';
 import {useI18n} from 'locale';
 import {Box, ButtonSingleLine} from 'components';
 import {useNavigation} from '@react-navigation/native';
@@ -6,15 +6,28 @@ import Markdown from 'react-native-markdown-display';
 import {StyleSheet} from 'react-native';
 
 import {ItemView, ItemViewProps} from './ItemView';
+import {focusOnElement} from 'shared/useAccessibilityAutoFocus';
 
 export const Permissions = (props: Pick<ItemViewProps, 'isActive'>) => {
   const i18n = useI18n();
   const navigation = useNavigation();
   const onPrivacy = useCallback(() => navigation.navigate('Privacy'), [navigation]);
+  const focusRef = useRef(null);
+  const [focusOnButton, setFocusOnButton] = useState(false);
 
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      setFocusOnButton(true);
+      focusOnElement(focusRef.current);
+    });
+
+    // Return the function to unsubscribe from the event so it gets removed on unmount
+    return unsubscribe;
+  }, [navigation]);
   return (
     <ItemView
       {...props}
+      autoFocus={!focusOnButton}
       image={require('assets/onboarding-enable.png')}
       altText={i18n.translate('Onboarding.Permissions.ImageAltText')}
       header={i18n.translate('Onboarding.Permissions.Title')}
