42e1a65095c89263ca154f0fc9726488b727d2ba
Joshua Tessier <joshpc@gmail.com>
2020-07-22 20:11:14 -0400

    Updates ScrollView contentInset when keyboard is visible
    This listens to keyboard events (willShow|willHide|willChangeFrame) and updates the contentInset on the DataSharing ScrollView accordingly.
    
    This gives us the desired behaviour of:
    - The entire content is scrollable when the keyboard is visible
    - The content is NOT cut off when scrolling behind the keyboard (i.e. we preserve the translucent keyboard effect)
    
    We use this method instead of using `KeyboardAvoidingView` which will cut the content off at the top of the keyboard (and additionally, attempt to animate the values causing jitter issues when rotating.)

--- src/screens/datasharing/DataSharing.tsx
+++ src/screens/datasharing/DataSharing.tsx
@@ -1,8 +1,8 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useState, useEffect} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {Box, Toolbar} from 'components';
-import {StyleSheet, Alert, ScrollView} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {StyleSheet, Alert, ScrollView, Keyboard} from 'react-native';
+import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useI18n} from 'locale';
 import {useExposureStatus} from 'services/ExposureNotificationService';
 
@@ -22,6 +22,20 @@ export const DataSharingScreen = () => {
   // if keySubmissionStatus is None we need the 1-time code, otherwise we should go right to consent
   const [isVerified, setIsVerified] = useState(exposureStatus.type === 'diagnosed');
   const [isConfirmedStep1, setIsConfirmedStep1] = useState(false);
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+  useEffect(() => {
+    const keyboardWillShowSubcription = Keyboard.addListener('keyboardWillShow', keyboardWillUpdate);
+    const keyboardWillHideSubscription = Keyboard.addListener('keyboardWillHide', keyboardWillUpdate);
+    const keyboardWillChangeFrameSubscription = Keyboard.addListener('keyboardWillChangeFrame', keyboardWillUpdate);
+
+    return () => {
+      keyboardWillShowSubcription.remove();
+      keyboardWillHideSubscription.remove();
+      keyboardWillChangeFrameSubscription.remove();
+    };
+  });
+
   const onErrorForm = () => {
     Alert.alert(i18n.translate('DataUpload.FormView.ErrorTitle'), i18n.translate('DataUpload.FormView.ErrorBody'), [
       {text: i18n.translate('DataUpload.FormView.ErrorAction')},
@@ -35,6 +49,10 @@ export const DataSharingScreen = () => {
       i18n.translate('DataUpload.ConsentView.ErrorBody'),
       [{text: i18n.translate('DataUpload.FormView.ErrorAction')}],
     );
+  };
+
+  const keyboardWillUpdate = (event) => {
+    setKeyboardHeight(event.endCoordinates.height);
   };
 
   const handleVerify = useCallback(async () => {
@@ -55,6 +73,16 @@ export const DataSharingScreen = () => {
     }
   };
 
+  // We will calculate the contentInset for the scroll view taking into account
+  // the safeAreaInsets provided to us. Scroll views automatically take into account
+  // (see: `automaticallyAdjustContentInsets`) the "safe" areas so we have to be
+  // sure to match their values.
+  const safeAreaInsets = useSafeAreaInsets();
+  const scrollInsets = keyboardHeight > 0 ? {
+    top: -safeAreaInsets.top,
+    bottom: keyboardHeight - safeAreaInsets.top - safeAreaInsets.bottom * 2
+  } : undefined;
+
   return (
     <Box backgroundColor="overlayBackground" flex={1}>
       <SafeAreaView style={styles.flex}>
@@ -65,7 +93,7 @@ export const DataSharingScreen = () => {
           navLabel={i18n.translate('DataUpload.Cancel')}
           onIconClicked={close}
         />
-        <ScrollView style={styles.flex} keyboardShouldPersistTaps="handled">
+        <ScrollView style={styles.flex} keyboardShouldPersistTaps="handled" contentInset={scrollInsets}>
           {getContent()}
         </ScrollView>
       </SafeAreaView>
