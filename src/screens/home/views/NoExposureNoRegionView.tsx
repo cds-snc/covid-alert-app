99d1d24ec57c3b78d7fda1974d6b81473d16055e
smcmurtry <smcmurtry@gmail.com>
2020-07-28 15:11:15 -0600

    added thumbs up to the no province monitoring screen

--- src/screens/home/views/NoExposureNoRegionView.tsx
+++ src/screens/home/views/NoExposureNoRegionView.tsx
@@ -28,7 +28,7 @@ export const NoExposureNoRegionView = ({isBottomSheetExpanded}: {isBottomSheetEx
 
   return (
     // note you can add an icon i.e. <BaseHomeView iconName="icon-offline>
-    <BaseHomeView>
+    <BaseHomeView iconName="thumbs-up">
       <Text focusRef={autoFocusRef} variant="bodyTitle" color="bodyText" marginBottom="m" accessibilityRole="header">
         {i18n.translate('Home.NoExposureDetected.NoRegion.Title')}
       </Text>
