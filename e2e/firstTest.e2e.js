39ca496aacc1fccfdb7f7739d32450ef5e7c0844
smcmurtry <smcmurtry@gmail.com>
2020-07-28 14:48:59 -0600

    removed comments, reverted changes to Info.plist

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -6,14 +6,4 @@ describe('Example', () => {
   it('should have landing screen', async () => {
     await expect(element(by.text('English'))).toBeVisible();
   });
-
-  // it('should show hello screen after tap', async () => {
-  //   await element(by.id('hello_button')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
-
-  // it('should show world screen after tap', async () => {
-  //   await element(by.id('world_button')).tap();
-  //   await expect(element(by.text('World!!!'))).toBeVisible();
-  // });
 });
