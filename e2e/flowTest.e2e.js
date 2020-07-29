e142d6c3429453e5abc9d4a8795ddf5a043d28e8
nickmly <nickmly@gmail.com>
2020-07-29 09:56:53 -0400

    Full onboarding test with screenshots

--- e2e/flowTest.e2e.js
+++ e2e/flowTest.e2e.js
@@ -1,25 +1,38 @@
 /* eslint-disable no-undef */
-const NUM_ONBOARDING_SCREENS = 6;
-describe('Test flow', () => {
+describe('Setup app and landing screen', () => {
   beforeEach(async () => {
+    await device.launchApp({permissions: {notifications: 'YES'}});
     await device.reloadReactNative();
   });
 
   it('has landing screen', async () => {
+    await device.launchApp({permissions: {notifications: 'YES'}});
     await expect(element(by.id('enButton'))).toBeVisible();
     await expect(element(by.id('frButton'))).toBeVisible();
     await element(by.id('enButton')).tap();
   });
+});
+
+const NUM_ONBOARDING_SCREENS = 6;
+describe('Test onboarding flow', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
 
   it('has onboarding screens', async () => {
-    for (var i = 1; i <= NUM_ONBOARDING_SCREENS; i++) {
+    for (var i = 1; i < NUM_ONBOARDING_SCREENS; i++) {
       await expect(element(by.text(`Step ${i} of ${NUM_ONBOARDING_SCREENS}`))).toBeVisible();
-      if (i < NUM_ONBOARDING_SCREENS) {
-        await expect(element(by.text('Next'))).toBeVisible();
-        await element(by.text('Next')).tap();
-      } else {
-        await expect(element(by.text('Done'))).toBeVisible();
-      }
+      await expect(element(by.text('Next'))).toBeVisible();
+      await device.takeScreenshot(`step ${i}`);
+      await element(by.text('Next')).tap();
     }
+    await device.takeScreenshot('step 6');
+    await expect(element(by.text('Done'))).toBeVisible();
+    await expect(element(by.id('step-6ScrollView'))).toBeVisible();
+    await expect(element(by.id('AB'))).toBeVisible();
+    await element(by.id('step-6ScrollView')).scrollTo('bottom');
+    await expect(element(by.id('ON'))).toBeVisible();
+    await element(by.id('ON')).tap();
+    await element(by.text('Done')).tap();
   });
 });
