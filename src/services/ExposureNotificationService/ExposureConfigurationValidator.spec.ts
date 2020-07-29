b4d80c0db65043b4fb3a6b61d8c26ca4b7ce150a
James Eberhardt <james@eberhardt.ca>
2020-07-29 15:29:25 -0400

    check bad configuration with missing: minimumRiskScore

--- src/services/ExposureNotificationService/ExposureConfigurationValidator.spec.ts
+++ src/services/ExposureNotificationService/ExposureConfigurationValidator.spec.ts
@@ -1,4 +1,4 @@
-import {ExposureConfigurationValidator} from './ExposureConfigurationValidator';
+import {ExposureConfigurationValidationError, ExposureConfigurationValidator} from './ExposureConfigurationValidator';
 import exposureConfigurationDefault from './ExposureConfigurationDefault.json';
 import exposureConfigurationSchema from './ExposureConfigurationSchema.json';
 
@@ -10,5 +10,30 @@ describe('ExposureConfigurationValidator', () => {
     );
 
     expect(result.errors).toHaveLength(0);
+  });
+
+  it('check bad configuration with missing: minimumRiskScore', async () => {
+    const exposureConfig1: any = {
+      attenuationLevelValues: [0, 0, 2, 2, 2, 2, 2, 2],
+      attenuationWeight: 50,
+      daysSinceLastExposureLevelValues: [0, 1, 1, 1, 1, 1, 1, 1],
+      daysSinceLastExposureWeight: 50,
+      durationLevelValues: [0, 0, 0, 0, 5, 5, 5, 5],
+      durationWeight: 50,
+      transmissionRiskLevelValues: [1, 1, 1, 1, 1, 1, 1, 1],
+      transmissionRiskWeight: 50,
+    };
+
+    let validatorError;
+    try {
+      new ExposureConfigurationValidator().validateExposureConfiguration(exposureConfig1, exposureConfigurationSchema);
+    } catch (error) {
+      validatorError = error;
+    }
+    expect(validatorError).toStrictEqual(
+      new ExposureConfigurationValidationError(
+        'Invalid Exposure Configuration JSON. instance requires property "minimumRiskScore"',
+      ),
+    );
   });
 });
