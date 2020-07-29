9d3de55d1d28a3972d5bd5fe6664f0f1051e5250
nickmly <nickmly@gmail.com>
2020-07-29 10:06:45 -0400

    Set demo mode for iOS and Android before doing test

--- e2e/flowTest.e2e.js
+++ e2e/flowTest.e2e.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-undef */
+const execSync = require('child_process').execSync;
+
 describe('Setup app and landing screen', () => {
   beforeEach(async () => {
     await device.launchApp({permissions: {notifications: 'YES'}});
@@ -13,6 +15,27 @@ describe('Setup app and landing screen', () => {
   });
 });
 
+async function setDemoMode() {
+  if (device.getPlatform() === 'ios') {
+    execSync(
+      'xcrun simctl status_bar "iPhone 11" override --time "12:00" --batteryState charged --batteryLevel 100 --wifiBars 3 --cellularMode active --cellularBars 4',
+    );
+  } else {
+    // enter demo mode
+    execSync('adb shell settings put global sysui_demo_allowed 1');
+    // display time 12:00
+    execSync('adb shell am broadcast -a com.android.systemui.demo -e command clock -e hhmm 1200');
+    // Display full mobile data with 4g type and no wifi
+    execSync(
+      'adb shell am broadcast -a com.android.systemui.demo -e command network -e mobile show -e level 4 -e datatype 4g -e wifi false',
+    );
+    // Hide notifications
+    execSync('adb shell am broadcast -a com.android.systemui.demo -e command notifications -e visible false');
+    // Show full battery but not in charging state
+    execSync('adb shell am broadcast -a com.android.systemui.demo -e command battery -e plugged false -e level 100');
+  }
+}
+
 const NUM_ONBOARDING_SCREENS = 6;
 describe('Test onboarding flow', () => {
   beforeEach(async () => {
@@ -20,6 +43,7 @@ describe('Test onboarding flow', () => {
   });
 
   it('has onboarding screens', async () => {
+    await setDemoMode();
     for (var i = 1; i < NUM_ONBOARDING_SCREENS; i++) {
       await expect(element(by.text(`Step ${i} of ${NUM_ONBOARDING_SCREENS}`))).toBeVisible();
       await expect(element(by.text('Next'))).toBeVisible();
