44aece196cbb47b126fb5ec57aa42ac39b537470
Piotr K. Isajew <pki@ex.com.pl>
2020-07-31 15:56:00 +0200

    lint + tsc fixes

--- src/components/CodeInput.spec.tsx
+++ src/components/CodeInput.spec.tsx
@@ -1,42 +1,42 @@
 import React from 'react';
+import {render, fireEvent, waitFor} from '@testing-library/react-native';
 import {ThemeProvider} from 'shared/theme';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import { create } from 'react-test-renderer';
-import { CodeInput } from './CodeInput';
-import defaultTeme, {Theme} from 'shared/theme/default';
-import { StorageServiceProvider } from 'services/StorageService';
+import {StorageServiceProvider} from 'services/StorageService';
 
+import {CodeInput} from './CodeInput';
 
 jest.mock('react-native-localize', () => ({
   getLocales: () => [{countryCode: 'US', languageTag: 'en-US', languageCode: 'en', isRTL: false}],
 }));
 
 describe('CodeInput', () => {
-
   const changeMock = jest.fn();
-  let componentQuery;
-  
+  let componentQuery: any;
+
   beforeEach(async () => {
-    componentQuery = await waitFor(() => render(
-      <StorageServiceProvider>
-        <ThemeProvider>
-        <CodeInput onChange={changeMock} accessibilityLabel='codeInput' />
-        </ThemeProvider>
-      </StorageServiceProvider>
-    ));
+    componentQuery = await waitFor(() =>
+      render(
+        <StorageServiceProvider>
+          <ThemeProvider>
+            <CodeInput value="" onChange={changeMock} accessibilityLabel="codeInput" />
+          </ThemeProvider>
+        </StorageServiceProvider>,
+      ),
+    );
   });
 
   afterEach(() => {
     jest.clearAllMocks();
     jest.resetAllMocks();
   });
-  
+
   it('returns trimmed text', async () => {
     const textInput = componentQuery.getByHintText('codeInput');
     fireEvent.changeText(textInput, ' MYSECRETCODE ');
-    expect(changeMock).toBeCalledWith('MYSECRETCODE');
+    expect(changeMock).toHaveBeenCalledWith('MYSECRETCODE');
   });
 
+  // eslint-disable-next-line jest/no-commented-out-tests
   /* TODO: uncomment after https://github.com/cds-snc/covid-alert-app/pull/844 is merged
   it('disallows special characters on input', () => {
     const changeMock = jest.fn();
