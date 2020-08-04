8742185107da909e4864c4556be7f6de4695a689
Henry Tao <henry.tao@shopify.com>
2020-08-04 08:39:05 -0400

    Fix tsc

--- src/screens/tutorial/Tutorial.tsx
+++ src/screens/tutorial/Tutorial.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useCallback, useRef} from 'react';
-import {StyleSheet, useWindowDimensions, View, StatusBar} from 'react-native';
+import {StyleSheet, useWindowDimensions, View} from 'react-native';
 import Carousel, {CarouselStatic, CarouselProps} from 'react-native-snap-carousel';
 import {useNavigation} from '@react-navigation/native';
 import {Box, Button, Toolbar, ProgressCircles, SafeAreaView} from 'components';
