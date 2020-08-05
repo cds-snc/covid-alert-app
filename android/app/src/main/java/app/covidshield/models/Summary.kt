72b4b0971763397c2333de9c01240225d2752880
James Eberhardt <james@eberhardt.ca>
2020-08-05 17:10:32 -0400

    Adding support for "attenuationDurationThresholds".

--- android/app/src/main/java/app/covidshield/models/Summary.kt
+++ android/app/src/main/java/app/covidshield/models/Summary.kt
@@ -3,6 +3,7 @@ package app.covidshield.models
 import com.google.gson.annotations.SerializedName
 
 data class Summary(
+    @SerializedName("attenuationDurations") val attenuationDurations: List<Int>,
     @SerializedName("daysSinceLastExposure") val daysSinceLastExposure: Int,
     @SerializedName("matchedKeyCount") val matchedKeyCount: Int,
     @SerializedName("maximumRiskScore") val maximumRiskScore: Int
