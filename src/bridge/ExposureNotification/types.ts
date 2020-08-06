8bdfc0e4f4fd232177df42e810b91ffbcb70b4c4
Stephen McMurtry <smcmurtry@gmail.com>
2020-08-06 10:20:15 -0600

    Merge pull request #1009 from cds-snc/patch/second-filter
    Added android/ios specific logic for 2nd filter

--- src/bridge/ExposureNotification/types.ts
+++ src/bridge/ExposureNotification/types.ts
@@ -37,7 +37,8 @@ export interface ExposureSummary {
 
 export interface ExposureConfiguration {
   metadata?: object;
-  minimumRiskScore: number;
+  minimumExposureDurationMinutes?: number;
+  attenuationDurationThresholds: number[];
   attenuationLevelValues: number[];
   attenuationWeight: number;
   daysSinceLastExposureLevelValues: number[];
