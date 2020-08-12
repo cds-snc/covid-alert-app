081c12192302f2e5d0459a1c5e6bfb53a3994a62
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-08-12 11:51:58 -0400

    logging improvements

--- src/shared/log.ts
+++ src/shared/log.ts
@@ -1,21 +1,43 @@
-export const captureMessage = async (message: string, params: {[key in string]: any} = {}) => {
-  // force return for production
-  return;
+import AsyncStorage from '@react-native-community/async-storage';
+import { getRandomString } from 'bridge/CovidShield';
 
-  const finalMessage = message;
-  const finalParams = params;
+let sentryEnabled = false;
 
-  if (!__DEV__) {
-    return;
-  }
-  console.log(finalMessage, finalParams); // eslint-disable-line no-console
+const UUID_KEY = 'UUID_KEY';
+
+const cachedUUID = AsyncStorage.getItem(UUID_KEY)
+  .then(uuid => uuid || getRandomString(8))
+  .then(uuid => {
+    AsyncStorage.setItem(UUID_KEY, uuid);
+    return uuid;
+  })
+  .catch(() => null);
+
+let currentUUID = '';
+
+export const setLogUUID = (uuid: string) => {
+  currentUUID = uuid;
+  AsyncStorage.setItem(UUID_KEY, uuid);
 };
 
-export const captureException = async (message: string, error: any, params: {[key in string]: any} = {}) => {
-  // force return for production
-  return;
+export const getLogUUID = async () => {
+  return currentUUID || (await cachedUUID) || 'unset';
+};
 
-  const finalMessage = `Error: ${message}`;
+export const captureMessage = async (message: string, params: { [key in string]: any } = {}) => {
+  const uuid = await getLogUUID();
+  const finalMessage = `[${uuid}] ${message}`.replace(/\n/g, '');
+  const finalParams = params;
+
+  if (__DEV__) {
+    console.log(finalMessage, finalParams); // eslint-disable-line no-console
+  }
+};
+
+export const captureException = async (message: string, error: any, params: { [key in string]: any } = {}) => {
+  const uuid = await getLogUUID();
+  const finalMessage = `[${uuid}] Error: ${message}`.replace(/\n/g, '');
+
   const finalParams = {
     ...params,
     error: {
@@ -23,8 +45,8 @@ export const captureException = async (message: string, error: any, params: {[ke
       error,
     },
   };
-  if (!__DEV__) {
-    return;
+
+  if (__DEV__) {
+    console.log(finalMessage, finalParams); // eslint-disable-line no-console
   }
-  console.log(finalMessage, finalParams); // eslint-disable-line no-console
 };
