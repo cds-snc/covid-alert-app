011c466536998a282db44b443c330b2c1fb2790e
smcmurtry <smcmurtry@gmail.com>
2020-08-12 16:38:04 -0600

    added an e2e test for one time code entry

---
+++ e2e/oneTimeCode.e2e.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+import {onboard} from './shared';
+
+describe('Test one time code flow', () => {
+  it('can get to the one time code screen', async () => {
+    await onboard();
+    await element(by.id('tapPromptCollapsed')).tap();
+    await device.takeScreenshot('BottomSheet');
+    await element(by.text('Enter your one-time key')).tap();
+    await device.takeScreenshot('OTC-Step1');
+    await element(by.text('Next')).tap();
+    await device.takeScreenshot('OTC-Step2');
+    await element(by.id('textInput')).tap();
+    await device.takeScreenshot('OTC-Step2-focussed');
+    await element(by.id('textInput')).typeText('ABC123ABCD');
+    await element(by.text('Submit')).tap();
+    await device.takeScreenshot('OTC-Step2-error');
+    await expect(element(by.text('Try again later'))).toBeVisible();
+  });
+});
