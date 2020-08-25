f49d0434bfd2a05a59940a6fd698f0fca64bc1f5
smcmurtry <smcmurtry@gmail.com>
2020-08-25 15:11:36 -0600

    fixed tests, removed unused ids from en.json and fr.json

--- src/locale/locale.spec.tsx
+++ src/locale/locale.spec.tsx
@@ -6,8 +6,13 @@ import REGION_CONTENT from './translations/region.json';
 describe('deep merge', () => {
   it('deep merges objects', async () => {
     const content: any = merge(LOCALES, REGION_CONTENT);
-    const localeText = content.en.Home.DiagnosedView.Tip.ON.Body;
-    const regionText = content.en.RegionContent.DiagnosedView.Active.ON.Tip.Body;
-    expect(localeText).toStrictEqual(regionText);
+
+    const contentTipTitle = content.en.Home.DiagnosedView.Tip.Title;
+    const localesTipTitle = LOCALES.en.Home.DiagnosedView.Tip.Title;
+    expect(contentTipTitle).toStrictEqual(localesTipTitle);
+
+    const contentTipCTA = content.en.RegionContent.DiagnosedView.Active.ON.Tip.CTA;
+    const regionTipCTA = REGION_CONTENT.en.RegionContent.DiagnosedView.Active.ON.Tip.CTA;
+    expect(contentTipCTA).toStrictEqual(regionTipCTA);
   });
 });
