82b4e981c11d663a16dc53d8f35964fcc63449a5
Tim Arney <tim@line37.com>
2020-08-25 14:24:26 -0400

    deep merge

--- src/locale/locale.spec.tsx
+++ src/locale/locale.spec.tsx
@@ -2,8 +2,8 @@ import merge from 'deepmerge';
 import LOCALES from './translations';
 import REGION_CONTENT from './translations/region.json';
 
-describe('createCancellableCallbackPromise', () => {
-  it('does not call callback if the promise has been canceled before being called', async () => {
+describe('deep merge', () => {
+  it('deep merges objects', async () => {
     const content: any = merge(LOCALES, REGION_CONTENT);
     const localeText = content['en']['Home']['DiagnosedView']['Tip']['ON']['Body'];
     const regionText = content['en']['RegionalContent']['DiagnosedView']['Active']['ON']['Tip']['Body'];
