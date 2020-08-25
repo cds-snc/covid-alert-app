a942cd5e9036c92635b1e785596e9c1ef8bcce51
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-08-25 14:57:25 -0400

    rename content_id to message_id

--- notifications/receipts.sql
+++ notifications/receipts.sql
@@ -1,4 +1,4 @@
 /*
  * Create sqlite database table for local storage of read receipts
  */
-create table if not exists receipts (id integer primary key autoincrement, content_id text)
+create table if not exists receipts (id integer primary key autoincrement, message_id text)
