89eb0c6302f53e185a210b7dfb6d714d2eead0c3
James Eberhardt <james@eberhardt.ca>
2020-08-26 15:58:03 -0400

    Added a region interface for JSON objects.

--- src/shared/Region.ts
+++ src/shared/Region.ts
@@ -1,2 +1,8 @@
 export type Region = 'AB' | 'BC' | 'MB' | 'NB' | 'NL' | 'NT' | 'NS' | 'NU' | 'ON' | 'PE' | 'QC' | 'SK' | 'YT' | 'None';
 export type RegionCase = 'regionNotCovered' | 'noRegionSet' | 'regionCovered';
+
+export interface RegionContent {
+  Active: Region | Region[];
+  en: any;
+  fr: any;
+}
