4bf1a0dcadf68d1658032d7dda04793c868fdf54
smcmurtry <smcmurtry@gmail.com>
2020-08-28 10:18:51 -0600

    fixed ts error

--- src/locale/regional.spec.tsx
+++ src/locale/regional.spec.tsx
@@ -6,13 +6,13 @@ const activeRegions = regionalI18n.activeRegions;
 
 describe('if region is active', () => {
   it('has the last paragraph text for ExposureView', async () => {
-    activeRegions.forEach(region => {
+    activeRegions.forEach((region: string) => {
       expect(regionalI18n.translate(`RegionContent.ExposureView.Active.${region}.Body`)).not.toStrictEqual('');
     });
   });
 
   it('has all the required NoCode Content', async () => {
-    activeRegions.forEach(region => {
+    activeRegions.forEach((region: string) => {
       expect(regionalI18n.translate(`RegionContent.NoCode.Active.${region}.Title`)).not.toStrictEqual('');
       expect(regionalI18n.translate(`RegionContent.NoCode.Active.${region}.Body`)).not.toStrictEqual('');
       expect(regionalI18n.translate(`RegionContent.NoCode.Active.${region}.CTA`)).not.toStrictEqual('');
