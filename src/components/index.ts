c6a7855171ecb69fd590c55f5b1af017dc509dd9
Tim Arney <tim@line37.com>
2020-08-28 05:50:46 -0400

    add error box

--- src/components/index.ts
+++ src/components/index.ts
@@ -20,3 +20,4 @@ export * from './Toolbar';
 export * from './TouchableIcon';
 export * from './LanguageToggle';
 export * from './LastCheckedDisplay';
+export * from './ErrorBox';
--- src/screens/home/views/ExposureView.tsx
+++ src/screens/home/views/ExposureView.tsx
@@ -2,7 +2,7 @@ import React, {useCallback} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {Linking} from 'react-native';
 import {useI18n} from 'locale';
-import {Text, Box, ButtonSingleLine} from 'components';
+import {Text, Box, ButtonSingleLine, ErrorBox} from 'components';
 import {useStorage} from 'services/StorageService';
 import {useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
 import {captureException} from 'shared/log';
@@ -37,6 +37,8 @@ export const ExposureView = ({isBottomSheetExpanded}: {isBottomSheetExpanded: bo
 
   const regionalGuidanceCTA = getGuidanceCTA();
 
+  console.log('guidace');
+
   const onActionGuidance = useCallback(() => {
     Linking.openURL(getGuidanceURL()).catch(error => captureException('An error occurred', error));
   }, [getGuidanceURL]);
@@ -63,10 +65,17 @@ export const ExposureView = ({isBottomSheetExpanded}: {isBottomSheetExpanded: bo
         )}
       </Text>
 
-      {regionalGuidanceCTA !== '' && (
+      {regionalGuidanceCTA !== '' ? (
         <Box alignSelf="stretch" marginTop="l" marginBottom={regionActive ? 'xxl' : 'm'}>
-          <ButtonSingleLine text={getGuidanceCTA()} variant="bigFlatPurple" externalLink onPress={onActionGuidance} />
+          <ButtonSingleLine
+            text={regionalGuidanceCTA}
+            variant="bigFlatPurple"
+            externalLink
+            onPress={onActionGuidance}
+          />
         </Box>
+      ) : (
+        <ErrorBox marginTop="m" />
       )}
 
       {!regionActive && (
