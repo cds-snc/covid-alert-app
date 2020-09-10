ff956099026c806fe882aa13a0d7d0f7534fa2a2
Platform <platform@Platforms-Mac-mini.local>
2020-09-10 12:56:31 -0400

    add radio buttons to step 2

--- src/screens/datasharing/Step2Screen.tsx
+++ src/screens/datasharing/Step2Screen.tsx
@@ -1,18 +1,20 @@
-import React, {useCallback} from 'react';
+import React, {useState} from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
 import {Box, Text, Button} from 'components';
 import {useI18n} from 'locale';
 import {useNavigation} from '@react-navigation/native';
+import {RadioButton} from './components/Radio';
 
 import {BaseDataSharingView} from './components/BaseDataSharingView';
 
 export const Step2Screen = () => {
   const i18n = useI18n();
   const navigation = useNavigation();
-  const onOption1 = useCallback(() => navigation.navigate('SymptomOnsetDate'), [navigation]);
-  const onOption2 = useCallback(() => navigation.navigate('TestDate'), [navigation]);
-  const onOption3 = useCallback(() => navigation.navigate('TestDate'), [navigation]);
-  const onOption4 = useCallback(() => navigation.navigate('TekUploadNoDate'), [navigation]);
+  const [radio, setRadio] = useState('');
+
+  const radioHandler = (val: string) => {
+    setRadio(val);
+  };
 
   return (
     <BaseDataSharingView>
@@ -22,16 +24,42 @@ export const Step2Screen = () => {
             {i18n.translate('DataUpload.Step2.Title')}
           </Text>
           <Box marginTop="m">
-            <Button variant="thinFlat" text={i18n.translate('DataUpload.Step2.Option1')} onPress={onOption1} />
+            <RadioButton
+              active={radio}
+              value="0"
+              text={i18n.translate('DataUpload.Step2.Option1')}
+              onPress={radioHandler}
+            />
+            <RadioButton
+              active={radio}
+              value="1"
+              text={i18n.translate('DataUpload.Step2.Option2')}
+              onPress={radioHandler}
+            />
+            <RadioButton
+              active={radio}
+              value="2"
+              text={i18n.translate('DataUpload.Step2.Option3')}
+              onPress={radioHandler}
+            />
+            <RadioButton
+              active={radio}
+              value="3"
+              text={i18n.translate('DataUpload.Step2.Option4')}
+              onPress={radioHandler}
+            />
           </Box>
+
           <Box marginTop="m">
-            <Button variant="thinFlat" text={i18n.translate('DataUpload.Step2.Option2')} onPress={onOption2} />
-          </Box>
-          <Box marginTop="m">
-            <Button variant="thinFlat" text={i18n.translate('DataUpload.Step2.Option3')} onPress={onOption3} />
-          </Box>
-          <Box marginTop="m">
-            <Button variant="thinFlat" text={i18n.translate('DataUpload.Step2.Option4')} onPress={onOption4} />
+            <Button
+              variant="thinFlat"
+              text={i18n.translate('DataUpload.Step2.Option4')}
+              onPress={() => {
+                const routes = ['SymptomOnsetDate', 'TestDate', 'TestDate', 'TekUploadNoDate'];
+                const selected = Number(radio);
+                navigation.navigate(routes[selected]);
+              }}
+            />
           </Box>
         </Box>
       </ScrollView>
---
+++ src/screens/datasharing/components/Radio.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Box, Text} from 'components';
+import {TouchableOpacity, View, StyleSheet} from 'react-native';
+
+export const RadioButton = (props: any) => {
+  const activeStyles = props.active === props.value ? {...styles.active} : {};
+  return (
+    <Box marginBottom="m">
+      <TouchableOpacity
+        onPress={() => {
+          props.onPress(props.value);
+        }}
+      >
+        <Box flex={1} flexDirection="row">
+          <Box style={{...styles.circle, ...activeStyles}}>
+            {props.active === props.value ? <View style={styles.checkedCircle} /> : <View />}
+          </Box>
+          <Box style={{marginTop: 0}}>
+            <Text variant="bodyText">{props.text}</Text>
+          </Box>
+        </Box>
+      </TouchableOpacity>
+    </Box>
+  );
+};
+
+const styles = StyleSheet.create({
+  circle: {
+    height: 30,
+    width: 30,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#000',
+    alignItems: 'center', // To center the checked circle…
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  active: {
+    borderWidth: 3,
+  },
+  checkedCircle: {
+    width: 16,
+    height: 16,
+    borderRadius: 30,
+    backgroundColor: '#000',
+    borderWidth: 4,
+  },
+});
