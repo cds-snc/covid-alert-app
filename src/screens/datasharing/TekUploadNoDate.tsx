18e44e03dc5323cc8d7e4119be3c9a11807ddc5e
smcmurtry <smcmurtry@gmail.com>
2020-09-11 10:03:01 -0600

    removed back button from TekUploadNoDate screen

--- src/screens/datasharing/TekUploadNoDate.tsx
+++ src/screens/datasharing/TekUploadNoDate.tsx
@@ -1,20 +1,15 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import {Box, Text, TextMultiline} from 'components';
 import {useI18n} from 'locale';
-import {useNavigation} from '@react-navigation/native';
 
 import {BaseTekUploadView, StepXofY} from './components';
 
 export const TekUploadNoDate = () => {
   const i18n = useI18n();
-  const navigation = useNavigation();
-  const secondaryButtonOnPress = useCallback(() => navigation.navigate('Step2'), [navigation]);
   return (
     <BaseTekUploadView
       buttonText={i18n.translate('DataUpload.TekUploadNoDate.CTA')}
       contagiousDateInfo={{dateType: 'noDate', dateString: ''}}
-      secondaryButtonText={i18n.translate('DataUpload.TekUploadNoDate.CTA2')}
-      secondaryButtonOnPress={secondaryButtonOnPress}
     >
       <Box paddingHorizontal="m">
         <StepXofY currentStep={4} />
