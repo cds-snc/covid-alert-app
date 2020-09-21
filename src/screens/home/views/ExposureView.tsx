ef599955971fabc67296477c3ae9636bc20f18da
smcmurtry <smcmurtry@gmail.com>
2020-09-21 11:39:47 -0600

    removed code from exposure view that was moved to new component

--- src/screens/home/views/ExposureView.tsx
+++ src/screens/home/views/ExposureView.tsx
@@ -1,11 +1,9 @@
 import React, {useCallback} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {Linking} from 'react-native';
 import {useI18n} from 'locale';
-import {Text, Box, ButtonSingleLine, ErrorBox} from 'components';
+import {Text, Box, ButtonSingleLine} from 'components';
 import {useStorage} from 'services/StorageService';
 import {useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
-import {captureException} from 'shared/log';
 import {isRegionActive} from 'shared/RegionLogic';
 import {useRegionalI18n} from 'locale/regional';
 import {ExposedHelpButton} from 'components/ExposedHelpButton';
@@ -25,32 +23,6 @@ export const ExposureView = ({isBottomSheetExpanded}: {isBottomSheetExpanded: bo
   const regionalI18n = useRegionalI18n();
   const navigation = useNavigation();
   const regionActive = isRegionActive(region, regionalI18n.activeRegions);
-  const getGuidanceURL = useCallback(() => {
-    if (region !== undefined && region !== 'None') {
-      console.log('regiona', region);
-      return regionActive
-        ? regionalI18n.translate(`RegionContent.ExposureView.Active.${region}.URL`)
-        : regionalI18n.translate(`RegionContent.ExposureView.Inactive.${region}.URL`);
-    }
-    console.log('regionb', region);
-    return regionalI18n.translate(`RegionContent.ExposureView.Inactive.CA.URL`);
-  }, [region, regionActive, regionalI18n]);
-
-  const getGuidanceCTA = useCallback(() => {
-    if (region !== undefined && region !== 'None') {
-      return regionActive
-        ? regionalI18n.translate(`RegionContent.ExposureView.Active.${region}.CTA`)
-        : regionalI18n.translate(`RegionContent.ExposureView.Inactive.${region}.CTA`);
-    }
-    return regionalI18n.translate(`RegionContent.ExposureView.Inactive.CA.CTA`);
-  }, [region, regionActive, regionalI18n]);
-
-  const regionalGuidanceCTA = getGuidanceCTA();
-
-  const onActionGuidance = useCallback(() => {
-    console.log('getGuidanceURL', getGuidanceURL());
-    Linking.openURL(getGuidanceURL()).catch(error => captureException('An error occurred', error));
-  }, [getGuidanceURL]);
   const onHowToIsolate = useCallback(() => navigation.navigate('HowToIsolate'), [navigation]);
   const autoFocusRef = useAccessibilityAutoFocus(!isBottomSheetExpanded);
   const activeBodyText = regionalI18n.translate(`RegionContent.ExposureView.Active.${region}.Body`);
