7136b0fdc85a811008b649d6756d4ca958d35c53
Tim Arney <tim@line37.com>
2020-09-25 09:36:38 -0400

    add summary log

--- src/services/ExposureNotificationService/ExposureNotificationService.ts
+++ src/services/ExposureNotificationService/ExposureNotificationService.ts
@@ -458,6 +458,8 @@ export class ExposureNotificationService {
           period: periodSinceEpoch(today, HOURS_PER_PERIOD),
         };
         summaries = pendingSummaries;
+
+        captureMessage('pendingSummaries', {summaries: summaries});
       } else {
         const currentStatus = this.exposureStatus.get();
         await this.updateExposure();
@@ -466,6 +468,8 @@ export class ExposureNotificationService {
         lastCheckedPeriod = keysAndLastChecked.lastChecked;
 
         summaries = await this.exposureNotification.detectExposure(exposureConfiguration, keysAndLastChecked.keys);
+
+        captureMessage('getSummariesFromEnFramework', {summaries: summaries});
       }
       return {summaries, lastCheckedPeriod};
     } catch (error) {
