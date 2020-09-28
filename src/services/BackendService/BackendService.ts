905ab368382fc6c5faeae03e0af00cfb793b6cd3
Tim Arney <tim@line37.com>
2020-09-28 16:25:26 -0400

    add fetch headers

--- src/services/BackendService/BackendService.ts
+++ src/services/BackendService/BackendService.ts
@@ -78,9 +78,8 @@ export class BackendService implements BackendInterface {
 
   async getRegionContent(): Promise<RegionContentResponse> {
     try {
-      const headers: any = {};
       // try fetching server content
-      const response = await fetch(this.getRegionContentUrl(), {method: 'GET', headers});
+      const response = await fetch(this.getRegionContentUrl(), FETCH_HEADERS);
       const payload = await response.json();
       this.isValidRegionContent({status: response.status, payload});
       captureMessage('getRegionContent - saving stored content', payload);
