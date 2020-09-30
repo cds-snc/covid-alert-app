3d95b51c1521b276fc3e836e60cf4841833e29a6
James Eberhardt <james@eberhardt.ca>
2020-09-30 14:40:08 -0400

    Fixed the `dateSpy` implementation.

--- src/services/ExposureNotificationService/ExposureNotificationService.spec.ts
+++ src/services/ExposureNotificationService/ExposureNotificationService.spec.ts
@@ -213,9 +213,10 @@ describe('ExposureNotificationService', () => {
   );
 
   it('backfills keys when last timestamp not available', async () => {
-    dateSpy
-      .mockImplementationOnce(() => new OriginalDate('2020-05-19T07:10:00+0000'))
-      .mockImplementation((args: any) => new OriginalDate(args));
+    dateSpy.mockImplementation((args: any) => {
+      if (args === undefined) return new OriginalDate('2020-05-19T11:10:00+0000');;
+      return new OriginalDate(args);
+    });
 
     await service.updateExposureStatus();
     expect(server.retrieveDiagnosisKeys).toHaveBeenCalledTimes(14);
