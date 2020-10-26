1970e25cb465609f46f7b64d10df41441ee37429
smcmurtry <smcmurtry@gmail.com>
2020-10-26 10:13:51 -0600

    added some more observable tests

--- src/shared/Observable.spec.ts
+++ src/shared/Observable.spec.ts
@@ -1,7 +1,7 @@
 import {MapObservable, Observable} from './Observable';
 
 describe('Observable', () => {
-  it('handles mutiple synchronous calls and ends with the correct values', async () => {
+  it('handles mutiple synchronous calls and ends with the last value', async () => {
     const handleChange = jest.fn();
     const observableVal = new Observable('off');
     observableVal.observe(handleChange);
@@ -15,10 +15,36 @@ describe('Observable', () => {
     expect(handleChange).toHaveBeenCalledTimes(values.length);
     expect(observableVal.get()).toStrictEqual(values[values.length - 1]);
   });
+
+  it.each(['on', 'off', 'off', 'on', 'on', 'off'])('change handler is called with new value %p when set', async val => {
+    const handleChange = jest.fn();
+    const observableVal = new Observable('off');
+    observableVal.observe(handleChange);
+    observableVal.set(val);
+    expect(handleChange).toHaveBeenCalledWith(val);
+    expect(observableVal.get()).toStrictEqual(val);
+  });
 });
 
 describe('Map Observable', () => {
-  it('handles mutiple synchronous calls and ends with the correct values', async () => {
+  it.each(['on', 'off', 'off', 'on', 'on', 'off', 'on', 'on'])(
+    'both change handlers are called with new value %p when set',
+    async val => {
+      // test using mutliple observe handlers
+      const handleChange1 = jest.fn();
+      const handleChange2 = jest.fn();
+
+      const mapObservableVal = new MapObservable({type: 'status', mode: 'off'});
+      mapObservableVal.observe(handleChange1);
+      mapObservableVal.observe(handleChange2);
+      mapObservableVal.append({mode: val});
+      expect(handleChange1).toHaveBeenCalledWith({mode: val, type: 'status'});
+      expect(handleChange2).toHaveBeenCalledWith({mode: val, type: 'status'});
+      expect(mapObservableVal.get().mode).toStrictEqual(val);
+    },
+  );
+
+  it('handles mutiple synchronous calls and ends with the last value', async () => {
     // test using mutliple observe handlers
     const handleChange1 = jest.fn();
     const handleChange2 = jest.fn();
