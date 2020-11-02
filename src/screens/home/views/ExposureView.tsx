f4c98d8bbe7b44faf0a2716e256f21d3494776dd
Tim Arney <timarney@users.noreply.github.com>
2020-11-02 14:55:26 -0500

    Update exposed screen for non covered regions (#1227)

--- src/screens/home/views/ExposureView.tsx
+++ src/screens/home/views/ExposureView.tsx
@@ -1,10 +1,9 @@
-import React, {useCallback} from 'react';
-import {useNavigation} from '@react-navigation/native';
+import React from 'react';
 import {useI18n} from 'locale';
-import {Text, Box, ButtonSingleLine} from 'components';
-import {useStorage} from 'services/StorageService';
+import {Text} from 'components';
 import {useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
 import {isRegionActive} from 'shared/RegionLogic';
+import {useStorage} from 'services/StorageService';
 import {useRegionalI18n} from 'locale/regional';
 import {ExposedHelpButton} from 'components/ExposedHelpButton';
 
@@ -17,46 +16,39 @@ const ActiveContent = ({text}: {text: string}) => {
   return <Text marginBottom="m">{text}</Text>;
 };
 
-export const ExposureView = ({isBottomSheetExpanded}: {isBottomSheetExpanded: boolean}) => {
+const ExposureText = ({isBottomSheetExpanded}: {isBottomSheetExpanded: boolean}) => {
   const {region} = useStorage();
-  const i18n = useI18n();
   const regionalI18n = useRegionalI18n();
-  const navigation = useNavigation();
   const regionActive = isRegionActive(region, regionalI18n.activeRegions);
-  const onHowToIsolate = useCallback(() => navigation.navigate('HowToIsolate'), [navigation]);
+  const i18n = useI18n();
   const autoFocusRef = useAccessibilityAutoFocus(!isBottomSheetExpanded);
   const activeBodyText = regionalI18n.translate(`RegionContent.ExposureView.Active.${region}.Body`);
 
   return (
-    <BaseHomeView iconName="hand-caution" testID="exposure">
+    <>
       <Text focusRef={autoFocusRef} variant="bodyTitle" marginBottom="m" accessibilityRole="header">
         {i18n.translate('Home.ExposureDetected.Title')}
       </Text>
       <Text marginBottom="m">{i18n.translate('Home.ExposureDetected.Body1')}</Text>
+
       <Text variant="bodyTitle" marginBottom="m" accessibilityRole="header">
-        {i18n.translate(`Home.ExposureDetected.Title2`)}
+        {i18n.translate('Home.ExposureDetected.Title2')}
       </Text>
+
       {regionActive ? (
-        <ActiveContent text={activeBodyText} />
+        <ActiveContent text={activeBodyText} /> /* pulls from region.json */
       ) : (
-        <Text marginBottom="m">
-          <Text>{i18n.translate('Home.ExposureDetected.RegionNotCovered.Body2')}</Text>
-          <Text fontWeight="bold">{i18n.translate('Home.ExposureDetected.RegionNotCovered.Body3')}</Text>
-        </Text>
+        <Text marginBottom="m">{i18n.translate('Home.ExposureDetected.Body2')}</Text>
       )}
+    </>
+  );
+};
 
+export const ExposureView = ({isBottomSheetExpanded}: {isBottomSheetExpanded: boolean}) => {
+  return (
+    <BaseHomeView iconName="hand-caution" testID="exposure">
+      <ExposureText isBottomSheetExpanded={isBottomSheetExpanded} />
       <ExposedHelpButton />
-
-      {!regionActive && (
-        <Box alignSelf="stretch" marginBottom="m">
-          <ButtonSingleLine
-            text={i18n.translate(`Home.ExposureDetected.RegionNotCovered.HowToIsolateCTA`)}
-            variant="bigFlatDarkGrey"
-            onPress={onHowToIsolate}
-            internalLink
-          />
-        </Box>
-      )}
     </BaseHomeView>
   );
 };
