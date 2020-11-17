c54d42711e0fcd6e7c4d2743cd3e276646460351
Tim Arney <tim@line37.com>
2020-11-17 07:44:47 -0500

    clear userStopped from HomeScreen

--- src/screens/home/views/ExposureNotificationsUserStoppedView.tsx
+++ src/screens/home/views/ExposureNotificationsUserStoppedView.tsx
@@ -3,16 +3,19 @@ import {Box, ButtonSingleLine, Text} from 'components';
 import React, {useCallback} from 'react';
 import {useStartExposureNotificationService} from 'services/ExposureNotificationService';
 import {useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
+import {useStorage} from 'services/StorageService';
 
 import {BaseHomeView} from '../components/BaseHomeView';
 
 export const ExposureNotificationsUserStoppedView = ({isBottomSheetExpanded}: {isBottomSheetExpanded: boolean}) => {
   const i18n = useI18n();
+  const {setUserStopped} = useStorage();
   const startExposureNotificationService = useStartExposureNotificationService();
 
   const startEn = useCallback(() => {
+    setUserStopped(false);
     startExposureNotificationService();
-  }, [startExposureNotificationService]);
+  }, [setUserStopped, startExposureNotificationService]);
 
   const onPress = () => {
     return startEn();
