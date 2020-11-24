1f3816b205556755d5d88433a71dbdee40261f01
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-11-24 17:17:30 -0500

    refactor old logging methods (to be replaced)

--- src/shared/log.ts
+++ src/shared/log.ts
@@ -1,62 +1,23 @@
-import AsyncStorage from '@react-native-community/async-storage';
-import {Platform} from 'react-native';
-
-const PLATFORM = Platform.OS;
-const UUID_KEY = 'UUID_KEY';
-
-const getRandomString = (size: number) => {
-  const chars = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'];
-
-  return [...Array(size)].map(_ => chars[(Math.random() * chars.length) | 0]).join('');
-};
-
-const cachedUUID = AsyncStorage.getItem(UUID_KEY)
-  .then(uuid => uuid || getRandomString(8))
-  .then(uuid => {
-    AsyncStorage.setItem(UUID_KEY, uuid);
-    return uuid;
-  })
-  .catch(() => null);
-
-let currentUUID = '';
-
-export const setLogUUID = (uuid: string) => {
-  currentUUID = uuid;
-  AsyncStorage.setItem(UUID_KEY, uuid);
-};
-
-export const getLogUUID = async () => {
-  return currentUUID || (await cachedUUID) || 'unset';
-};
+import {log} from './logging/config';
 
 const isTest = () => {
   return process.env.JEST_WORKER_ID !== undefined;
 };
 
 export const captureMessage = async (message: string, params: {[key in string]: any} = {}) => {
-  //
-  const uuid = await getLogUUID();
-  const finalMessage = `[${uuid}] - ${PLATFORM} ${message}`.replace(/\n/g, '');
-  const finalParams = params;
-
   if (__DEV__ && !isTest()) {
-    console.log(finalMessage, JSON.stringify(finalParams)); // eslint-disable-line no-console
+    log.info({
+      message,
+      payload: params,
+    });
   }
 };
 
 export const captureException = async (message: string, error: any, params: {[key in string]: any} = {}) => {
-  const uuid = await getLogUUID();
-  const finalMessage = `[${uuid}] - ${PLATFORM} Error: ${message}`.replace(/\n/g, '');
-
-  const finalParams = {
-    ...params,
-    error: {
-      message: error && error.message,
-      error,
-    },
-  };
-
   if (__DEV__ && !isTest()) {
-    console.error(finalMessage, finalParams); // eslint-disable-line no-console
+    log.error({
+      message,
+      error,
+    });
   }
 };
