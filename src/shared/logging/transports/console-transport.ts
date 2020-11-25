9e36cef709e0801927a5dc0b2e6bc345a6498d26
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-11-25 15:54:09 -0500

    add colours to console messages

--- src/shared/logging/transports/console-transport.ts
+++ src/shared/logging/transports/console-transport.ts
@@ -1,6 +1,15 @@
 import {transportFunctionType} from 'react-native-logs';
 
 const consoleTransport: transportFunctionType = async (msg, level, _options) => {
+  const colours = {
+    red: '\u001b[31m',
+    black: '\u001b[30m',
+    blue: '\u001b[34m',
+    orange: '\u001b[33m',
+    cyan: '\u001b[36m',
+    reset: '\u001b[0m',
+  };
+
   let message: string;
 
   if (typeof msg === 'string') {
@@ -13,16 +22,19 @@ const consoleTransport: transportFunctionType = async (msg, level, _options) => 
 
   switch (level.text) {
     case 'info':
-      console.info(message); // eslint-disable-line no-console
+      console.info(`${colours.blue}${message}${colours.reset}`); // eslint-disable-line no-console
       break;
     case 'error':
-      console.error(message); // eslint-disable-line no-console
+      console.error(`${colours.red}${message}${colours.reset}`); // eslint-disable-line no-console
       break;
     case 'warn':
-      console.warn(message); // eslint-disable-line no-console
+      console.warn(`${colours.orange}${message}${colours.reset}`); // eslint-disable-line no-console
+      break;
+    case 'debug':
+      console.debug(`${colours.cyan}${message}${colours.reset}`); // eslint-disable-line no-console
       break;
     default:
-      console.log(message); // eslint-disable-line no-console
+      console.log(`${colours.black}${message}${colours.reset}`); // eslint-disable-line no-console
   }
 };
 
