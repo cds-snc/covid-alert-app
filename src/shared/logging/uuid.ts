a12417d6e287f6208707548a450eb2863f4eb2b2
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-11-27 08:16:42 -0500

    Feature - log levels and logging improvements (#1263)
    Used only for local dev + staging
    
    * add log-levels package
    
    * add new LOG_LEVEL env var and LOGGLY_URL
    
    * add new logging transports, config, and break out uuid functionality for re-use
    
    * only call loggly transport if url is set
    
    * set default log level to debug
    
    * simplify levels
    
    * add colours to console messages
    
    * remove silly status + add conditional wrap to transports
    
    Co-authored-by: Tim Arney <tim@line37.com>

---
+++ src/shared/logging/uuid.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+const UUID_KEY = 'UUID_KEY';
+let currentUUID = '';
+
+const getRandomString = (size: number) => {
+  const chars = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'];
+
+  return [...Array(size)].map(_ => chars[(Math.random() * chars.length) | 0]).join('');
+};
+
+const cachedUUID = AsyncStorage.getItem(UUID_KEY)
+  .then(uuid => uuid || getRandomString(8))
+  .then(uuid => {
+    AsyncStorage.setItem(UUID_KEY, uuid);
+    return uuid;
+  })
+  .catch(() => null);
+
+export const setLogUUID = (uuid: string) => {
+  currentUUID = uuid;
+  AsyncStorage.setItem(UUID_KEY, uuid);
+};
+
+export const getLogUUID = async () => {
+  return currentUUID || (await cachedUUID) || 'unset';
+};
