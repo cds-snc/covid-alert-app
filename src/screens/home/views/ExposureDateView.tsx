ee90d92f2eca2d09936e97d3edba2af99e30f9cc
Tim Arney <tim@line37.com>
2020-12-02 06:00:35 -0500

    add default timestamp if forcing the screen

--- src/screens/home/views/ExposureDateView.tsx
+++ src/screens/home/views/ExposureDateView.tsx
@@ -3,10 +3,12 @@ import {Text} from 'components';
 import {useI18n} from 'locale';
 import {useExposureNotificationService} from 'services/ExposureNotificationService';
 import {formatExposedDate} from 'shared/date-fns';
+import {useStorage} from 'services/StorageService';
 
 export const ExposureDateView = () => {
   const i18n = useI18n();
   const dateLocale = i18n.locale === 'fr' ? 'fr-CA' : 'en-CA';
+  const {forceScreen} = useStorage();
   const dateFormatOptions = {
     month: 'short',
     day: 'numeric',
@@ -17,8 +19,11 @@ export const ExposureDateView = () => {
 
   const date = useMemo(() => {
     const timeStamp = exposureNotificationService.getExposureDetectedAt();
+
     if (timeStamp)
       return formatExposedDate(dateLocale, new Date(timeStamp).toLocaleString(dateLocale, dateFormatOptions));
+    else if (forceScreen)
+      return formatExposedDate(dateLocale, new Date().toLocaleString(dateLocale, dateFormatOptions));
   }, [dateFormatOptions, dateLocale, exposureNotificationService]);
 
   return date ? (
