82ac22050773b0039f11569ba3462d5dc1e34374
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-12-10 11:14:24 -0500

    exit if no arguments, output current crontab

--- scripts/adb-mon-register.sh
+++ scripts/adb-mon-register.sh
@@ -1,7 +1,18 @@
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied."
+    exit -1
+fi
+
 BASE_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 SCRIPT=$BASE_DIR/adb-mon.sh
 LOGFILE=$BASE_DIR/adb-mon.log
 touch $LOGFILE
+
+echo "======= CURRENT CRONTAB ======="
+echo "$(crontab -l)"
+echo "======= CURRENT CRONTAB ======="
 
 CRON="0 * * * * $SCRIPT >> $LOGFILE 2>&1"
 
