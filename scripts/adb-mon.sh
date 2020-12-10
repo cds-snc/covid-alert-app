ed88c3e5c9932be3a6cc5b6409ac57b0195ae0c5
Andréas <andreas@klef.ca>
2020-12-10 12:06:45 -0500

    Support for multiple devices

--- scripts/adb-mon.sh
+++ scripts/adb-mon.sh
@@ -7,32 +7,65 @@
 ## Description: script to log Power Monitor service info on Android device
 ## Intended audience: devs testing on android
 ## Usage: to be called by a scheduler every hour
+## Parameters: ['UUID'] ['DEVICE_SERIAL']
 
 
 # POLL DEVICE
+# possible bucket values are:
+# 5: exempt
+# 10: active
+# 20: working-set
+# 30: frequent
+# 40: rare
 ADBStandByBuckets=$( adb shell am get-standby-bucket 2>&1);
 
 
-
-if `grep -q 'error' <<< "$ADBStandByBuckets"`; then
+## ERROR CHECKING
+if `grep -q 'no devices' <<< "$ADBStandByBuckets"`; then
+  echo "Could not find devices, exiting..."
+  exit -1;
+elif `grep -q 'more than one' <<< "$ADBStandByBuckets"`; then
+  if [ $# -eq 0 ]; then
+    echo "Could not differentiate multiple devices, exiting..."
+    exit -1;
+  fi
+  MULTI=true
+elif (( "${#1}" < 8 )); then
+    echo "Provided UUID is not of the expected length, exiting..."
+    exit -1;
+elif [[ "$2" =~ [^a-zA-Z0-9] ]]; then
+    echo "Format of provided serial is not alphanumeric as expected, exiting..."
+    exit -1;
+elif `grep -q 'error' <<< "$ADBStandByBuckets"`; then
   echo "Could not connect to adb, exiting..."
   exit -1;
-else
-  echo "adb connection established..."
-  # possible bucket values are:
-  # 5: exempt
-  # 10: active
-  # 20: working-set
-  # 30: frequent
-  # 40: rare
-  ADBStandByBuckets=$( echo "$ADBStandByBuckets" | grep covid );
+fi 
+
+## POLL DEVICES
+echo "adb connection established..."
+DEVICES=$( adb devices );
+
+## DEVICE VALIDATION
+if [ "$MULTI" = true ]; then
+  if [ $# -lt 2 ]; then
+    echo "Please use format './adb-mon [UUID] [DEVICE_SERIAL]', exiting..."
+    exit -1;
+  elif `! grep -q "$2" <<< "$DEVICES"`; then
+    echo "Could not match provided serial to local device, exiting..."
+    exit -1;
+  else
+    DEVICE_SERIAL="$2"
+    SERIAL_STR="-s $DEVICE_SERIAL"
+  fi
 fi
 
+ADBStandByBuckets=`adb $SERIAL_STR $shell am get-standby-bucket 2>&1 | grep covid`;
+
 # Dump all scheduled jobs, then filter
-JobSchedulerLogs=`adb shell dumpsys jobscheduler | grep -A 20 "JOB" | grep -A 23 ca.gc.hcsc.canada.covidalert`;
+JobSchedulerLogs=`adb $SERIAL_STR shell dumpsys jobscheduler | grep -A 20 "JOB" | grep -A 23 ca.gc.hcsc.canada.covidalert`;
 
 # Empty string checking
-if [ -z "$JobSchedulerLogs" ] ; then
+if [ -z "$JobSchedulerLogs" ]; then
   echo "COVID Alert uninstalled from device, exiting."
   exit -1;
 fi
@@ -51,7 +84,7 @@ else
 fi
 
 # Empty string checking
-if [ -z "$DEVICE_UUID" ] ; then
+if [ -z "$DEVICE_UUID" ]; then
   echo "No UUID, exiting."
   exit -1;
 else
@@ -64,13 +97,14 @@ fi
 #DEVICE_IP=0.0.0.0; #can be set manually
 
 
-
 JSONlogData=$( jq -n \
   --arg u "$USER" \
+  --arg t "$LOCAL_TIME" \
   --arg uuid "$DEVICE_UUID" \
+  --arg serial "$DEVICE_SERIAL" \
   --arg stbyBkt "$ADBStandByBuckets" \
   --arg schdJbs "$JobSchedulerLogs" \
-  '{user: $u, UUID: $uuid, standbyBucket: $stbyBkt, scheduledJobs: $schdJbs}' );
+  '{user: $u, localTime: $t, UUID: $uuid, serial: $serial, standbyBucket: $stbyBkt, scheduledJobs: $schdJbs}' );
 
 
 # POST THE LOGS
