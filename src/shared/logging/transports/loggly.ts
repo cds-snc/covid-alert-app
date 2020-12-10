08485e2a8d4466a0687609c12d7f1fdd48c5b7b2
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-12-10 15:00:09 -0500

    add lastChecked and currentStatus to logs

--- src/shared/logging/transports/loggly.ts
+++ src/shared/logging/transports/loggly.ts
@@ -1,6 +1,9 @@
 import {Platform} from 'react-native';
 import {APP_VERSION_CODE, APP_VERSION_NAME, LOGGLY_URL} from 'env';
 import {transportFunctionType} from 'react-native-logs';
+import AsyncStorage from '@react-native-community/async-storage';
+import {getCurrentDate, minutesBetween} from 'shared/date-fns';
+import {EXPOSURE_STATUS} from 'services/ExposureNotificationService';
 
 import {getLogUUID} from '../uuid';
 
@@ -9,6 +12,19 @@ const logglyTransport: transportFunctionType = async (msg, level, _options) => {
   const platform = Platform.OS;
   const versionCode = APP_VERSION_CODE;
   const versionName = APP_VERSION_NAME;
+  let status = '';
+  let minutes = 'n/a';
+
+  const today = getCurrentDate();
+  const exposureStatusJson = await AsyncStorage.getItem(EXPOSURE_STATUS);
+
+  if (exposureStatusJson) {
+    const exposureStatus = JSON.parse(exposureStatusJson);
+    const lastCheckedTimestamp = exposureStatus.lastChecked.timestamp;
+    const lastCheckedDate = new Date(lastCheckedTimestamp);
+    minutes = Math.ceil(minutesBetween(lastCheckedDate, today)).toString();
+    status = exposureStatus.type;
+  }
 
   // used for staging env - never production
   if (LOGGLY_URL) {
@@ -25,6 +41,8 @@ const logglyTransport: transportFunctionType = async (msg, level, _options) => {
         level,
         versionCode,
         versionName,
+        currentStatus: status,
+        lastCheckedMinutesAgo: minutes,
       }),
     }).catch(error => {
       console.log(error); // eslint-disable-line no-console
