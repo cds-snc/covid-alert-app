74293396289765d0f18ea3b7064b20cccfc17742
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-12-10 11:03:48 -0500

    better path resolution and register the job

--- scripts/adb-mon-register.sh
+++ scripts/adb-mon-register.sh
@@ -1,11 +1,11 @@
-BASE_DIR=`pwd -P`
-SCRIPT=$BASE_DIR/scripts/adb-mon.sh
-LOGFILE=$BASE_DIR/scripts/adb-mon.log
+BASE_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPT=$BASE_DIR/adb-mon.sh
+LOGFILE=$BASE_DIR/adb-mon.log
 touch $LOGFILE
 
-CRON="@hourly $SCRIPT >> $LOGFILE 2>&1"
+CRON="0 * * * * $SCRIPT >> $LOGFILE 2>&1"
 
 echo $SCRIPT
 echo "$CRON"
 
-(crontab -l ; echo $CRON)| crontab -
+(crontab -l ; echo "$CRON")| crontab -
