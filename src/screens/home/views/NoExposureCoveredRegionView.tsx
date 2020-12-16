153a767d0331938d29584b5aec7d407256d27c75
Tim Arney <tim@line37.com>
2020-12-16 18:29:43 -0500

    add whatsnew view

--- src/screens/home/views/NoExposureCoveredRegionView.tsx
+++ src/screens/home/views/NoExposureCoveredRegionView.tsx
@@ -7,6 +7,7 @@ import {useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
 
 import {AllSetView} from '../components/AllSetView';
 import {BaseHomeView} from '../components/BaseHomeView';
+import {WhatsNew} from '../components/WhatsNewView';
 import {StyleSheet, Platform} from 'react-native';
 
 export const NoExposureCoveredRegionView = ({isBottomSheetExpanded}: {isBottomSheetExpanded: boolean}) => {
@@ -50,34 +51,7 @@ export const NoExposureCoveredRegionView = ({isBottomSheetExpanded}: {isBottomSh
           />
         </Box>
       </Box>
-
-      <Box alignSelf="stretch" marginTop="m" style={styles.roundedBox2}>
-        <Box paddingHorizontal="m" paddingVertical="m">
-          <Text
-            variant="bodySubTitle"
-            color="bodyText"
-            marginBottom="m"
-            accessibilityRole="header"
-            accessibilityAutoFocus
-          >
-            {i18n.translate('Home.NoExposureDetected.WhatsNew.Title')}
-          </Text>
-
-          <TextMultiline
-            variant="bodyText"
-            color="bodyText"
-            marginBottom="m"
-            text={i18n.translate('Home.NoExposureDetected.WhatsNew.Body1')}
-          />
-
-          <TextMultiline
-            variant="bodyText"
-            color="bodyText"
-            marginBottom="m"
-            text={i18n.translate('Home.NoExposureDetected.WhatsNew.Body2')}
-          />
-        </Box>
-      </Box>
+      <WhatsNew />
     </BaseHomeView>
   );
 };
