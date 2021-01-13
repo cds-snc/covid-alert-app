fefeee54381e292f0b524546a062a492f25075a8
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-01-13 16:40:16 -0500

    add a couple ids for testing

--- e2e/oneTimeCode.e2e.js
+++ e2e/oneTimeCode.e2e.js
@@ -8,13 +8,21 @@ describe('Test one time code flow', () => {
     await device.takeScreenshot('BottomSheet');
     await element(by.text('Enter your one-time key')).tap();
     await device.takeScreenshot('OTC-Step1');
+
+    await element(by.id('Step0Body')).swipe('up');
+    await element(by.id('Step0Body')).swipe('up');
     await element(by.text('Next')).tap();
     await device.takeScreenshot('OTC-Step2');
+
     await element(by.id('textInput')).tap();
     await device.takeScreenshot('OTC-Step2-focussed');
+
     await element(by.id('textInput')).typeText('ABC123ABCD');
+    await element(by.id('pageTitle')).tap();
+
     await element(by.id('submitTEKButton')).tap();
     await device.takeScreenshot('OTC-Step2-error');
+
     await expect(element(by.text('OK'))).toBeVisible();
   });
 });
--- src/screens/datasharing/FormScreen.tsx
+++ src/screens/datasharing/FormScreen.tsx
@@ -77,6 +77,7 @@ export const FormScreen = () => {
       <Box marginHorizontal="m" marginBottom="l">
         <StepXofY currentStep={1} />
         <Text
+          testID="pageTitle"
           variant="bodyTitle"
           color="overlayBodyText"
           accessibilityRole="header"
