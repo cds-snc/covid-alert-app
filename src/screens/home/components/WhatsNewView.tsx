826a68b92b0521a1252bcd4704ce7ad70dff5e2a
Omar Tehsin <omartehsin@Omars-MacBook-Pro.local>
2021-01-13 11:02:09 -0500

    updated whats new view

--- src/screens/home/components/WhatsNewView.tsx
+++ src/screens/home/components/WhatsNewView.tsx
@@ -1,45 +1,30 @@
 import React from 'react';
-import {Box, Text, TextMultiline} from 'components';
+import {Text, TextMultiline, RoundedBox} from 'components';
 import {useI18n} from 'locale';
-import {StyleSheet, Platform} from 'react-native';
+import {Platform} from 'react-native';
 
 export const WhatsNew = () => {
   const i18n = useI18n();
 
   return Platform.OS === 'ios' ? null : (
-    <Box alignSelf="stretch" marginTop="m" style={styles.roundedBox2}>
-      <Box paddingHorizontal="m" paddingVertical="m">
-        <Text
-          variant="bodySubTitle"
-          color="bodyText"
-          marginBottom="m"
-          accessibilityRole="header"
-          accessibilityAutoFocus
-        >
-          {i18n.translate('Home.NoExposureDetected.WhatsNew.Title')}
-        </Text>
+    <RoundedBox isBoxOne={false}>
+      <Text variant="bodySubTitle" color="bodyText" marginBottom="m" accessibilityRole="header" accessibilityAutoFocus>
+        {i18n.translate('Home.NoExposureDetected.WhatsNew.Title')}
+      </Text>
 
-        <TextMultiline
-          variant="bodyText"
-          color="bodyText"
-          marginBottom="m"
-          text={i18n.translate('Home.NoExposureDetected.WhatsNew.Body1')}
-        />
+      <TextMultiline
+        variant="bodyText"
+        color="bodyText"
+        marginBottom="m"
+        text={i18n.translate('Home.NoExposureDetected.WhatsNew.Body1')}
+      />
 
-        <TextMultiline
-          variant="bodyText"
-          color="bodyText"
-          marginBottom="m"
-          text={i18n.translate('Home.NoExposureDetected.WhatsNew.Body2')}
-        />
-      </Box>
-    </Box>
+      <TextMultiline
+        variant="bodyText"
+        color="bodyText"
+        marginBottom="m"
+        text={i18n.translate('Home.NoExposureDetected.WhatsNew.Body2')}
+      />
+    </RoundedBox>
   );
 };
-
-const styles = StyleSheet.create({
-  roundedBox2: {
-    borderRadius: 10,
-    backgroundColor: 'white',
-  },
-});
