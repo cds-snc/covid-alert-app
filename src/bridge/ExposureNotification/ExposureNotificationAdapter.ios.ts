77c03e565d8e7e5203177de7cfde77a65f32a459
Stephen McMurtry <smcmurtry@gmail.com>
2021-01-18 11:34:50 -0700

    wip for loop bug fix

--- src/bridge/ExposureNotification/ExposureNotificationAdapter.ios.ts
+++ src/bridge/ExposureNotification/ExposureNotificationAdapter.ios.ts
@@ -2,6 +2,7 @@ import {log} from 'shared/logging/config';
 import {unzip} from 'react-native-zip-archive';
 
 import {ExposureConfiguration, ExposureNotificationAPI, ExposureSummary, ExposureWindow} from './types';
+import { RESULTS } from 'react-native-permissions';
 
 export default function ExposureNotificationAdapter(exposureNotificationAPI: ExposureNotificationAPI) {
   return {
@@ -42,13 +43,31 @@ export default function ExposureNotificationAdapter(exposureNotificationAPI: Exp
         message: 'exposureNotificationAPI.detectExposure before for loop',
         payload: {diagnosisKeysURLs},
       });
+
+      const delay = (delayTimeMs: number) => new Promise(resolve => setTimeout(resolve, delayTimeMs));
+
+      const forLoop = async () => {
+        const i = 0;
+        const results: ExposureWindow[] = [];
+        for (const keyZipUrl of diagnosisKeysURLs.reverse()) {
+          const exposureWindows = await mapKeysToExposureWindows(keyZipUrl);
+          results.concat(exposureWindows);
+          if (i === diagnosisKeysURLs.length - 1) {
+            return results;
+          }
+          console.log('start pause');
+          await delay(20000);
+          console.log('done pause');
+        }
+      };
+
       const exposureWindowArray = Promise.all(diagnosisKeysURLs.map(mapKeysToExposureWindows));
       const allExposureWindows = exposureWindowArray.then(arrays => {
         // https://stackoverflow.com/questions/42173350/synchronous-and-asynchronous-loops-in-javascript
         // eslint-disable-next-line prefer-spread
         return [].concat.apply([], arrays);
       });
-
+      // const allExposureWindows = await forLoop();
       log.debug({
         category: 'configuration',
         payload: configuration,
