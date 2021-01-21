3b9e20b9840bd2ee9b09645b968d4863dfd11367
Tim Arney <tim@line37.com>
2021-01-21 06:43:33 -0500

    prep for 1371

--- src/shared/metrics.tsx
+++ src/shared/metrics.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-case-declarations */
 import {log} from 'shared/logging/config';
+import {APP_VERSION_CODE, LOGGLY_URL} from 'env';
+import {useMemo} from 'react';
 import {
   useExposureStatus,
   ExposureStatusType,
@@ -9,8 +11,8 @@ import {
 } from 'services/ExposureNotificationService';
 import {useStorage} from 'services/StorageService';
 import {useNotificationPermissionStatus} from 'screens/home/components/NotificationPermissionStatus';
-import {LOGGLY_URL} from 'env';
 import {getCurrentDate} from 'shared/date-fns';
+import {Platform} from 'react-native';
 
 const checkStatus = (exposureStatus: ExposureStatus): {exposed: boolean} => {
   if (exposureStatus.type === ExposureStatusType.Exposed) {
@@ -50,11 +52,52 @@ interface EnToggleMetric extends Metric {
 
 type Payload = Metric | OnboardedMetric | OTKMetric | EnToggleMetric;
 
-// note this can used direct i.e. outside of the React hook
-export const sendMetricEvent = (payload: Payload) => {
-  if (LOGGLY_URL) {
-    log.debug({category: 'metrics', message: payload.identifier, payload});
+// this will be replaces with DefaultMetricsJsonSerializer #1371
+export class Logger {
+  private appVersion: number;
+  private appOs: string;
+
+  constructor(appVersion: number, appOs: string) {
+    console.log('===== new logger ======');
+    this.appVersion = appVersion;
+    this.appOs = appOs;
   }
+
+  log(payload: any) {
+    console.log(payload); // eslint-disable-line no-console
+
+    if (LOGGLY_URL) {
+      log.debug({
+        category: 'metrics',
+        message: payload.identifier,
+        payload: {appVersion: this.appVersion, appOs: this.appOs},
+      });
+    }
+  }
+}
+
+// this will be replaced with DefaultMetricsService #1371
+class MetricsService implements MetricsService {
+  static initialize(logger: Logger) {
+    console.log('===== MetricsService initialize ======');
+    return new MetricsService(logger);
+  }
+
+  private logger: any;
+
+  private constructor(logger: any) {
+    this.logger = logger;
+  }
+
+  sendMetrics(timestamp: number, identifier: string, region: string | undefined, data: {}): Promise<void> {
+    return this.logger.log({timestamp, identifier, region, data});
+  }
+}
+
+// note this can used direct i.e. outside of the React hook
+export const sendMetricEvent = (payload: Payload, metricsService: MetricsService) => {
+  const {timestamp, identifier, region, ...data} = payload;
+  metricsService.sendMetrics(timestamp, identifier, region, data);
 };
 
 export const useMetrics = () => {
@@ -63,6 +106,12 @@ export const useMetrics = () => {
   const [systemStatus] = useSystemStatus();
   const [notificationStatus] = useNotificationPermissionStatus();
 
+  const metricsService = useMemo(() => {
+    const metricsJsonSerializer = new Logger(APP_VERSION_CODE, Platform.OS);
+    const metricsService = MetricsService.initialize(metricsJsonSerializer);
+    return metricsService;
+  }, []);
+
   const addEvent = (eventType: EventTypeMetric) => {
     const initialPayload: Metric = {identifier: eventType, timestamp: getCurrentDate().getTime(), region};
 
@@ -70,7 +119,7 @@ export const useMetrics = () => {
       case EventTypeMetric.Installed:
       case EventTypeMetric.Exposed:
         const metric: Metric = {...initialPayload};
-        sendMetricEvent(metric);
+        sendMetricEvent(metric, metricsService);
         break;
       case EventTypeMetric.Onboarded:
         const onboardedMetric: OnboardedMetric = {
@@ -78,18 +127,18 @@ export const useMetrics = () => {
           pushnotification: notificationStatus,
           frameworkenabled: systemStatus === SystemStatus.Active,
         };
-        sendMetricEvent(onboardedMetric);
+        sendMetricEvent(onboardedMetric, metricsService);
         break;
       case EventTypeMetric.OtkNoDate:
       case EventTypeMetric.OtkWithDate:
         const otkMetric: OTKMetric = {...checkStatus(exposureStatus), ...initialPayload};
-        sendMetricEvent(otkMetric);
+        sendMetricEvent(otkMetric, metricsService);
         break;
       case EventTypeMetric.EnToggle:
         const enToggleMetric: EnToggleMetric = userStopped
           ? {...initialPayload, state: false}
           : {...initialPayload, state: true};
-        sendMetricEvent(enToggleMetric);
+        sendMetricEvent(enToggleMetric, metricsService);
         break;
     }
   };
