c0451a2b36b12dfc243d4239553aa7020f767838
Tim Arney <tim@line37.com>
2021-01-21 15:02:34 -0500

    fix ts errors

--- src/shared/MetricsProvider.tsx
+++ src/shared/MetricsProvider.tsx
@@ -1,48 +1,13 @@
 import React, {useContext} from 'react';
-import {log} from 'shared/logging/config';
-import {APP_VERSION_CODE, LOGGLY_URL} from 'env';
+import {APP_VERSION_CODE} from 'env';
 import {Platform} from 'react-native';
 import {DefaultMetricsService, MetricsService} from 'services/MetricsService/MetricsService';
-import {DefaultMetricsJsonSerializer, MetricsJsonSerializer} from 'services/MetricsService/MetricsJsonSerializer';
+import {DefaultMetricsJsonSerializer} from 'services/MetricsService/MetricsJsonSerializer';
 
 interface MetricsProviderProps {
   service?: MetricsService;
   children?: React.ReactElement;
 }
-
-// this will be replaced with DefaultMetricsService #1371
-/* class MetricsService implements MetricsService {
-  private logger: any;
-
-  public constructor(logger: any) {
-    this.logger = logger;
-  }
-
-  public sendMetrics(timestamp: number, identifier: string, region: string | undefined, data: {}): Promise<void> {
-    return this.logger.log({timestamp, identifier, region, data});
-  }
-}*/
-
-// this will be replaces with DefaultMetricsJsonSerializer #1371
-/* export class Logger {
-  private appVersion: number;
-  private appOs: string;
-
-  constructor(appVersion: number, appOs: string) {
-    this.appVersion = appVersion;
-    this.appOs = appOs;
-  }
-
-  public log(payload: any) {
-    if (LOGGLY_URL) {
-      log.debug({
-        category: 'metrics',
-        message: payload.identifier,
-        payload: {appVersion: this.appVersion, appOs: this.appOs, payload},
-      });
-    }
-  }
-}*/
 
 const MetricsContext = React.createContext<MetricsProviderProps | undefined>(undefined);
 
