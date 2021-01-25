654e277df076460de22a7526b5ef6669784b10c9
Omar Tehsin <omartehsin@gmail.com>
2021-01-25 10:38:49 -0500

    UI changes

--- src/screens/home/views/QRCodeView.tsx
+++ src/screens/home/views/QRCodeView.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import {BaseDataSharingView} from '../../datasharing/components/BaseDataSharingView';
-import {Box, Button, Text} from 'components';
+import {Box, Button, Text, Icon} from 'components';
 import {useStorage} from 'services/StorageService';
+import {StyleSheet} from 'react-native';
+import {log} from 'shared/logging/config';
 
 export const QRCodeView = ({route}: any) => {
   const {id, name} = route.params;
-  const {checkInID, setCheckIn} = useStorage();
+  const {checkInID, setCheckIn, removeCheckIn} = useStorage();
   setCheckIn(id.toString());
   return (
     <BaseDataSharingView showBackButton={false}>
-      <Box paddingHorizontal="m">
-        <Text>Successful Check-in</Text>
-        <Text variant="bodyTitle" marginBottom="l" accessibilityRole="header" accessibilityAutoFocus>
-          {urlToString(name)}
-        </Text>
-        <Button variant="thinFlat" text="Storage" onPress={() => console.log('Check In: ', checkInID)} />
+      <Box paddingHorizontal="m" alignItems="center">
+        <Box paddingBottom="l">
+          <Icon name="icon-green-check" height={120} width={150} />
+          <Text variant="bodySubTitle" marginTop="m">
+            Successful Check-in
+          </Text>
+        </Box>
+        <Box paddingBottom="l">
+          <Text variant="bodyTitle" marginBottom="m" accessibilityRole="header" accessibilityAutoFocus>
+            {urlToString(name)}
+          </Text>
+          <Text variant="bodySubTitle" marginBottom="xs">
+            Date
+          </Text>
+        </Box>
+        <Text marginBottom="m"> Thank you for scanning. You have successfully checked in</Text>
+
+        <Button
+          variant="thinFlat"
+          text="Check Storage"
+          onPress={() => log.debug({category: 'debug', payload: checkInID})}
+        />
+        {/* <Button variant="thinFlat" text="Cancel Check In" onPress={() => log.debug({category: 'debug', payload: checkInID})} /> */}
       </Box>
     </BaseDataSharingView>
   );
@@ -24,3 +43,15 @@ function urlToString(url: string): string {
   const title = url.replace('_', ' ');
   return title;
 }
+
+const styles = StyleSheet.create({
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 36,
+  },
+  bottomButton: {
+    position: 'absolute',
+    bottom: 0,
+  },
+});
