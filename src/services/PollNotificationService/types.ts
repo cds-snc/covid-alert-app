919a8947bcc770bd922c8014af327d6f8b4b9cbf
James Eberhardt <james.eberhardt@cds-snc.ca>
2021-01-29 14:06:00 -0500

    Updated interface.

--- src/services/PollNotificationService/types.ts
+++ src/services/PollNotificationService/types.ts
@@ -1,9 +1,10 @@
-import { Locale } from "react-native-localize";
-import { Region } from "shared/Region";
-
-export type NotificationMessage = {
-  id: string,
-  title: any,
-  message: any,
-  target_regions: [Region],
+/* eslint-disable @typescript-eslint/camelcase */
+export interface NotificationMessage {
+  id: string;
+  title: any;
+  message: any;
+  target_regions: any;
+  target_version: any;
+  expires_at: any;
+  priority: number;
 }
