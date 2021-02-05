c05063046dd535d6dde2fb725ebc54fd56989937
Stephen McMurtry <smcmurtry@gmail.com>
2021-02-05 12:06:23 -0700

    added time logic to outbreak exposure check

--- src/shared/qr.ts
+++ src/shared/qr.ts
@@ -19,13 +19,29 @@ export enum OutbreakStatusType {
 export interface ExposedLocationData {
   id: string;
   name: string;
-  startDate: string;
-  endDate: string;
+  startTime: string;
+  endTime: string;
 }
 export interface OutbreakStatus {
   type: OutbreakStatusType;
   lastChecked: number;
 }
+
+interface TimeWindow {
+  start: number;
+  end: number;
+}
+
+interface Data {
+  id: string;
+  outbreakData: ExposedLocationData[];
+  checkInData: CheckInData[];
+  matchCount: number;
+  matchedCheckIns: CheckInData[];
+  mostRecentCheckOut?: number;
+}
+
+const ONE_HOUR_IN_MS = 60 * 60 * 1000;
 
 export const initialOutbreakStatus = {type: OutbreakStatusType.Monitoring, lastChecked: 0};
 
@@ -50,25 +66,65 @@ export const getNewOutbreakStatus = async (checkInHistory: CheckInData[]): Promi
     return false;
   });
 
-  const matches = checkInLocationMatches.filter(checkIn => {
-    const checkInTime = new Date(checkIn.timestamp);
-    const timeAndLocationMatches = outbreakLocations
-      .filter(location => location.id === checkIn.id)
-      .filter(location => checkInTime > new Date(location.startDate) && checkInTime < new Date(location.endDate));
-    if (timeAndLocationMatches.length > 0) {
-      return true;
-    }
-    return false;
+  if (checkInLocationMatches.length === 0) {
+    return createOutbreakStatus({exposed: false});
+  }
+  const relevantOutbreakIds = checkInLocationMatches.map(data => data.id);
+  const uniqueRelevantOutbreakIds = Array.from(new Set(relevantOutbreakIds));
+  const relevantOutbreakData = outbreakLocations.filter(
+    location => uniqueRelevantOutbreakIds.indexOf(location.id) > -1,
+  );
+  const relevantCheckInData = checkInHistory.filter(data => uniqueRelevantOutbreakIds.indexOf(data.id) > -1);
+
+  const calcData: Data[] = uniqueRelevantOutbreakIds.map(id => {
+    return {
+      id,
+      outbreakData: relevantOutbreakData.filter(data => data.id === id),
+      checkInData: relevantCheckInData.filter(data => data.id === id),
+      matchCount: 0,
+      matchedCheckIns: [],
+    };
   });
 
-  log.debug({message: 'outbreak location matches', payload: {matches}});
+  calcData.forEach(data => {
+    for (const checkIn of data.checkInData) {
+      for (const outbreak of data.outbreakData) {
+        const window1: TimeWindow = {
+          start: checkIn.timestamp,
+          end: checkIn.timestamp + ONE_HOUR_IN_MS,
+        };
+        const window2: TimeWindow = {
+          start: new Date(outbreak.startTime).getTime(),
+          end: new Date(outbreak.endTime).getTime(),
+        };
+        if (doTimeWindowsOverlap(window1, window2)) {
+          data.matchCount += 1;
+          data.matchedCheckIns.push(checkIn);
+        }
+      }
+    }
+  });
+  const matches = calcData.filter(data => data.matchCount > 0);
 
-  const newOutbreakStatusType = matches.length > 0 ? OutbreakStatusType.Exposed : OutbreakStatusType.Monitoring;
+  log.debug({message: 'outbreak matches', payload: {matches}});
 
-  const newOutbreakStatus = {
-    type: newOutbreakStatusType,
+  return createOutbreakStatus({exposed: matches.length > 0});
+};
+
+export const createOutbreakStatus = ({exposed}: {exposed: boolean}) => {
+  const newOutbreakStatus: OutbreakStatus = {
+    type: exposed ? OutbreakStatusType.Exposed : OutbreakStatusType.Monitoring,
     lastChecked: getCurrentDate().getTime(),
   };
-  log.debug({message: 'getNewOutbreakStatus', payload: {newOutbreakStatus}});
   return newOutbreakStatus;
+};
+
+export const doTimeWindowsOverlap = (window1: TimeWindow, window2: TimeWindow) => {
+  if (window1.start >= window2.start && window1.start <= window2.end) {
+    return true;
+  }
+  if (window1.end >= window2.start && window1.end <= window2.end) {
+    return true;
+  }
+  return false;
 };
