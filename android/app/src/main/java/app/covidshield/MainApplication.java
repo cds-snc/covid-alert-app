651f62e1bdba45f621aa54a6df86b18b7e1eccc6
Tim Arney <timarney@users.noreply.github.com>
2021-02-15 10:55:47 -0500

    Remove flipper (#1416)
    * initial cleanup
    
    * remove FB_SONARKIT_ENABLED

--- android/app/src/main/java/app/covidshield/MainApplication.java
+++ android/app/src/main/java/app/covidshield/MainApplication.java
@@ -57,34 +57,6 @@ public class MainApplication extends Application implements ReactApplication {
             }
         };
 
-    /**
-     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-     */
-    private static void initializeFlipper(
-        Context context, ReactInstanceManager reactInstanceManager) {
-        if (BuildConfig.DEBUG) {
-            try {
-                /*
-                    We use reflection here to pick up the class that initializes Flipper,
-                    since Flipper library is not available in release mode
-                */
-                Class<?> aClass = Class.forName("app.covidshield.ReactNativeFlipper");
-                aClass
-                    .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-                    .invoke(null, context, reactInstanceManager);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     @Override
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
@@ -95,6 +67,5 @@ public class MainApplication extends Application implements ReactApplication {
         instance = this;
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
-        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
 }
