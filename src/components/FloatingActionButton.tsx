5bda4cd284b93c96d6f899d6a59846163c316f78
Omar Tehsin <omartehsin@gmail.com>
2021-02-16 16:55:59 -0500

    lint fix

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
+import {StyleSheet, TouchableOpacity, Text} from 'react-native';
 
 import {Icon, IconName} from './Icon';
 import {Box} from './Box';
