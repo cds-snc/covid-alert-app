67e5922be152b295615863f0f4021aa29f140aca
omartehsin1 <omartehsin@gmail.com>
2021-02-17 13:27:17 -0500

    rounded box component

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,9 +1,9 @@
-import React, {useCallback, useEffect, useState, useRef, useLayoutEffect} from 'react';
-import {useNetInfo} from '@react-native-community/netinfo';
-import {useNavigation} from '@react-navigation/native';
-import {BottomSheet, BottomSheetBehavior, Box} from 'components';
-import {DevSettings, Linking, Animated} from 'react-native';
-import {TEST_MODE, QR_ENABLED} from 'env';
+import React, { useCallback, useEffect, useState, useRef, useLayoutEffect } from 'react';
+import { useNetInfo } from '@react-native-community/netinfo';
+import { useNavigation } from '@react-navigation/native';
+import { BottomSheet, BottomSheetBehavior, Box } from 'components';
+import { DevSettings, Linking, Animated } from 'react-native';
+import { TEST_MODE, QR_ENABLED } from 'env';
 import {
   ExposureStatusType,
   SystemStatus,
@@ -13,66 +13,67 @@ import {
   useExposureNotificationSystemStatusAutomaticUpdater,
   useSystemStatus,
 } from 'services/ExposureNotificationService';
-import {useStorage} from 'services/StorageService';
-import {RegionCase} from 'shared/Region';
-import {getRegionCase} from 'shared/RegionLogic';
-import {usePrevious} from 'shared/usePrevious';
-import {ForceScreen} from 'shared/ForceScreen';
-import {useRegionalI18n} from 'locale';
-import {OutbreakStatusType} from 'shared/qr';
-import {useOutbreakService} from 'shared/OutbreakProvider';
+import { useStorage } from 'services/StorageService';
+import { RegionCase } from 'shared/Region';
+import { getRegionCase } from 'shared/RegionLogic';
+import { usePrevious } from 'shared/usePrevious';
+import { ForceScreen } from 'shared/ForceScreen';
+import { useRegionalI18n } from 'locale';
+import { OutbreakStatusType } from 'shared/qr';
+import { useOutbreakService } from 'shared/OutbreakProvider';
 
-import {useDeepLinks} from '../qr/utils';
+import { useDeepLinks } from '../qr/utils';
 
-import {BluetoothDisabledView} from './views/BluetoothDisabledView';
-import {CollapsedOverlayView} from './views/CollapsedOverlayView';
-import {DiagnosedShareView} from './views/DiagnosedShareView';
-import {DiagnosedView} from './views/DiagnosedView';
-import {DiagnosedShareUploadView} from './views/DiagnosedShareUploadView';
-import {ExposureNotificationsDisabledView} from './views/ExposureNotificationsDisabledView';
-import {ExposureNotificationsUnauthorizedView} from './views/ExposureNotificationsUnauthorizedView';
-import {ExposureView} from './views/ExposureView';
-import {NoExposureUncoveredRegionView} from './views/NoExposureUncoveredRegionView';
-import {NoExposureCoveredRegionView} from './views/NoExposureCoveredRegionView';
-import {NoExposureNoRegionView} from './views/NoExposureNoRegionView';
-import {NetworkDisabledView} from './views/NetworkDisabledView';
-import {OverlayView} from './views/OverlayView';
-import {FrameworkUnavailableView} from './views/FrameworkUnavailableView';
-import {ExposureNotificationsUserStoppedView} from './views/ExposureNotificationsUserStoppedView';
-import {UnknownProblemView} from './views/UnknownProblemView';
+import { BluetoothDisabledView } from './views/BluetoothDisabledView';
+import { CollapsedOverlayView } from './views/CollapsedOverlayView';
+import { DiagnosedShareView } from './views/DiagnosedShareView';
+import { DiagnosedView } from './views/DiagnosedView';
+import { DiagnosedShareUploadView } from './views/DiagnosedShareUploadView';
+import { ExposureNotificationsDisabledView } from './views/ExposureNotificationsDisabledView';
+import { ExposureNotificationsUnauthorizedView } from './views/ExposureNotificationsUnauthorizedView';
+import { ExposureView } from './views/ExposureView';
+import { NoExposureUncoveredRegionView } from './views/NoExposureUncoveredRegionView';
+import { NoExposureCoveredRegionView } from './views/NoExposureCoveredRegionView';
+import { NoExposureNoRegionView } from './views/NoExposureNoRegionView';
+import { NetworkDisabledView } from './views/NetworkDisabledView';
+import { OverlayView } from './views/OverlayView';
+import { FrameworkUnavailableView } from './views/FrameworkUnavailableView';
+import { ExposureNotificationsUserStoppedView } from './views/ExposureNotificationsUserStoppedView';
+import { UnknownProblemView } from './views/UnknownProblemView';
 import {
   useNotificationPermissionStatus,
   NotificationPermissionStatusProvider,
 } from './components/NotificationPermissionStatus';
-import {LocationOffView} from './views/LocationOffView';
-import {OutbreakExposedView} from './views/OutbreakExposedView';
+import { LocationOffView } from './views/LocationOffView';
+import { OutbreakExposedView } from './views/OutbreakExposedView';
 
 interface ContentProps {
   isBottomSheetExpanded: boolean;
 }
 
-const UploadShareView = ({hasShared, isBottomSheetExpanded}: {hasShared?: boolean; isBottomSheetExpanded: boolean}) => {
+const UploadShareView = ({ hasShared, isBottomSheetExpanded }: { hasShared?: boolean; isBottomSheetExpanded: boolean }) => {
   return hasShared ? (
     <DiagnosedShareView isBottomSheetExpanded={isBottomSheetExpanded} />
   ) : (
-    <DiagnosedShareUploadView isBottomSheetExpanded={isBottomSheetExpanded} />
-  );
+      <DiagnosedShareUploadView isBottomSheetExpanded={isBottomSheetExpanded} />
+    );
 };
 
-const Content = ({isBottomSheetExpanded}: ContentProps) => {
-  const {region, userStopped} = useStorage();
+const Content = ({ isBottomSheetExpanded }: ContentProps) => {
+  const { region, userStopped } = useStorage();
 
   const regionalI18n = useRegionalI18n();
   const regionCase = getRegionCase(region, regionalI18n.activeRegions);
   const exposureStatus = useExposureStatus();
   const [systemStatus] = useSystemStatus();
-  const {outbreakStatus} = useOutbreakService();
+  const { outbreakStatus } = useOutbreakService();
   const [, turnNotificationsOn] = useNotificationPermissionStatus();
   useEffect(() => {
     return turnNotificationsOn();
   }, [turnNotificationsOn]);
 
   const network = useNetInfo();
+  console.log('outbreakStatus', outbreakStatus.type);
 
   const getNoExposureView = (_regionCase: RegionCase) => {
     switch (_regionCase) {
@@ -86,7 +87,7 @@ const Content = ({isBottomSheetExpanded}: ContentProps) => {
   };
 
   // this is for the test menu
-  const {forceScreen} = useStorage();
+  const { forceScreen } = useStorage();
   if (TEST_MODE) {
     switch (forceScreen) {
       case ForceScreen.NoExposureView:
@@ -137,8 +138,8 @@ const Content = ({isBottomSheetExpanded}: ContentProps) => {
       return exposureStatus.needsSubmission ? (
         <UploadShareView isBottomSheetExpanded={isBottomSheetExpanded} hasShared={exposureStatus.hasShared} />
       ) : (
-        <DiagnosedView isBottomSheetExpanded={isBottomSheetExpanded} />
-      );
+          <DiagnosedView isBottomSheetExpanded={isBottomSheetExpanded} />
+        );
     case ExposureStatusType.Monitoring:
     default:
       if (!network.isConnected) {
@@ -192,9 +193,9 @@ const ExpandedContent = (bottomSheetBehavior: BottomSheetBehavior) => {
 };
 
 export const HomeScreen = () => {
-  const {checkForOutbreaks} = useOutbreakService();
+  const { checkForOutbreaks } = useOutbreakService();
   const navigation = useNavigation();
-  const {userStopped} = useStorage();
+  const { userStopped } = useStorage();
 
   useEffect(() => {
     if (__DEV__ && TEST_MODE) {
@@ -266,7 +267,7 @@ export const HomeScreen = () => {
           accessibilityElementsHidden={isBottomSheetExpanded}
           importantForAccessibility={isBottomSheetExpanded ? 'no-hide-descendants' : undefined}
         >
-          <Animated.View style={{opacity: fadeAnim}}>
+          <Animated.View style={{ opacity: fadeAnim }}>
             <Content isBottomSheetExpanded={isBottomSheetExpanded} />
           </Animated.View>
         </Box>
--- src/screens/home/views/OutbreakExposedView.tsx
+++ src/screens/home/views/OutbreakExposedView.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import {Box, Text} from 'components';
-import {useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
-import {StyleSheet, Platform} from 'react-native';
+import { Box, Text, RoundedBox } from 'components';
+import { useAccessibilityAutoFocus } from 'shared/useAccessibilityAutoFocus';
+import { StyleSheet, Platform } from 'react-native';
+import { useI18n } from 'locale';
 
-import {BaseHomeView} from '../components/BaseHomeView';
+import { BaseHomeView } from '../components/BaseHomeView';
 
-const ExposureText = ({isBottomSheetExpanded}: {isBottomSheetExpanded: boolean}) => {
+const ExposureText = ({ isBottomSheetExpanded }: { isBottomSheetExpanded: boolean }) => {
   const autoFocusRef = useAccessibilityAutoFocus(!isBottomSheetExpanded);
+  const i18n = useI18n();
   return (
     <>
-      <Box alignSelf="stretch" style={styles.roundedBox1}>
-        <Box paddingHorizontal="m" paddingVertical="m">
-          <Text focusRef={autoFocusRef} variant="bodyTitle" marginBottom="m" accessibilityRole="header">
-            You have been exposed to an Outbreak
+      <RoundedBox isFirstBox>
+        <Text focusRef={autoFocusRef} variant="bodyTitle" marginBottom="m" accessibilityRole="header">
+          You have been exposed to an Outbreak
           </Text>
-          <Text marginBottom="m">[Placeholder] There was an outbreak at one of the locations you scanned.</Text>
-        </Box>
-      </Box>
+        <Text marginBottom="m">[Placeholder] There was an outbreak at one of the locations you scanned.</Text>
+      </RoundedBox>
+
+      <RoundedBox isFirstBox={false}>
+        <Text variant="bodyTitle" marginBottom="m" accessibilityRole="header">
+          {i18n.translate('Home.ExposureDetected.Title2')}
+        </Text>
+      </RoundedBox>
     </>
   );
 };
 
-export const OutbreakExposedView = ({isBottomSheetExpanded}: {isBottomSheetExpanded: boolean}) => {
+export const OutbreakExposedView = ({ isBottomSheetExpanded }: { isBottomSheetExpanded: boolean }) => {
   return (
     <BaseHomeView iconName="hand-caution" testID="outbreakExposure">
       <ExposureText isBottomSheetExpanded={isBottomSheetExpanded} />
