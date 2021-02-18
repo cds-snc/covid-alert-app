5600b4ce2eb478a34e3bf043fe314058a596516f
omartehsin1 <omartehsin@gmail.com>
2021-02-18 14:07:02 -0500

    created new menu buttons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -32,6 +32,8 @@ import ScanQRCodeWhiteArrow from 'assets/scan-qr-code-white-arrow.svg';
 import Camera from 'assets/camera.svg';
 import GreenCircleCheck from 'assets/green-circle-check.svg';
 import RedCircleExclamation from 'assets/red-circle-exclamation.svg';
+import QRCodeIcon from 'assets/qr-code-icon.svg';
+import HamburgerMenu from 'assets/hamburger-menu.svg';
 
 const ICONS = {
   'icon-x': IconX,
@@ -67,6 +69,8 @@ const ICONS = {
   'camera-permission': Camera,
   'green-circle-check': GreenCircleCheck,
   'red-circle-exclamation': RedCircleExclamation,
+  'qr-code-icon': QRCodeIcon,
+  'hamburger-menu': HamburgerMenu,
 };
 
 export type IconName = keyof typeof ICONS;
@@ -78,7 +82,7 @@ export interface IconProps {
   height?: number;
 }
 
-export const Icon = ({name, size = 24, width, height}: IconProps) => {
+export const Icon = ({ name, size = 24, width, height }: IconProps) => {
   const IconImpl = name !== undefined ? ICONS[name] : null; // eslint-disable-line no-negated-condition
   return IconImpl ? <IconImpl width={width ? width : size} height={height ? height : size} /> : null;
 };
---
+++ src/screens/home/views/CollapsedMenuView.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Box, Icon, Button } from 'components';
+import { useI18n } from 'locale';
+
+
+export const CollapsedMenuView = () => {
+  const i18n = useI18n();
+  return (
+    <Box style={styles.content}>
+      <Box marginBottom="s" marginLeft="s" paddingLeft="s" style={styles.scanBox} backgroundColor="buttonGrey">
+        <Box marginTop="m" paddingLeft="s">
+          <Icon size={25} name="qr-code-icon" />
+        </Box>
+        <Button text={i18n.translate('QRCode.CTA')} variant="bigFlatNeutralGrey" onPress={() => { }} />
+      </Box>
+      <Box marginBottom="s" marginRight="s" paddingLeft="s" style={styles.menuBox} backgroundColor="buttonGrey">
+        <Box paddingTop="m" paddingLeft="s">
+          <Icon size={25} name="hamburger-menu" />
+        </Box>
+
+        <Button text="Menu" variant="bigFlatNeutralGrey" onPress={() => { }} />
+      </Box>
+    </Box>
+
+  )
+}
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingTop: 10,
+    justifyContent: 'space-between'
+  },
+  scanBox: {
+    flexDirection: 'row',
+    width: '60%',
+    borderRadius: 5,
+  },
+  menuBox: {
+    flexDirection: 'row',
+    width: '30%',
+    borderRadius: 5,
+  },
+});
