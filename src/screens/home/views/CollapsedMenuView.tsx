372a482921e8edea6ade810bd935a5ef9ce28bc3
omartehsin1 <omartehsin@gmail.com>
2021-02-18 15:57:42 -0500

    adjusted buttons

--- src/screens/home/views/CollapsedMenuView.tsx
+++ src/screens/home/views/CollapsedMenuView.tsx
@@ -3,18 +3,17 @@ import { StyleSheet } from 'react-native';
 import { Box, Icon, Button } from 'components';
 import { useI18n } from 'locale';
 
-
 export const CollapsedMenuView = () => {
   const i18n = useI18n();
   return (
     <Box style={styles.content}>
-      <Box marginBottom="s" marginLeft="s" paddingLeft="s" style={styles.scanBox} backgroundColor="buttonGrey">
+      <Box marginBottom="s" marginLeft="m" paddingLeft="s" style={styles.scanBox} backgroundColor="buttonGrey">
         <Box marginTop="m" paddingLeft="s">
           <Icon size={25} name="qr-code-icon" />
         </Box>
         <Button text={i18n.translate('QRCode.CTA')} variant="bigFlatNeutralGrey" onPress={() => { }} />
       </Box>
-      <Box marginBottom="s" marginRight="s" paddingLeft="s" style={styles.menuBox} backgroundColor="buttonGrey">
+      <Box marginBottom="s" marginRight="m" paddingLeft="s" style={styles.menuBox} backgroundColor="buttonGrey">
         <Box paddingTop="m" paddingLeft="s">
           <Icon size={25} name="hamburger-menu" />
         </Box>
@@ -22,19 +21,18 @@ export const CollapsedMenuView = () => {
         <Button text="Menu" variant="bigFlatNeutralGrey" onPress={() => { }} />
       </Box>
     </Box>
-
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   content: {
     flex: 1,
     flexDirection: 'row',
     paddingTop: 10,
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   scanBox: {
     flexDirection: 'row',
-    width: '60%',
+    width: '58%',
     borderRadius: 5,
   },
   menuBox: {
