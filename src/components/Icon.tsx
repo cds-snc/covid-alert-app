30ea7ecf99ca9d2eb57f3653cd0339411b8359e1
omartehsin1 <omartehsin@gmail.com>
2021-02-25 12:59:46 -0500

    created box, added editable buttons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -32,6 +32,7 @@ import ScanQRCodeWhiteArrow from 'assets/scan-qr-code-white-arrow.svg';
 import Camera from 'assets/camera.svg';
 import GreenCircleCheck from 'assets/green-circle-check.svg';
 import RedCircleExclamation from 'assets/red-circle-exclamation.svg';
+import PlacesDelete from 'assets/places-delete-icon.svg';
 
 const ICONS = {
   'icon-x': IconX,
@@ -67,6 +68,7 @@ const ICONS = {
   'camera-permission': Camera,
   'green-circle-check': GreenCircleCheck,
   'red-circle-exclamation': RedCircleExclamation,
+  'places-delete': PlacesDelete,
 };
 
 export type IconName = keyof typeof ICONS;
--- src/screens/qr/CheckInHistoryScreen.tsx
+++ src/screens/qr/CheckInHistoryScreen.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import {useI18n, useRegionalI18n} from 'locale';
-import {Box, Text} from 'components';
+import React, {useState} from 'react';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import {useI18n} from 'locale';
+import {Box, Text, Icon, Button} from 'components';
 import {BaseDataSharingView} from 'screens/datasharing/components/BaseDataSharingView';
 import {CheckInData} from 'shared/qr';
 import {formatCheckInDate} from 'shared/date-fns';
 import {useOutbreakService} from 'shared/OutbreakProvider';
 
-const CheckInList = ({checkIns}: {checkIns: CheckInData[]}) => {
+const CheckInList = ({checkIns, isEditing}: {checkIns: CheckInData[]; isEditing: boolean}) => {
   if (checkIns.length === 0) {
     return <Text>No Check-ins yet</Text>;
   }
@@ -14,12 +15,22 @@ const CheckInList = ({checkIns}: {checkIns: CheckInData[]}) => {
     <>
       {checkIns.map((checkIn, index) => {
         return (
-          <Box marginBottom="l" key={checkIn.id.concat(index.toString())}>
-            <Text>ID: {checkIn.id}</Text>
-            <Text>Name: {checkIn.name}</Text>
-            <Text>Address: {checkIn.address}</Text>
-            <Text>Time: {formatCheckInDate(new Date(checkIn.timestamp))}</Text>
-          </Box>
+          <>
+            <Box style={styles.boxStyle}>
+              <TouchableOpacity>{isEditing && <Icon size={20} name="places-delete" />}</TouchableOpacity>
+
+              <Box
+                padding="m"
+                backgroundColor="gray5"
+                style={styles.boxBorder}
+                key={checkIn.id.concat(index.toString())}
+              >
+                <Text variant="bodyTitle2">{checkIn.name}</Text>
+                <Text paddingTop="s">{checkIn.address}</Text>
+                <Text paddingTop="s">{formatCheckInDate(new Date(checkIn.timestamp))}</Text>
+              </Box>
+            </Box>
+          </>
         );
       })}
     </>
@@ -28,6 +39,12 @@ const CheckInList = ({checkIns}: {checkIns: CheckInData[]}) => {
 
 export const CheckInHistoryScreen = () => {
   const {checkInHistory} = useOutbreakService();
+  const [isEditing, setIsEditing] = useState(false);
+  const [isEditingText, setIsEditingText] = useState('Edit');
+  const onPressEdit = () => {
+    setIsEditing(!isEditing);
+    setIsEditingText(isEditing ? 'Edit' : 'Done');
+  };
   const i18n = useI18n();
   return (
     <BaseDataSharingView showBackButton={false}>
@@ -35,10 +52,47 @@ export const CheckInHistoryScreen = () => {
         <Text variant="bodyTitle" marginBottom="l" accessibilityRole="header">
           {i18n.translate('ScannedPlaces.Title')}
         </Text>
-        <Box>
-          <CheckInList checkIns={checkInHistory} />
+        <Text>{i18n.translate('ScannedPlaces.Body1')}</Text>
+        <Text marginTop="s" marginBottom="xl">
+          {i18n.translate('ScannedPlaces.Body2')}
+        </Text>
+      </Box>
+      <Box>
+        <Box style={isEditing ? styles.textBoxEditing : styles.textBoxNoEdit}>
+          {isEditing && <Button text="Delete All" variant="buttonSelect" onPress={onPressEdit} />}
+
+          <Button text={isEditingText} variant="buttonSelect" onPress={onPressEdit} />
+        </Box>
+
+        <Box paddingHorizontal="s" marginLeft="m" marginRight="m" style={styles.placesBox} backgroundColor="gray5">
+          <CheckInList checkIns={checkInHistory} isEditing={isEditing} />
         </Box>
       </Box>
     </BaseDataSharingView>
   );
 };
+
+const styles = StyleSheet.create({
+  boxBorder: {
+    borderBottomColor: '#8a8a8a',
+    borderBottomWidth: 1,
+  },
+  boxStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textBoxEditing: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textBoxNoEdit: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  placesBox: {
+    borderRadius: 10,
+  },
+});
