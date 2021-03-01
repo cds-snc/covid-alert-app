97573357d26d4f3c96b5a43280456d56dc8ae382
omartehsin1 <omartehsin@gmail.com>
2021-03-01 11:27:33 -0500

    ui changes

--- src/screens/qr/CheckInHistoryScreen.tsx
+++ src/screens/qr/CheckInHistoryScreen.tsx
@@ -3,6 +3,7 @@ import {StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import {useI18n} from 'locale';
 import {Box, Text, Icon, Button} from 'components';
 import {BaseDataSharingView} from 'screens/datasharing/components/BaseDataSharingView';
+
 import {CheckInData} from 'shared/qr';
 import {formatCheckInDate} from 'shared/date-fns';
 import {useOutbreakService} from 'shared/OutbreakProvider';
@@ -18,14 +19,13 @@ const CheckInList = ({checkIns, isEditing}: {checkIns: CheckInData[]; isEditing:
       {
         text: i18n.translate('ScannedPlaces.Alert.Cancel'),
         onPress: () => {},
-        style: 'cancel',
       },
       {
         text: i18n.translate('ScannedPlaces.Alert.Confirm'),
         onPress: () => {
           deleteScannedPlaces(id);
         },
-        style: 'default',
+        style: 'cancel',
       },
     ]);
   };
@@ -35,7 +35,7 @@ const CheckInList = ({checkIns, isEditing}: {checkIns: CheckInData[]; isEditing:
       {sortedCheckIn.map((checkIn, index) => {
         return (
           <>
-            <Box style={styles.boxStyle}>
+            <Box style={styles.boxStyle} key={checkIn.id.concat(index.toString())}>
               <TouchableOpacity
                 onPress={() => {
                   deleteConfirmationAlert(checkIn.id);
@@ -44,12 +44,7 @@ const CheckInList = ({checkIns, isEditing}: {checkIns: CheckInData[]; isEditing:
                 {isEditing && <Icon size={20} name="places-delete" />}
               </TouchableOpacity>
 
-              <Box
-                padding="m"
-                backgroundColor="gray5"
-                style={styles.boxBorder}
-                key={checkIn.id.concat(index.toString())}
-              >
+              <Box padding="m" key={checkIn.id.concat(index.toString())}>
                 <Text variant="bodyTitle2">{checkIn.name}</Text>
                 <Text paddingTop="s">{checkIn.address}</Text>
                 <Text paddingTop="s">{formatCheckInDate(new Date(checkIn.timestamp))}</Text>
@@ -81,20 +76,19 @@ export const CheckInHistoryScreen = () => {
       {
         text: i18n.translate('ScannedPlaces.Alert.Cancel'),
         onPress: () => {},
-        style: 'cancel',
       },
       {
         text: i18n.translate('ScannedPlaces.Alert.Confirm'),
         onPress: () => {
           deleteAllScannedPlaces();
         },
-        style: 'default',
+        style: 'cancel',
       },
     ]);
   };
 
   return (
-    <BaseDataSharingView showBackButton={false}>
+    <BaseDataSharingView showBackButton={true}>
       <Box paddingHorizontal="m">
         <Text variant="bodyTitle" marginBottom="l" accessibilityRole="header">
           {i18n.translate('ScannedPlaces.Title')}
@@ -108,15 +102,15 @@ export const CheckInHistoryScreen = () => {
       <Box>
         {checkInHistory.length === 0 ? null : (
           <Box style={styles.textBox}>
-            <Box>{isEditing && <Button text="Delete All" variant="buttonSelect" onPress={deleteAllPlaces} />}</Box>
+            <Box>{isEditing && <Button text="Delete All" variant="text" onPress={deleteAllPlaces} />}</Box>
 
             <Box>
-              <Button text={isEditingText} variant="buttonSelect" onPress={onPressEdit} />
+              <Button text={isEditingText} variant="text" onPress={onPressEdit} />
             </Box>
           </Box>
         )}
 
-        <Box paddingHorizontal="s" marginLeft="m" marginRight="m" style={styles.placesBox} backgroundColor="gray5">
+        <Box paddingHorizontal="xxs" marginLeft="m" marginRight="m" style={styles.placesBox} backgroundColor="gray5">
           <CheckInList checkIns={checkInHistory} isEditing={isEditing} />
         </Box>
       </Box>
@@ -125,14 +119,12 @@ export const CheckInHistoryScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  boxBorder: {
-    borderBottomColor: '#8a8a8a',
-    borderBottomWidth: 1,
-  },
   boxStyle: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
+    borderBottomColor: '#8a8a8a',
+    borderBottomWidth: 1,
   },
   textBox: {
     flex: 1,
