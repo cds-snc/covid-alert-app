c0695f7be9b7fc00a8220226176f2057d00380be
Clément Janin <clement.janin@cds-snc.ca>
2021-03-16 09:22:19 -0400

    Introduced MetricsService (Kotlin side)

---
+++ android/app/src/main/java/app/covidshield/services/metrics/DebugMetricsLifecycleInformationProvider.kt
@@ -0,0 +1,75 @@
+package app.covidshield.services.metrics
+
+import android.content.Context
+import android.text.format.DateUtils
+import java.util.*
+
+object DebugMetricsLifecycleInformationProvider {
+
+    private const val SharedPreferencesFileKey = "covid-shield-debug-metrics"
+    private const val LifecycleDailyCountKey = "lifecycle-daily-count"
+    private const val LifecycleDailyCountTimestampKey = "lifecycle-daily-count-timestamp"
+
+    private var cachedLifecycleIdentifier: String? = null
+    private var cachedLifecycleDailyCount: Number? = null
+
+    fun getLifecycleIdentifier(): String {
+
+        fun generateAndSaveNewLifecycleIdentifier(): String {
+            val uuidAsString = UUID.randomUUID().toString()
+            cachedLifecycleIdentifier = uuidAsString
+            return uuidAsString
+        }
+
+        return if (cachedLifecycleIdentifier == null) {
+            generateAndSaveNewLifecycleIdentifier()
+        } else {
+            cachedLifecycleIdentifier!!
+        }
+    }
+
+    fun getLifecycleDailyCount(context: Context): Number {
+
+        val sharedPreferences = context.getSharedPreferences(SharedPreferencesFileKey, Context.MODE_PRIVATE)
+
+        // Set timestamp if it does not exist (only the first time this code ever gets executed)
+        if (!sharedPreferences.contains(LifecycleDailyCountTimestampKey)) {
+            with (sharedPreferences.edit()) {
+                putLong(LifecycleDailyCountTimestampKey, Date().time)
+                apply()
+            }
+        }
+
+        fun shouldResetDailyCount(): Boolean {
+            val retrievedDailyCountTimestamp = sharedPreferences.getLong(LifecycleDailyCountTimestampKey, Date().time)
+
+            val shouldReset = DateUtils.isToday(retrievedDailyCountTimestamp) == false
+
+            if (shouldReset) {
+                with (sharedPreferences.edit()) {
+                    putLong(LifecycleDailyCountTimestampKey, Date().time)
+                    apply()
+                }
+            }
+
+            return shouldReset
+        }
+
+        return if (cachedLifecycleDailyCount == null) {
+            val retrievedDailyCount = if (shouldResetDailyCount()) 0 else sharedPreferences.getInt(LifecycleDailyCountKey, 0)
+
+            val incrementedDailyCount = retrievedDailyCount + 1
+
+            cachedLifecycleDailyCount = incrementedDailyCount
+            with (sharedPreferences.edit()) {
+                putInt(LifecycleDailyCountKey, incrementedDailyCount)
+                apply()
+            }
+
+            incrementedDailyCount
+        } else {
+            cachedLifecycleDailyCount!!
+        }
+    }
+
+}
---
+++ android/app/src/main/java/app/covidshield/services/metrics/MetricsService.kt
@@ -0,0 +1,100 @@
+package app.covidshield.services.metrics
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import app.covidshield.BuildConfig
+import app.covidshield.extensions.log
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+object MetricsService {
+
+    private const val metricUrl = BuildConfig.METRICS_URL
+    private const val metricApiKey = BuildConfig.METRICS_API_KEY
+
+    private val okHttpClient by lazy { OkHttpClient() }
+
+    fun publishPackageUpdatedMetric(context: Context) {
+        val jsonObject = JSONObject();
+
+        jsonObject.put("identifier", "android-package-replaced")
+        jsonObject.put("region", "None")
+        jsonObject.put("timestamp", TimeUnit.MILLISECONDS.toMillis(System.currentTimeMillis()))
+
+        val serializedGlobalMetricsPayload = serializeGlobalMetricsPayload(jsonObject, context)
+        this.push(serializedGlobalMetricsPayload, context)
+    }
+
+    fun publishDebugMetric(stepNumber: Double, context: Context) {
+
+        fun serializeMetricPayload(stepNumber: Double, lifecycleId: String, lifeCycleDailyCount: Number): JSONObject {
+            val jsonObject = JSONObject();
+
+            jsonObject.put("identifier", "ExposureNotificationCheck")
+            jsonObject.put("region", "None")
+            jsonObject.put("timestamp", TimeUnit.MILLISECONDS.toMillis(System.currentTimeMillis()))
+            jsonObject.put("step", stepNumber.toString())
+            jsonObject.put("lifecycleId", lifecycleId)
+            jsonObject.put("lifeCycleDailyCount", lifeCycleDailyCount.toString())
+
+            return jsonObject
+        }
+
+        val lifecycleIdentifier = DebugMetricsLifecycleInformationProvider.getLifecycleIdentifier()
+        val lifecycleDailyCount = DebugMetricsLifecycleInformationProvider.getLifecycleDailyCount(context)
+        val serializedMetricPayload = serializeMetricPayload(stepNumber, lifecycleIdentifier, lifecycleDailyCount)
+        val serializedGlobalMetricsPayload = serializeGlobalMetricsPayload(serializedMetricPayload, context)
+
+        this.push(serializedGlobalMetricsPayload, context)
+    }
+
+    private fun serializeGlobalMetricsPayload(metricPayload: JSONObject, context: Context): JSONObject {
+        val jsonObject = JSONObject();
+
+        jsonObject.put("metricstimestamp", TimeUnit.MILLISECONDS.toMillis(System.currentTimeMillis()))
+        val info: PackageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+        jsonObject.put("appversion", info.versionName)
+        jsonObject.put("appos", "android")
+        jsonObject.put("osversion", android.os.Build.VERSION.SDK_INT.toString())
+        jsonObject.put("manufacturer", android.os.Build.MANUFACTURER)
+        jsonObject.put("model", android.os.Build.MODEL)
+        jsonObject.put("androidreleaseversion", android.os.Build.VERSION.RELEASE)
+
+        val jsonArray = JSONArray()
+        jsonArray.put(metricPayload)
+
+        jsonObject.put("payload", jsonArray)
+
+        return jsonObject
+    }
+
+    private fun push(globalMetricsPayload: JSONObject, context: Context) {
+        val body: RequestBody = RequestBody.create(MediaType.parse("text/plain"), globalMetricsPayload.toString())
+
+        val request = Request.Builder()
+                .url(metricUrl)
+                .header("Accept", "application/json")
+                .addHeader("Content-Type", "text/plain")
+                .addHeader("x-api-key", metricApiKey)
+                .cacheControl(CacheControl.Builder().noStore().build())
+                .post(body)
+                .build()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    log(globalMetricsPayload.toString(), mapOf("response" to response.isSuccessful), "AndroidMetric")
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                }
+            }
+        })
+    }
+
+}
