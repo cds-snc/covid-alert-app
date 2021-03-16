875d439f8133ad9bcd9e6b2eb0890fdecd65dd27
Clément Janin <clement.janin@cds-snc.ca>
2021-03-16 09:22:19 -0400

    Introduced MetricsService (Kotlin side)

--- android/app/src/main/java/app/covidshield/receiver/PackageReceiver.kt
+++ android/app/src/main/java/app/covidshield/receiver/PackageReceiver.kt
@@ -3,14 +3,9 @@ package app.covidshield.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageInfo
-import app.covidshield.BuildConfig
 import app.covidshield.extensions.log
+import app.covidshield.services.metrics.MetricsService
 import okhttp3.*
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.IOException
-import java.util.concurrent.TimeUnit.MILLISECONDS
 
 class PackageReceiver () : BroadcastReceiver() {
 
@@ -21,47 +16,7 @@ class PackageReceiver () : BroadcastReceiver() {
         log("onReceive", mapOf("action" to action))
 
             if (action == "android.intent.action.MY_PACKAGE_REPLACED") {
-                val info: PackageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-                val versionName = info.versionName
-                val timestamp = MILLISECONDS.toMillis(System.currentTimeMillis())
-                val metricUrl = BuildConfig.METRICS_URL
-                val metricApiKey = BuildConfig.METRICS_API_KEY
-
-                val metricJson = JSONObject();
-                metricJson.put("metricstimestamp", timestamp)
-                metricJson.put("appversion", versionName)
-                metricJson.put("appos", "android")
-                metricJson.put("osversion", android.os.Build.VERSION.SDK_INT)
-                val metricPayload = JSONObject();
-                metricPayload.put("identifier", "android-package-replaced")
-                metricPayload.put("region", "None")
-                metricPayload.put("timestamp", timestamp)
-                val metricPayloads = JSONArray()
-                metricPayloads.put(metricPayload)
-                metricJson.put("payload", metricPayloads)
-                log("MY_PACKAGE_REPLACED", mapOf("metricJson" to metricJson.toString()))
-                val body: RequestBody = RequestBody.create(MediaType.parse("text/plain"), metricJson.toString())
-
-                val request = Request.Builder()
-                        .url(metricUrl)
-                        .header("Accept", "application/json")
-                        .addHeader("Content-Type", "text/plain")
-                        .addHeader("x-api-key", metricApiKey)
-                        .cacheControl(CacheControl.Builder().noStore().build())
-                        .post(body)
-                        .build()
-                okHttpClient.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                    }
-
-                    override fun onResponse(call: Call, response: Response) {
-                        response.use {
-                            log("MY_PACKAGE_REPLACED", mapOf("response" to response.isSuccessful))
-                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-                        }
-                    }
-                })
+                MetricsService.publishPackageUpdatedMetric(context);
             }
     }
 
---
+++ android/app/src/main/java/app/covidshield/services/metrics/DebugMetricsHelper.kt
@@ -0,0 +1,123 @@
+package app.covidshield.services.metrics
+
+import android.content.Context
+import android.text.format.DateUtils
+import app.covidshield.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
+import java.util.*
+
+object DebugMetricsHelper {
+
+    private const val SharedPreferencesFileKey = "covid-shield-debug-metrics"
+    private const val LifecycleDailyCountKey = "lifecycle-daily-count"
+    private const val DebugMetricsSwitch = "debug-metrics-switch"
+    private const val ResetTimestampKey = "reset-timestamp"
+    private const val configurationUrl = BuildConfig.EN_CONFIG_URL;
+
+    private val okHttpClient by lazy { OkHttpClient() }
+
+    private var cachedLifecycleIdentifier: String? = null
+    private var cachedLifecycleDailyCount: Number? = null
+    private var cachedActivationFlag: Boolean? = null
+
+    fun getLifecycleIdentifier(): String {
+
+        fun generateAndSaveNewLifecycleIdentifier(): String {
+            val uuidAsString = UUID.randomUUID().toString()
+            cachedLifecycleIdentifier = uuidAsString
+            return uuidAsString
+        }
+
+        return if (cachedLifecycleIdentifier == null) {
+            generateAndSaveNewLifecycleIdentifier()
+        } else {
+            cachedLifecycleIdentifier!!
+        }
+    }
+
+    fun getLifecycleDailyCount(context: Context): Number {
+
+        val sharedPreferences = context.getSharedPreferences(SharedPreferencesFileKey, Context.MODE_PRIVATE)
+
+        return if (cachedLifecycleDailyCount == null) {
+            val retrievedDailyCount = if (didDayChange(context)) 0 else sharedPreferences.getInt(LifecycleDailyCountKey, 0)
+
+            val incrementedDailyCount = retrievedDailyCount + 1
+
+            cachedLifecycleDailyCount = incrementedDailyCount
+            with (sharedPreferences.edit()) {
+                putInt(LifecycleDailyCountKey, incrementedDailyCount)
+                apply()
+            }
+
+            incrementedDailyCount
+        } else {
+            cachedLifecycleDailyCount!!
+        }
+    }
+
+    fun canPublishDebugMetrics(context: Context): Boolean {
+
+        val sharedPreferences = context.getSharedPreferences(SharedPreferencesFileKey, Context.MODE_PRIVATE)
+
+        return if (cachedActivationFlag == null) {
+            val isActive = if (didDayChange(context)) {
+                val request = Request.Builder().url(configurationUrl).build()
+
+                val stepsEnabled = okHttpClient.newCall(request).execute().use { response ->
+                    if (response.isSuccessful) {
+                        val jsonObject = JSONObject(response.body()!!.string())
+                        if (jsonObject.has("steps")) {
+                            jsonObject.getInt("steps") == 1
+                        } else {
+                            false
+                        }
+                    } else {
+                        false
+                    }
+                }
+
+                with (sharedPreferences.edit()) {
+                    putBoolean(DebugMetricsSwitch, stepsEnabled)
+                    apply()
+                }
+
+                stepsEnabled
+            } else {
+                sharedPreferences.getBoolean(DebugMetricsSwitch, false)
+            }
+
+            cachedActivationFlag = isActive
+            isActive
+        } else {
+            cachedActivationFlag!!
+        }
+    }
+
+    private fun didDayChange(context: Context): Boolean {
+        val sharedPreferences = context.getSharedPreferences(SharedPreferencesFileKey, Context.MODE_PRIVATE)
+
+        return if (!sharedPreferences.contains(ResetTimestampKey)) {
+            with (sharedPreferences.edit()) {
+                putLong(ResetTimestampKey, Date().time)
+                apply()
+            }
+            true
+        } else {
+            val resetTimestamp = sharedPreferences.getLong(ResetTimestampKey, Date().time)
+
+            val shouldReset = DateUtils.isToday(resetTimestamp) == false
+
+            if (shouldReset) {
+                with (sharedPreferences.edit()) {
+                    putLong(ResetTimestampKey, Date().time)
+                    apply()
+                }
+            }
+
+            shouldReset
+        }
+    }
+}
