773046b5e6bb89545a0dbdfca639be5d962ea8a9
Tim Arney <tim@line37.com>
2021-04-15 13:42:12 -0400

    initial qr code onboard setup

---
+++ src/screens/qr/onboarding/Tutorial.tsx
@@ -0,0 +1,117 @@
+import React, {useState, useCallback, useRef} from 'react';
+import {ListRenderItem, StyleSheet, useWindowDimensions, View} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import {useNavigation} from '@react-navigation/native';
+import {Box, Button, Toolbar, ProgressCircles} from 'components';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useI18n} from 'locale';
+import {useAccessibilityService} from 'services/AccessibilityService';
+
+import {TutorialContent, tutorialData, TutorialKey} from './TutorialContent';
+
+export const QRCodeOnboardScreen = () => {
+  const navigation = useNavigation();
+  const {width: viewportWidth} = useWindowDimensions();
+  const carouselRef = useRef<Carousel<TutorialKey>>(null);
+  const [currentStep, setCurrentStep] = useState(0);
+  const i18n = useI18n();
+  const close = useCallback(() => navigation.goBack(), [navigation]);
+
+  const isStart = currentStep === 0;
+  const isEnd = currentStep === tutorialData.length - 1;
+
+  const {isScreenReaderEnabled} = useAccessibilityService();
+  const currentStepForRenderItem = isScreenReaderEnabled ? currentStep : -1;
+
+  const renderItem: ListRenderItem<TutorialKey> = useCallback(
+    ({item, index}) => {
+      return (
+        <View style={styles.flex} accessibilityElementsHidden={index !== currentStepForRenderItem}>
+          <TutorialContent key={item} item={item} isActive={index === currentStepForRenderItem} />
+        </View>
+      );
+    },
+    [currentStepForRenderItem],
+  );
+
+  const nextItem = useCallback(() => {
+    if (isEnd) {
+      close();
+      return;
+    }
+    carouselRef.current?.snapToNext();
+  }, [close, isEnd]);
+
+  const prevItem = useCallback(() => {
+    carouselRef.current?.snapToPrev();
+  }, []);
+
+  const onSnapToItem = useCallback((newIndex: number) => {
+    setCurrentStep(newIndex);
+  }, []);
+
+  return (
+    <Box backgroundColor="overlayBackground" flex={1}>
+      <SafeAreaView style={styles.flex}>
+        <Toolbar
+          title=""
+          navIcon="icon-back-arrow"
+          navText={i18n.translate('Tutorial.Close')}
+          navLabel={i18n.translate('Tutorial.Close')}
+          onIconClicked={close}
+        />
+        <View style={styles.flex}>
+          <Carousel
+            ref={carouselRef}
+            data={tutorialData}
+            renderItem={renderItem}
+            sliderWidth={viewportWidth}
+            itemWidth={viewportWidth}
+            onSnapToItem={onSnapToItem}
+            importantForAccessibility="no"
+            accessible={false}
+            removeClippedSubviews={false}
+          />
+        </View>
+        <Box flexDirection="row" borderTopWidth={2} borderTopColor="gray5">
+          <Box flex={0} style={{...styles.offset1}}>
+            {!isStart && <Button text={i18n.translate(`Tutorial.ActionBack`)} variant="text" onPress={prevItem} />}
+          </Box>
+
+          <Box flex={2} justifyContent="center" style={{...styles.offset2}}>
+            <ProgressCircles numberOfSteps={tutorialData.length} activeStep={currentStep + 1} marginBottom="none" />
+          </Box>
+
+          <Box flex={0} style={{...styles.offset3}}>
+            <Button
+              text={i18n.translate(`Tutorial.Action${isEnd ? 'End' : 'Next'}`)}
+              variant="text"
+              onPress={nextItem}
+            />
+          </Box>
+        </Box>
+      </SafeAreaView>
+    </Box>
+  );
+};
+
+const styles = StyleSheet.create({
+  offset1: {
+    width: 147,
+    right: 10,
+  },
+  offset2: {
+    left: 1,
+  },
+  offset3: {
+    width: 147,
+  },
+  flex: {
+    flex: 1,
+  },
+  dotContainerStyle: {},
+  dotWrapperStyle: {
+    marginTop: -14,
+    fontSize: 20,
+  },
+});
