a5ac13861a02f4ed438a1a71079fffdae2da0ac7
James Eberhardt <james.eberhardt@cds-snc.ca>
2021-04-28 15:03:11 -0400

    Add timestamp to remove specific check-in.

--- src/screens/qr/CheckInHistoryScreen.tsx
+++ src/screens/qr/CheckInHistoryScreen.tsx
@@ -17,7 +17,7 @@ const CheckInList = ({scannedCheckInData}: {scannedCheckInData: CheckInData[]}) 
   const checkIns = sortedCheckInArray(scannedCheckInData);
   const dateLocale = i18n.locale === 'fr' ? 'fr-CA' : 'en-CA';
 
-  const deleteConfirmationAlert = (locationId: string) => {
+  const deleteConfirmationAlert = (locationId: string, timestamp: number) => {
     Alert.alert(i18n.translate('PlacesLog.Alert.Title'), i18n.translate('PlacesLog.Alert.Subtitle'), [
       {
         text: i18n.translate('PlacesLog.Alert.Cancel'),
@@ -26,7 +26,7 @@ const CheckInList = ({scannedCheckInData}: {scannedCheckInData: CheckInData[]}) 
       {
         text: i18n.translate('PlacesLog.Alert.ConfirmDelete'),
         onPress: () => {
-          deleteScannedPlace(locationId);
+          deleteScannedPlace(locationId, timestamp);
         },
         style: 'cancel',
       },
@@ -68,7 +68,7 @@ const CheckInList = ({scannedCheckInData}: {scannedCheckInData: CheckInData[]}) 
                           accessibilityLabel={`${i18n.translate('PlacesLog.DeleteIcon')} ${data.checkIns.name}`}
                           style={styles.deleteIcon}
                           onPress={() => {
-                            deleteConfirmationAlert(data.checkIns.id);
+                            deleteConfirmationAlert(data.checkIns.id, data.checkIns.timestamp);
                           }}
                         >
                           <Icon size={40} name="delete-icon" />
