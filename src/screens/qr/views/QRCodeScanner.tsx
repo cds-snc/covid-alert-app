9723c3e8b90c5888e0acb6160469805686e5f5c0
Stephen McMurtry <smcmurtry@gmail.com>
2021-04-29 19:16:19 -0600

    Fixed the QR scanning screen mask for landscape (#1543)

--- src/screens/qr/views/QRCodeScanner.tsx
+++ src/screens/qr/views/QRCodeScanner.tsx
@@ -7,12 +7,14 @@ import {useI18n} from 'locale';
 import {useNavigation} from '@react-navigation/native';
 import {log} from 'shared/logging/config';
 import {useOutbreakService} from 'services/OutbreakService';
+import {useOrientation} from 'shared/useOrientation';
 
 import {handleOpenURL} from '../utils';
 
 export const QRCodeScanner = () => {
   const navigation = useNavigation();
   const {addCheckIn} = useOutbreakService();
+  const {orientation} = useOrientation();
   const [scanned, setScanned] = useState<boolean>(false);
 
   const i18n = useI18n();
@@ -41,7 +43,12 @@ export const QRCodeScanner = () => {
           <Toolbar2 navText={i18n.translate('DataUpload.Close')} useWhiteText onIconClicked={close} showBackButton />
         </Box>
 
-        <Box style={styles.info} paddingTop="s" paddingHorizontal="m">
+        <Box
+          style={styles.info}
+          paddingVertical="s"
+          paddingHorizontal="m"
+          height={orientation === 'landscape' ? 40 : '25%'}
+        >
           <Text variant="bodyText" accessibilityRole="header" accessibilityAutoFocus color="bodyTitleWhite">
             {i18n.translate(`QRCode.Reader.Title`)}
           </Text>
@@ -64,7 +71,6 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
     width: '100%',
-    paddingBottom: '40%',
   },
   toolbar: {
     backgroundColor: 'black',
