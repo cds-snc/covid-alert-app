235592673637a317deaf6811c84510637dca8c5d
Tim Arney <tim@line37.com>
2021-05-03 09:41:14 -0400

    add nav to outbreak screen

--- src/screens/exposures/RecentExposureView.tsx
+++ src/screens/exposures/RecentExposureView.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {useI18n} from 'locale';
-import {Text} from 'components';
+import {Box, Toolbar} from 'components';
+import {useNavigation} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ScrollView} from 'react-native-gesture-handler';
+import {StyleSheet} from 'react-native';
 
-import {BaseHomeView} from '../home/components/BaseHomeView';
+import {OutbreakExposedView} from '../home/views/OutbreakExposedView';
 
 export const RecentExposureScreen = () => {
   const i18n = useI18n();
 
+  const navigation = useNavigation();
+  const back = useCallback(() => navigation.goBack(), [navigation]);
   return (
-    <BaseHomeView iconName="icon-bluetooth-disabled">
-      <Text variant="bodyText" color="bodyText" marginBottom="m">
-        {i18n.translate('Home.EnableBluetoothCTA')}
-      </Text>
-    </BaseHomeView>
+    <Box flex={1} backgroundColor="overlayBackground">
+      <SafeAreaView style={styles.flex}>
+        <Toolbar title="" navIcon="icon-back-arrow" navText={i18n.translate('PlacesLog.Back')} onIconClicked={back} />
+        <ScrollView style={styles.flex}>
+          <OutbreakExposedView />
+        </ScrollView>
+      </SafeAreaView>
+    </Box>
   );
 };
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+});
