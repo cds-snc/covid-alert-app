ea41a5999e6bb61d521666725e15fb5c3ed96243
Tim Arney <tim@line37.com>
2021-05-20 10:21:28 -0400

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,8 @@ import regionContentDefault from './locale/translations/region.json';
 import {RegionContent, RegionContentResponse} from './shared/Region';
 import {OutbreakProvider} from './services/OutbreakService';
 
+import {Aes, Sha, Hmac, Pbkdf2, Rsa} from '@trackforce/react-native-crypto';
+
 // this allows us to use new Date().toLocaleString() for date formatting on android
 // https://github.com/facebook/react-native/issues/19410#issuecomment-482804142
 if (Platform.OS === 'android') {
@@ -40,6 +42,36 @@ interface IFetchData {
 
 const appInit = async () => {
   SplashScreen.hide();
+};
+
+const testCrypto = async () => {
+  const iterations = 4096;
+  const keyInBytes = 32;
+  const message = 'data to encrypt';
+  const key = await Pbkdf2.hash('a0', 'a1b4efst', iterations, keyInBytes, 'SHA1');
+  console.log(`pbkdf2 key: ${key}`);
+
+  const hmac256Hash = await Hmac.hmac256(message, key);
+  console.log(`hmac256: ${hmac256Hash}`);
+
+  const sha1hash = await Sha.sha1('test');
+  console.log(`sha1: ${sha1hash}`);
+
+  const rsaKeys = await Rsa.generateKeys(1024);
+  console.log('1024 private:', rsaKeys.private);
+  console.log('1024 public:', rsaKeys.public);
+
+  const rsaEncryptedMessage = await Rsa.encrypt(message, rsaKeys.public);
+  console.log('rsa Encrypt:', rsaEncryptedMessage);
+
+  const rsaSignature = await Rsa.sign(rsaEncryptedMessage, rsaKeys.private, 'SHA256');
+  console.log('rsa Signature:', rsaSignature);
+
+  const validSignature = await Rsa.verify(rsaSignature, rsaEncryptedMessage, rsaKeys.public, 'SHA256');
+  console.log('rsa signature verified:', validSignature);
+
+  const rsaDecryptedMessage = await Rsa.decrypt(rsaEncryptedMessage, rsaKeys.private);
+  console.log('rsa Decrypt:', rsaDecryptedMessage);
 };
 
 const App = () => {
@@ -60,6 +92,7 @@ const App = () => {
 
   useEffect(() => {
     const onAppStateChange = async (newState: AppStateStatus) => {
+      testCrypto();
       if (newState === 'active') {
         await fetchData();
       }
