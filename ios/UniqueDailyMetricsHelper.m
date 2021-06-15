393d1197c99ec8fc4fddceb0368d56176936d1b0
Clément Janin <clement.janin@cds-snc.ca>
2021-06-15 09:16:46 -0400

    Reworked the way we make sure iOS metrics are being sent once per UTC day

--- ios/UniqueDailyMetricsHelper.m
+++ ios/UniqueDailyMetricsHelper.m
@@ -10,7 +10,8 @@
 
 @interface UniqueDailyMetricsHelper ()
 
-@property (nonatomic, strong) NSUserDefaults *userDefaults;
+@property (nonatomic, strong) NSFileManager *fileManager;
+@property (nonatomic, strong) NSString *filePath;
 
 @end
 
@@ -20,15 +21,18 @@
 {
   self = [super init];
   if (self) {
-    self.userDefaults = [NSUserDefaults standardUserDefaults];
+    self.fileManager = [NSFileManager defaultManager];
+    self.filePath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"unique-daily-metrics-savestate.json"];
   }
   return self;
 }
 
 - (BOOL)canPublishMetricWithIdentifier:(NSString *)identifier currentDate:(NSDate *)currentDate
 {
-  NSDate *retrieveUTCDate = [self.userDefaults objectForKey:identifier];
-  if (retrieveUTCDate != nil) {
+  NSDictionary *savestate = [self readSavestate];
+  NSNumber *retrievedTimestamp = [savestate objectForKey:identifier];
+  if (retrievedTimestamp != nil) {
+    NSDate *retrieveUTCDate = [NSDate dateWithTimeIntervalSince1970:retrievedTimestamp.doubleValue];
     return ![DateUtils isSameDay:retrieveUTCDate date2:currentDate];
   } else {
     return YES;
@@ -37,7 +41,29 @@
 
 - (void)markMetricAsPublishedWithIdentifier:(NSString *)identifier currentDate:(NSDate *)currentDate
 {
-  [self.userDefaults setObject:currentDate forKey:identifier];
+  NSMutableDictionary *savestate = [[self readSavestate] mutableCopy];
+  [savestate setObject:@(currentDate.timeIntervalSince1970) forKey:identifier];
+  [self writeSavestate:savestate];
+}
+
+- (NSDictionary *)readSavestate
+{
+  if ([self.fileManager fileExistsAtPath:self.filePath]) {
+    NSData *data = [self.fileManager contentsAtPath:self.filePath];
+    return [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+  } else {
+    return @{};
+  }
+}
+
+- (void)writeSavestate:(NSDictionary *)dictionary
+{
+  if ([self.fileManager fileExistsAtPath:self.filePath] == NO) {
+    [self.fileManager createFileAtPath:self.filePath contents:nil attributes:nil];
+  }
+  
+  NSData *data = [NSJSONSerialization dataWithJSONObject:dictionary options:kNilOptions error:nil];
+  [data writeToFile:self.filePath atomically:YES];
 }
 
 @end
