900eef7d75c58a327599d40e2d680fd25bc5e5cf
Tim Arney <tim@line37.com>
2021-06-16 20:01:20 -0400

    detect missing translation keys

---
+++ translation-keys.js
@@ -0,0 +1,67 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const asyncForEach = async (arr, cb) => {
+  for (let index = 0; index < arr.length; index++) {
+    await cb(arr[index], index, arr);
+  }
+};
+
+const getRegionContent = async (activeLanguages, contentDirectory) => {
+  const data = {};
+
+  await asyncForEach(activeLanguages, async lang => {
+    const filePath = path.join(__dirname, contentDirectory, `${lang}.json`);
+    data[lang] = await fs.readFile(filePath, 'utf8');
+  });
+
+  return data;
+};
+
+const resolveObjectPath = (targetPath, obj) => {
+  return targetPath.split('.').reduce((prev, curr) => {
+    return prev ? prev[curr] : '';
+  }, obj);
+};
+
+/* eslint-disable */
+// original https://codegolf.stackexchange.com/questions/195476/extract-all-keys-from-an-object-json
+const parseKeys = (obj, str, cb) => {
+  !obj | ([obj] == obj) ||
+    Object.keys(obj).map(key => {
+      parseKeys(obj[key], (key = str ? str + [, key] : key), cb, cb(key));
+    });
+};
+/* eslint-enable */
+
+const parseFiles = (targetPath, languages, regionContent) => {
+  const en = JSON.parse(regionContent[languages[0]]);
+  const fr = JSON.parse(regionContent[languages[1]]);
+  let missing = '';
+
+  parseKeys(en, undefined, arr => {
+    const strPath = arr.split(',').join('.');
+    const enStr = resolveObjectPath(strPath, en);
+    const frStr = resolveObjectPath(strPath, fr);
+
+    if (typeof enStr === 'string' && enStr && !frStr) {
+      missing += ` - Missing FR string: "${strPath}" ${'\n'}`;
+    }
+  });
+
+  if (!missing) {
+    console.log(`${targetPath} ${languages}`, '✅'); // eslint-disable-line no-console
+    return;
+  }
+
+  console.log(`${targetPath} ${languages}`, '❌'); // eslint-disable-line no-console
+  console.log(missing); // eslint-disable-line no-console
+};
+
+const contentPaths = ['src/locale/translations', 'src/locale/translations/regional'];
+
+contentPaths.forEach(async targetPath => {
+  const languages = ['en', 'fr'];
+  const regionContent = await getRegionContent(languages, targetPath);
+  parseFiles(targetPath, languages, regionContent);
+});
